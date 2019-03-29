3f2c47348dfdc425584f792984caee2f28d9f4af
Sky Rose <srose@mbta.com>
2019-03-29 16:49:37 -0400

    specify gtfs url in environment variable

--- config/config.exs
+++ config/config.exs
@@ -8,7 +8,7 @@
 use Mix.Config
 
 config :skate,
-  gtfs_url: "https://cdn.mbta.com/MBTA_GTFS.zip"
+  gtfs_url: System.get_env("SKATE_GTFS_URL") || "https://cdn.mbta.com/MBTA_GTFS.zip"
 
 # Configures the endpoint
 config :skate, SkateWeb.Endpoint,
