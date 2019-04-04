5c497d2346f4ce6a6aa22849905706918ac351c4
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2019-04-04 14:35:22 -0400

    Add favicon and remove Phoenix starter content (#5)
    Copy favicons from dotcom.

--- test/skate_web/controllers/page_controller_test.exs
+++ test/skate_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule SkateWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "div id=\"app\""
   end
 end
