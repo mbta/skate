1c20a429d3a9e15f1784f1d4e8b6242c10fa5ad3
Sky Rose <srose@mbta.com>
2019-04-08 13:27:09 -0400

    move fetching to separate api.ts file

---
+++ assets/src/api.ts
@@ -0,0 +1,20 @@
+import { Route } from "./skate.d"
+
+interface RoutesRequestResponse {
+  data: Route[]
+}
+
+export const fetchRoutes = (): Promise<Route[]> =>
+  fetch("/api/routes")
+    .then((response: Response) => {
+      if (response.status !== 200) {
+        throw new Error(`Response error: ${response.status}`)
+      }
+      return response
+    })
+    .then((response: Response) => response.json())
+    .then(({ data: routes }: RoutesRequestResponse) => routes)
+    .catch(error => {
+      console.log(`Error loading routes`, error)
+      throw error
+    })
--- assets/src/components/app.tsx
+++ assets/src/components/app.tsx
@@ -1,10 +1,7 @@
 import React, { useEffect, useReducer } from "react"
+import * as Api from "../api"
 import { Route } from "../skate.d"
 import RoutePicker from "./route_picker"
-
-interface RoutesRequestResponse {
-  data: Route[]
-}
 
 interface State {
   routes: null | Route[]
@@ -44,20 +41,10 @@ const App = (): JSX.Element => {
   const [state, dispatch] = useReducer(reducer, initialState)
 
   useEffect(() => {
-    fetch("/api/routes")
-      .then((response: Response) => {
-        if (response.status !== 200) {
-          throw new Error(`Response error: ${response.status}`)
-        }
-        return response
-      })
-      .then((response: Response) => response.json())
-      .then(({ data: routes }: RoutesRequestResponse) =>
-        dispatch(setRoutes(routes)),
+    Api.fetchRoutes()
+      .then((routes: Route[]) =>
+        dispatch(setRoutes(routes))
       )
-      .catch(error => {
-        console.log(`Error loading routes`, error)
-      })
   }, [])
 
   return (
