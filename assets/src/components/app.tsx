c7ae715f9e163e88f5f2f22e14cd1c093c3ffd9f
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2019-04-08 12:02:50 -0400

    Fetch routes from the server and parse results

--- assets/src/components/app.tsx
+++ assets/src/components/app.tsx
@@ -1,22 +1,64 @@
-import React, { useReducer } from "react"
+import React, { useEffect, useReducer } from "react"
 import { Route } from "../skate.d"
 import RoutePicker from "./route_picker"
 
+interface RoutesRequestResponse {
+  data: Route[]
+}
+
 interface State {
-  routes: null | Route[],
+  routes: null | Route[]
 }
 
 const initialState: State = {
   routes: null,
 }
 
-function reducer(state: State, _action: Function) {
-  return state
+interface SetRoutesAction {
+  type: "SET_ROUTES"
+  payload: {
+    routes: Route[]
+  }
 }
 
+type Action = SetRoutesAction
+
+const setRoutes = (routes: Route[]): SetRoutesAction => ({
+  type: "SET_ROUTES",
+  payload: { routes },
+})
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case "SET_ROUTES":
+      return {
+        ...state,
+        routes: action.payload.routes,
+      }
+    default:
+      return state
+  }
+}
 
 const App = (): JSX.Element => {
-  const [state, _dispatch] = useReducer(reducer, initialState)
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    fetch("/api/routes")
+      .then((response: Response) => {
+        if (response.status !== 200) {
+          throw new Error(`Response error: ${response.status}`)
+        }
+        return response
+      })
+      .then((response: Response) => response.json())
+      .then(({ data: routes }: RoutesRequestResponse) =>
+        dispatch(setRoutes(routes)),
+      )
+      .catch(error => {
+        console.log(`Error loading routes`, error)
+      })
+  }, [])
 
   return (
     <>
