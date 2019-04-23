ad4e1829f9bb6add5c0b349f18b9b551857c6d2b
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2019-04-23 16:08:02 -0400

    Test that we fetch routes when the app is mounted

--- assets/tests/components/app.test.tsx
+++ assets/tests/components/app.test.tsx
@@ -1,6 +1,11 @@
+import { mount } from "enzyme"
 import React from "react"
-import renderer from "react-test-renderer"
+import renderer, { act } from "react-test-renderer"
+import * as Api from "../../src/api"
 import App from "../../src/components/app"
+import { Route } from "../../src/skate"
+
+jest.mock("../../src/api")
 
 test("renders", () => {
   const tree = renderer.create(<App />).toJSON()
@@ -8,3 +13,19 @@ test("renders", () => {
   expect(tree).toMatchSnapshot()
 })
 
+test("fetches routes", () => {
+  const mockFetchRoutes = jest.spyOn(Api, "fetchRoutes")
+  const mockFetchRoutesImplementation: () => Promise<Route[]> = () => ({
+    [Symbol.toStringTag]: "Promise",
+    catch: jest.fn(),
+    finally: jest.fn(),
+    then: jest.fn(),
+  })
+  mockFetchRoutes.mockImplementation(mockFetchRoutesImplementation)
+
+  act(() => {
+    mount(<App />)
+  })
+
+  expect(mockFetchRoutes).toHaveBeenCalled()
+})
