1a78e7bc4181beed243afa722bc3f07ba01060be
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2019-04-25 10:39:51 -0400

    Add typespecs to VehiclesChannel

--- lib/skate_web/channels/vehicles_channel.ex
+++ lib/skate_web/channels/vehicles_channel.ex
@@ -1,6 +1,11 @@
 defmodule SkateWeb.VehiclesChannel do
   use SkateWeb, :channel
 
+  alias Phoenix.Socket
+  alias Realtime.{Server, Vehicle}
+
+  @spec join(topic :: String.t(), payload :: map, socket :: Socket.t()) ::
+          {:ok, reply :: %{vehicles: Server.vehicles()}, Socket.t()}
   def join("vehicles:" <> route_id, _message, socket) do
     vehicles = Realtime.Server.subscribe(route_id)
     {:ok, %{vehicles: vehicles}, socket}
@@ -10,6 +15,8 @@ defmodule SkateWeb.VehiclesChannel do
     {:error, %{message: "no such topic \"#{topic}\""}}
   end
 
+  @spec handle_info(msg :: {:new_realtime_data, [Vehicle.t()]}, socket :: Socket.t()) ::
+          {:noreply, Socket.t()}
   def handle_info({:new_realtime_data, vehicles}, socket) do
     push(socket, "vehicles", %{vehicles: vehicles})
     {:noreply, socket}
