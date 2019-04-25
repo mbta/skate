15863a741e1c4a451e4a71ed5dd60e1494da89e3
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2019-04-25 10:22:46 -0400

    Set up a channel for sending vehicles to client

---
+++ lib/skate_web/channels/vehicles_channel.ex
@@ -0,0 +1,18 @@
+defmodule SkateWeb.VehiclesChannel do
+  use SkateWeb, :channel
+
+  def join("vehicles:" <> route_id, _message, socket) do
+    socket = assign(socket, :route_id, route_id)
+    vehicles = Realtime.Server.subscribe(route_id)
+    {:ok, %{vehicles: vehicles}, socket}
+  end
+
+  def join(topic, _message, _socket) do
+    {:error, %{message: "no such topic \"#{topic}\""}}
+  end
+
+  def handle_info({:new_realtime_data, vehicles}, socket) do
+    push(socket, "vehicles", %{vehicles: vehicles})
+    {:noreply, socket}
+  end
+end
