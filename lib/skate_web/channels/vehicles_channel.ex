3b15c4daf9a557f1204e45cddef21bbc809edcec
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2019-04-25 12:41:28 -0400

    Use `@impl Phoenix.Channel`

--- lib/skate_web/channels/vehicles_channel.ex
+++ lib/skate_web/channels/vehicles_channel.ex
@@ -1,13 +1,11 @@
 defmodule SkateWeb.VehiclesChannel do
   use SkateWeb, :channel
 
-  alias Phoenix.Socket
-  alias Realtime.{Server, Vehicle}
+  alias Realtime.Server
 
-  @spec join(topic :: String.t(), payload :: map, socket :: Socket.t()) ::
-          {:ok, reply :: %{vehicles: Server.vehicles()}, Socket.t()}
+  @impl Phoenix.Channel
   def join("vehicles:" <> route_id, _message, socket) do
-    vehicles = Realtime.Server.subscribe(route_id)
+    vehicles = Server.subscribe(route_id)
     {:ok, %{vehicles: vehicles}, socket}
   end
 
@@ -15,8 +13,7 @@ defmodule SkateWeb.VehiclesChannel do
     {:error, %{message: "no such topic \"#{topic}\""}}
   end
 
-  @spec handle_info(msg :: {:new_realtime_data, [Vehicle.t()]}, socket :: Socket.t()) ::
-          {:noreply, Socket.t()}
+  @impl Phoenix.Channel
   def handle_info({:new_realtime_data, vehicles}, socket) do
     push(socket, "vehicles", %{vehicles: vehicles})
     {:noreply, socket}
