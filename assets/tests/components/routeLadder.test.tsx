9f65eedbd30ea3cb497f64ae155c3f1f850e127e
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2019-05-06 12:36:20 -0400

    Add a test for rendering a route ladder with vehicles

--- assets/tests/components/routeLadder.test.tsx
+++ assets/tests/components/routeLadder.test.tsx
@@ -3,7 +3,7 @@ import React from "react"
 import renderer, { act } from "react-test-renderer"
 import RouteLadder from "../../src/components/routeLadder"
 import DispatchProvider from "../../src/providers/dispatchProvider"
-import { Route } from "../../src/skate"
+import { Route, Vehicle } from "../../src/skate"
 import { deselectRoute } from "../../src/state"
 
 test("renders a route ladder", () => {
@@ -12,6 +12,55 @@ test("renders a route ladder", () => {
 
   const tree = renderer
     .create(<RouteLadder route={route} timepoints={timepoints} vehicles={[]} />)
+    .toJSON()
+
+  expect(tree).toMatchSnapshot()
+})
+
+test("renders a route ladder with vehicles", () => {
+  const route: Route = { id: "28" }
+  const timepoints = [{ id: "MATPN" }, { id: "WELLH" }, { id: "MORTN" }]
+  const vehicles: Vehicle[] = [
+    {
+      direction_id: 0,
+      id: "y1818",
+      label: "1818",
+      route_id: "1",
+      stop_status: {
+        status: "in_transit_to",
+        stop_id: "57",
+      },
+      timepoint_status: {
+        status: "in_transit_to",
+        timepoint_id: "MATPN",
+        percent_of_the_way_to_timepoint: 50,
+      },
+      timestamp: 1557160307,
+      trip_id: "39914237",
+    },
+    {
+      direction_id: 1,
+      id: "y0479",
+      label: "0479",
+      route_id: "1",
+      stop_status: {
+        status: "in_transit_to",
+        stop_id: "59",
+      },
+      timepoint_status: {
+        status: "in_transit_to",
+        timepoint_id: "MORTN",
+        percent_of_the_way_to_timepoint: 100,
+      },
+      timestamp: 1557160347,
+      trip_id: "39914128",
+    },
+  ]
+
+  const tree = renderer
+    .create(
+      <RouteLadder route={route} timepoints={timepoints} vehicles={vehicles} />
+    )
     .toJSON()
 
   expect(tree).toMatchSnapshot()
