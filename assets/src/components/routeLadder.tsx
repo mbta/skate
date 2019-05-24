26713b6adba0006aa32aec6948f455ba8bb2f957
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2019-05-24 12:04:06 -0400

    Remove vehicle debugging box

--- assets/src/components/routeLadder.tsx
+++ assets/src/components/routeLadder.tsx
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react"
 import DispatchContext from "../contexts/dispatchContext"
 import { reverseIcon, reverseIconReversed } from "../helpers/icon"
 import { LoadableTimepoints, Route, Vehicle, VehicleId } from "../skate"
-import { deselectRoute, selectVehicle } from "../state"
+import { deselectRoute } from "../state"
 import CloseButton from "./closeButton"
 import Ladder, { flipLadderDirection, LadderDirection } from "./ladder"
 import Loading from "./loading"
@@ -23,61 +23,6 @@ const Header = ({ route }: { route: Route }) => {
 
       <div className="m-route-ladder__route-name">{route.id}</div>
     </div>
-  )
-}
-
-const Vehicle = ({ vehicle }: { vehicle: Vehicle }) => {
-  const dispatch = useContext(DispatchContext)
-
-  return (
-    <ul
-      className="m-route-ladder__vehicle"
-      style={{ border: "1px solid black" }}
-      onClick={() => dispatch(selectVehicle(vehicle.id))}
-    >
-      <li>
-        <em>id:</em> {vehicle.id}
-      </li>
-      <li>
-        <em>label:</em> {vehicle.label}
-      </li>
-      <li>
-        <em>timestamp:</em> {vehicle.timestamp}
-      </li>
-      <li>
-        <em>latitude:</em> {vehicle.latitude}
-      </li>
-      <li>
-        <em>longitude:</em> {vehicle.longitude}
-      </li>
-      <li>
-        <em>direction_id:</em> {vehicle.direction_id}
-      </li>
-      <li>
-        <em>route_id:</em> {vehicle.route_id}
-      </li>
-      <li>
-        <em>trip_id:</em> {vehicle.trip_id}
-      </li>
-      <li>
-        <em>headsign:</em> {vehicle.headsign}
-      </li>
-      <li>
-        <em>variant:</em> {vehicle.via_variant}
-      </li>
-      <li>
-        <em>stop status:</em> {vehicle.stop_status.status},<br />
-        <em>stop:</em> {vehicle.stop_status.stop_id}
-      </li>
-      <li>
-        <em>timepoint:</em>{" "}
-        {vehicle.timepoint_status && vehicle.timepoint_status.timepoint_id},
-        <br />
-        <em>fraction before:</em>{" "}
-        {vehicle.timepoint_status &&
-          vehicle.timepoint_status.fraction_until_timepoint}
-      </li>
-    </ul>
   )
 }
 
@@ -116,9 +61,6 @@ const RouteLadder = ({
       ) : (
         <Loading />
       )}
-      {vehicles.map(vehicle => (
-        <Vehicle key={vehicle.id} vehicle={vehicle} />
-      ))}
     </div>
   )
 }
