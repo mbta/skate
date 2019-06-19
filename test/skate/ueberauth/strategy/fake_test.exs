a5c9ca04595a2fbed3b30bad7cbb5ba3d687b93e
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2019-06-19 16:48:01 -0400

    Add tests for our fake strategy

---
+++ test/skate/ueberauth/strategy/fake_test.exs
@@ -0,0 +1,23 @@
+defmodule Skate.Ueberauth.Strategy.FakeTest do
+  use ExUnit.Case, async: true
+
+  alias Skate.Ueberauth.Strategy.Fake
+  alias Ueberauth.Auth.{Credentials, Extra, Info}
+
+  test "credentials returns a credentials struct" do
+    assert Fake.credentials(%{}) == %Credentials{
+             token: "fake_access_token",
+             refresh_token: "fake_refresh_token",
+             expires: true,
+             expires_at: System.system_time(:second) + 60 * 60
+           }
+  end
+
+  test "info returns an empty Info struct" do
+    assert Fake.info(%{}) == %Info{}
+  end
+
+  test "extra returns an Extra struct with empty raw_info" do
+    assert Fake.extra(%{}) == %Extra{raw_info: %{}}
+  end
+end
