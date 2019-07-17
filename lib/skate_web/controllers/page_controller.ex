9a6c55732a746bb2213e48a1306b428fd4fbe9cf
Kate Hedgpeth <katehedgpeth@gmail.com>
2019-07-17 13:20:44 -0400

    log uid on page visit

--- lib/skate_web/controllers/page_controller.ex
+++ lib/skate_web/controllers/page_controller.ex
@@ -1,7 +1,12 @@
 defmodule SkateWeb.PageController do
+  require Logger
   use SkateWeb, :controller
+  alias SkateWeb.AuthManager
 
   def index(conn, _params) do
+    uid = AuthManager.Plug.current_resource(conn)
+    _ = Logger.info("uid=#{uid}")
+
     render(conn, "index.html")
   end
 end
