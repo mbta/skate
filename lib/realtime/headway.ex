339fae475a5728ebfe20bb8f9df8d252c8e20e68
Kate Hedgpeth <katehedgpeth@gmail.com>
2019-07-19 11:00:30 -0400

    Move data out of priv folder (#100)
    * read data at compile time instead of runtime

--- lib/realtime/headway.ex
+++ lib/realtime/headway.ex
@@ -2,6 +2,44 @@ defmodule Realtime.Headway do
   alias Gtfs.{Direction, Route, Stop}
   alias Realtime.TimePeriod
 
+  defmodule Parser do
+    alias Realtime.Headway
+
+    @spec parse_json_data(map()) :: Headway.key_route_headways()
+    def parse_json_data(json_data) do
+      Map.new(json_data, fn {route_id, direction_origin_headways} ->
+        {route_id, parse_direction_origin_headways(direction_origin_headways)}
+      end)
+    end
+
+    @spec parse_direction_origin_headways(map()) :: Headway.direction_origin_headways()
+    def parse_direction_origin_headways(direction_origin_headways) do
+      Map.new(direction_origin_headways, fn {direction_id, origin_headways} ->
+        {String.to_integer(direction_id), parse_origin_headways(origin_headways)}
+      end)
+    end
+
+    @spec parse_origin_headways(map()) :: Headway.origin_headways()
+    def parse_origin_headways(origin_headways) do
+      Map.new(origin_headways, fn {stop_id, time_period_headways} ->
+        {stop_id, parse_time_period_headways(time_period_headways)}
+      end)
+    end
+
+    @spec parse_time_period_headways([map()]) :: [Headway.time_period_headway()]
+    def parse_time_period_headways(time_period_headways) do
+      Enum.map(time_period_headways, fn %{
+                                          "time_period_name" => time_period_name,
+                                          "average_headway" => average_headway
+                                        } ->
+        %{
+          time_period_name: time_period_name,
+          average_headway: average_headway
+        }
+      end)
+    end
+  end
+
   @type headway_spacing ::
           :very_bunched
           | :bunched
@@ -21,6 +59,12 @@ defmodule Realtime.Headway do
         }
 
   @type seconds :: non_neg_integer()
+
+  @key_route_headways [File.cwd!(), "data", "key_route_headways.json"]
+                      |> Path.join()
+                      |> File.read!()
+                      |> Jason.decode!()
+                      |> Parser.parse_json_data()
 
   @spec current_headway_spacing(seconds(), seconds()) :: headway_spacing()
   def current_headway_spacing(expected_headway_seconds, headway_seconds)
@@ -44,8 +88,7 @@ defmodule Realtime.Headway do
   @spec current_expected_headway_seconds(Route.id(), Direction.id(), Stop.id(), DateTime.t()) ::
           seconds() | nil
   def current_expected_headway_seconds(route_id, direction_id, origin_stop_id, date_time) do
-    with {:ok, key_route_headways} <- data(),
-         {:ok, direction_origin_headways} <- Map.fetch(key_route_headways, route_id),
+    with {:ok, direction_origin_headways} <- Map.fetch(@key_route_headways, route_id),
          {:ok, origin_headways} <- Map.fetch(direction_origin_headways, direction_id),
          {:ok, headways} <- Map.fetch(origin_headways, origin_stop_id),
          time_period <- TimePeriod.current(date_time),
@@ -72,51 +115,6 @@ defmodule Realtime.Headway do
     seconds = String.to_integer(seconds_string)
 
     hours * 60 * 60 + minutes * 60 + seconds
-  end
-
-  @spec data() :: {:ok, key_route_headways()}
-  def data() do
-    filename = Application.app_dir(:skate, "priv/data/key_route_headways.json")
-
-    with {:ok, body} <- File.read(filename), {:ok, json_data} <- Jason.decode(body) do
-      key_route_headways = parse_json_data(json_data)
-
-      {:ok, key_route_headways}
-    end
-  end
-
-  @spec parse_json_data(map()) :: key_route_headways()
-  defp parse_json_data(json_data) do
-    Map.new(json_data, fn {route_id, direction_origin_headways} ->
-      {route_id, parse_direction_origin_headways(direction_origin_headways)}
-    end)
-  end
-
-  @spec parse_direction_origin_headways(map()) :: direction_origin_headways()
-  defp parse_direction_origin_headways(direction_origin_headways) do
-    Map.new(direction_origin_headways, fn {direction_id, origin_headways} ->
-      {String.to_integer(direction_id), parse_origin_headways(origin_headways)}
-    end)
-  end
-
-  @spec parse_origin_headways(map()) :: origin_headways()
-  defp parse_origin_headways(origin_headways) do
-    Map.new(origin_headways, fn {stop_id, time_period_headways} ->
-      {stop_id, parse_time_period_headways(time_period_headways)}
-    end)
-  end
-
-  @spec parse_time_period_headways([map()]) :: [time_period_headway()]
-  defp parse_time_period_headways(time_period_headways) do
-    Enum.map(time_period_headways, fn %{
-                                        "time_period_name" => time_period_name,
-                                        "average_headway" => average_headway
-                                      } ->
-      %{
-        time_period_name: time_period_name,
-        average_headway: average_headway
-      }
-    end)
   end
 
   @spec by_name(time_period_headway(), TimePeriod.t()) :: boolean
