2916f3416c7f69b0f5cd955098d00d6acd33abc1
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2019-08-05 12:09:24 -0400

    Configure prod to use our CDN for static assets (#127)
    Use `static_url` instead of `static_path`.
    
    Parse static URL settings for the endpoint.

--- config/prod.exs
+++ config/prod.exs
@@ -13,6 +13,12 @@ config :skate, SkateWeb.Endpoint,
   server: true,
   http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [host: {:system, "HOST"}, port: 80],
+  static_url: [
+    scheme: {:system, "STATIC_SCHEME"},
+    host: {:system, "STATIC_HOST"},
+    port: {:system, "STATIC_PORT"},
+    path: {:system, "STATIC_PATH"}
+  ],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 config :skate, :websocket_check_origin, [
