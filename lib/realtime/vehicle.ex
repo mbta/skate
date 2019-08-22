b9ca4419279f20ea84d9b46751b0589fefe94998
Sky Rose <srose@mbta.com>
2019-08-22 14:30:59 -0400

    Move timepoint_status into its own file (#156)
    * move timepoint_status out of vehicle.ex
    
    * small type and name fixes

--- lib/realtime/vehicle.ex
+++ lib/realtime/vehicle.ex
@@ -1,7 +1,8 @@
 defmodule Realtime.Vehicle do
   alias Concentrate.{DataDiscrepancy, TripUpdate, VehiclePosition}
-  alias Gtfs.{Block, Direction, Route, RoutePattern, Stop, StopTime, Trip}
+  alias Gtfs.{Block, Direction, Route, RoutePattern, Stop, Trip}
   alias Realtime.Headway
+  alias Realtime.TimepointStatus
 
   @type current_status :: :in_transit_to | :stopped_at
   @type stop_status :: %{
@@ -9,22 +10,6 @@ defmodule Realtime.Vehicle do
           stop_id: Stop.id(),
           stop_name: String.t()
         }
-  @typedoc """
-  fraction_until_timepoint ranges
-    from 0.0 (inclusive) if the vehicle is at the given timepoint
-    to 1.0 (exclusive) if the vehicle has just left the previous timepoint
-  """
-  @type timepoint_status ::
-          %{
-            timepoint_id: StopTime.timepoint_id(),
-            fraction_until_timepoint: float()
-          }
-  @type scheduled_location ::
-          %{
-            route_id: Route.id(),
-            direction_id: Direction.id(),
-            timepoint_status: timepoint_status()
-          }
   @type route_status :: :incoming | :on_route
 
   @type t :: %__MODULE__{
@@ -41,7 +26,7 @@ defmodule Realtime.Vehicle do
           bearing: integer() | nil,
           speed: integer() | nil,
           stop_sequence: integer() | nil,
-          block_id: String.t() | nil,
+          block_id: Block.id() | nil,
           operator_id: String.t() | nil,
           operator_name: String.t() | nil,
           run_id: String.t() | nil,
@@ -60,8 +45,8 @@ defmodule Realtime.Vehicle do
           sources: MapSet.t(String.t()),
           data_discrepancies: [DataDiscrepancy.t()],
           stop_status: stop_status(),
-          timepoint_status: timepoint_status() | nil,
-          scheduled_location: scheduled_location() | nil,
+          timepoint_status: TimepointStatus.timepoint_status() | nil,
+          scheduled_location: TimepointStatus.scheduled_location() | nil,
           route_status: route_status()
         }
 
@@ -153,8 +138,8 @@ defmodule Realtime.Vehicle do
     via_variant = trip && trip.route_pattern_id && RoutePattern.via_variant(trip.route_pattern_id)
     stop_times_on_trip = (trip && trip.stop_times) || []
     stop_name = stop_name(vehicle_position, stop_id)
-    timepoint_status = timepoint_status(stop_times_on_trip, stop_id)
-    scheduled_location = scheduled_location(block, now_fn.())
+    timepoint_status = TimepointStatus.timepoint_status(stop_times_on_trip, stop_id)
+    scheduled_location = TimepointStatus.scheduled_location(block, now_fn.())
 
     # If a vehicle is scheduled to be on another line, don't draw any line.
     scheduled_location =
@@ -275,118 +260,6 @@ defmodule Realtime.Vehicle do
     now_time_of_day - Block.end_time(block) <= one_hour_in_seconds
   end
 
-  @spec timepoint_status([StopTime.t()], Stop.id()) :: timepoint_status() | nil
-  def timepoint_status(stop_times, stop_id) do
-    {past_stop_times, future_stop_times} = Enum.split_while(stop_times, &(&1.stop_id != stop_id))
-
-    case Enum.find(future_stop_times, &is_a_timepoint?(&1)) do
-      %StopTime{timepoint_id: next_timepoint_id} ->
-        # past_count needs +1 for the step between the current timepoint and the first of the past stops
-        past_count = count_to_timepoint(Enum.reverse(past_stop_times)) + 1
-        future_count = count_to_timepoint(future_stop_times)
-
-        %{
-          timepoint_id: next_timepoint_id,
-          fraction_until_timepoint: future_count / (future_count + past_count)
-        }
-
-      nil ->
-        nil
-    end
-  end
-
-  @doc """
-  If a block isn't scheduled to have started yet:
-    the start of the first trip
-  If a block is scheduled to have finished:
-    the end of the last trip
-  If now is in the middle of a layover:
-    the end of the previous trip
-  If now is in the middle of a trip:
-    the next timpeoint in that trip
-  """
-  @spec scheduled_location(Block.t(), Util.Time.timestamp()) :: scheduled_location() | nil
-  def scheduled_location(nil, _now) do
-    nil
-  end
-
-  def scheduled_location([], _now) do
-    nil
-  end
-
-  def scheduled_location(block, now) do
-    now_time_of_day =
-      Util.Time.next_time_of_day_for_timestamp_after(
-        now,
-        # Allow a little wiggle room in case a bus appears just before its block starts
-        Util.Time.time_of_day_add_minutes(Block.start_time(block), -60)
-      )
-
-    trip = current_trip_on_block(block, now_time_of_day)
-    timepoints = Enum.filter(trip.stop_times, &is_a_timepoint?/1)
-    timepoint_status = current_timepoint_status(timepoints, now_time_of_day)
-
-    %{
-      route_id: trip.route_id,
-      direction_id: trip.direction_id,
-      timepoint_status: timepoint_status
-    }
-  end
-
-  @spec current_trip_on_block(Block.t(), Util.Time.time_of_day()) :: Trip.t()
-  defp current_trip_on_block(block, now) do
-    cond do
-      now <= Block.start_time(block) ->
-        # Block isn't scheduled to have started yet
-        List.first(block)
-
-      now >= Block.end_time(block) ->
-        # Block is scheduled to have finished
-        List.last(block)
-
-      true ->
-        # Either the current trip or the trip that just ended (the last trip to have started)
-        block
-        |> Enum.take_while(fn trip ->
-          Trip.start_time(trip) <= now
-        end)
-        |> List.last()
-    end
-  end
-
-  @spec current_timepoint_status([StopTime.t()], Util.Time.time_of_day()) :: timepoint_status
-  defp current_timepoint_status(timepoints, now) do
-    cond do
-      now <= List.first(timepoints).time ->
-        # Trip isn't scheduled to have started yet
-        %{
-          timepoint_id: List.first(timepoints).timepoint_id,
-          fraction_until_timepoint: 0
-        }
-
-      now >= List.last(timepoints).time ->
-        # Trip is scheduled to have finished
-        %{
-          timepoint_id: List.last(timepoints).timepoint_id,
-          fraction_until_timepoint: 0
-        }
-
-      true ->
-        # Trip is scheduled to be between two timepoints
-        {previous_timepoint, next_timepoint} =
-          Realtime.Helpers.find_and_previous(timepoints, fn timepoint ->
-            timepoint.time > now
-          end)
-
-        %{
-          timepoint_id: next_timepoint.timepoint_id,
-          fraction_until_timepoint:
-            (next_timepoint.time - now) /
-              (next_timepoint.time - previous_timepoint.time)
-        }
-    end
-  end
-
   @spec stop_name(map() | nil, String.t() | nil) :: String.t() | nil
   defp stop_name(vehicle_position, stop_id) do
     vp_stop_name = VehiclePosition.stop_name(vehicle_position)
@@ -403,16 +276,6 @@ defmodule Realtime.Vehicle do
     stop = Gtfs.stop(stop_id)
     if stop, do: stop.name, else: stop_id
   end
-
-  @spec count_to_timepoint([StopTime.t()]) :: non_neg_integer()
-  defp count_to_timepoint(stop_times) do
-    count = Enum.find_index(stop_times, &is_a_timepoint?(&1))
-
-    if is_number(count), do: count, else: length(stop_times)
-  end
-
-  @spec is_a_timepoint?(StopTime.t()) :: boolean
-  defp is_a_timepoint?(%StopTime{timepoint_id: timepoint_id}), do: timepoint_id != nil
 
   @spec decode_current_status(atom()) :: current_status()
   defp decode_current_status(:IN_TRANSIT_TO), do: :in_transit_to
