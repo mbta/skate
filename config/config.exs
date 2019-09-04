mbta/skate

commit bfe60668e53aa5d6fe81358175497cca449575ab

don't fetch gtfs or realtime data in tests

lib/skate/application.ex
@@ -16,12 +16,20 @@ defmodule Skate.Application do
     )
 
     # List all child processes to be supervised
-    children = [
-      SkateWeb.Endpoint,
-      Gtfs.Supervisor,
-      Realtime.Supervisor,
-      RefreshTokenStore
-    ]
+    children =
+      if Mix.env() == :test do
+        [
+          SkateWeb.Endpoint,
+          RefreshTokenStore
+        ]
+      else
+        [
+          SkateWeb.Endpoint,
+          Gtfs.Supervisor,
+          Realtime.Supervisor,
+          RefreshTokenStore
+        ]
+      end
 
     Supervisor.start_link(children, strategy: :one_for_all, name: Skate.Supervisor)
   end

test/channels/vehicles_channel_test.exs
@@ -12,6 +12,9 @@ defmodule SkateWeb.VehiclesChannelTest do
 
     socket = socket(UserSocket, "", %{})
 
+    start_supervised({Registry, keys: :duplicate, name: Realtime.Server.registry_name()})
+    start_supervised({Realtime.Server, name: Realtime.Server.default_name()})
+
     {:ok, socket: socket}
   end
 

test/gtfs/health_server_test.exs
@@ -19,8 +19,9 @@ defmodule Gtfs.HealthServerTest do
     assert HealthServer.ready?(health_server_pid)
   end
 
-  test "real gtfs server uses the default health server" do
-    # The real data takes longer to load than the tests take to run
+  test "can start in supervision tree with default name" do
+    start_supervised({Gtfs.HealthServer, []})
+    # No Gtfs server points to this health server, so it won't be ready
     refute HealthServer.ready?(HealthServer.default_server())
   end
 end

test/realtime/server_test.exs
@@ -50,6 +50,11 @@ defmodule Realtime.ServerTest do
     "1" => @vehicles_for_route
   }
 
+  setup do
+    start_supervised({Registry, keys: :duplicate, name: Realtime.Server.registry_name()})
+    :ok
+  end
+
   describe "subscribe_to_route" do
     setup do
       reassign_env(:realtime, :trip_fn, fn _trip_id -> nil end)

test/skate_web/controllers/health_controller_test.exs
@@ -18,9 +18,9 @@ defmodule SkateWeb.HealthControllerTest do
     end
 
     test "uses the default health server", %{conn: conn} do
-      # The real gtfs data takes longer to load than the tests take to run.
-      # So it won't be ready
+      start_supervised({Gtfs.HealthServer, []})
       response = get(conn, Routes.health_path(conn, :index))
+      # No Gtfs server points to this health server, so it won't be ready
       assert response.status == 503
     end
   end

commit 779c750421f383c6bd0006a6098796ef839d5cd5

use application env to turn off gtfs and realtime process in test
fixes the bug introduced by bfe6066
Mix.env can't be used at runtime

config/config.exs
@@ -15,6 +15,7 @@ config :skate,
   swiftly_realtime_vehicles_url: {:system, "SWIFTLY_REALTIME_VEHICLES_URL"},
   secret: {:system, "SKATE_SECRET"},
   signed_secret: {:system, "SKATE_SIGNED_SECRET"},
+  start_data_processes: true,
   record_fullstory: false,
   log_duration_timing: true,
   refresh_token_store: RefreshTokenStore

config/test.exs
@@ -1,5 +1,8 @@
 use Mix.Config
 
+config :skate,
+  start_data_processes: false
+
 config :skate, Gtfs.CacheFile, cache_filename: "test_cache.terms"
 
 # We don't run a server during test. If one is required,

lib/skate/application.ex
@@ -17,19 +17,18 @@ defmodule Skate.Application do
 
     # List all child processes to be supervised
     children =
-      if Mix.env() == :test do
-        [
-          SkateWeb.Endpoint,
-          RefreshTokenStore
-        ]
-      else
-        [
-          SkateWeb.Endpoint,
-          Gtfs.Supervisor,
-          Realtime.Supervisor,
-          RefreshTokenStore
-        ]
-      end
+      [
+        SkateWeb.Endpoint,
+        RefreshTokenStore
+      ] ++
+        if Application.get_env(:skate, :start_data_processes) do
+          [
+            Gtfs.Supervisor,
+            Realtime.Supervisor
+          ]
+        else
+          []
+        end
 
     Supervisor.start_link(children, strategy: :one_for_all, name: Skate.Supervisor)
   end