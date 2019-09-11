b3f3268b0a66236eb2fb0c8b443ea56340490ebe
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2019-09-11 13:49:07 -0400

    Revert "remove shuttle map flag (#186)" (#190)
    This reverts commit f5a4ed74bd3bea5bca848e69be8cbf6283b485f5.
    
    Temporarily just so we can deploy a bug fix before this goes live.

--- assets/src/components/tabBar.tsx
+++ assets/src/components/tabBar.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from "react"
 import { NavLink } from "react-router-dom"
+import featureIsEnabled from "../laboratoryFeatures"
 
 interface Props {
   pickerContainerIsVisible: boolean
@@ -24,17 +25,19 @@ const TabBar = ({
           {ladderIcon}
         </NavLink>
       </li>
-      <li>
-        <NavLink
-          activeClassName="m-tab-bar__link--active"
-          className="m-tab-bar__link"
-          exact={true}
-          title="Shuttle Map"
-          to="/shuttle-map"
-        >
-          {mapIcon}
-        </NavLink>
-      </li>
+      {featureIsEnabled("shuttle_map") && (
+        <li>
+          <NavLink
+            activeClassName="m-tab-bar__link--active"
+            className="m-tab-bar__link"
+            exact={true}
+            title="Shuttle Map"
+            to="/shuttle-map"
+          >
+            {mapIcon}
+          </NavLink>
+        </li>
+      )}
 
       <li>
         <NavLink
