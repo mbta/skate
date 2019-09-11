c3e4a75fe0b6eb4a95281b87470842bfe7bb6c35
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2019-09-11 16:09:16 -0400

    Revert "Revert "remove shuttle map flag (#186)" (#190)" (#191)
    This reverts commit b3f3268b0a66236eb2fb0c8b443ea56340490ebe.

--- assets/src/components/app.tsx
+++ assets/src/components/app.tsx
@@ -1,7 +1,6 @@
 import React, { ReactElement, useContext } from "react"
 import { BrowserRouter, Route as BrowserRoute } from "react-router-dom"
 import { StateDispatchContext } from "../contexts/stateDispatchContext"
-import featureIsEnabled from "../laboratoryFeatures"
 import AboutPage from "./aboutPage"
 import LadderPage from "./ladderPage"
 import SettingsPage from "./settingsPage"
@@ -16,13 +15,11 @@ const App = (): ReactElement<HTMLDivElement> => {
       <div className="m-app">
         <TabBar pickerContainerIsVisible={pickerContainerIsVisible} />
         <BrowserRoute exact={true} path="/" component={LadderPage} />
-        {featureIsEnabled("shuttle_map") && (
-          <BrowserRoute
-            exact={true}
-            path="/shuttle-map"
-            component={ShuttleMapPage}
-          />
-        )}
+        <BrowserRoute
+          exact={true}
+          path="/shuttle-map"
+          component={ShuttleMapPage}
+        />
         <BrowserRoute exact={true} path="/settings" component={SettingsPage} />
         <BrowserRoute path="/about" component={AboutPage} />
       </div>
