ea2811d8c13da6ac63054efaacd0fb8fb881e532
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2019-09-24 15:35:25 -0400

    Refresh the page when you click the sk8 logo (#206)

--- assets/tests/components/tabBar.test.tsx
+++ assets/tests/components/tabBar.test.tsx
@@ -1,9 +1,32 @@
 import { mount } from "enzyme"
 import React from "react"
 import { BrowserRouter } from "react-router-dom"
+import renderer from "react-test-renderer"
 import TabBar from "../../src/components/tabBar"
 
 describe("tabBar", () => {
+  it("renders", () => {
+    const tree = renderer
+      .create(
+        <BrowserRouter>
+          <TabBar pickerContainerIsVisible={true} />
+        </BrowserRouter>
+      )
+      .toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it("sets class to visible when picker is visible", () => {
+    const wrapper = mount(
+      <BrowserRouter>
+        <TabBar pickerContainerIsVisible={true} />
+      </BrowserRouter>
+    )
+    expect(wrapper.find(".m-tab-bar").hasClass("visible")).toBe(true)
+    expect(wrapper.find(".m-tab-bar").hasClass("hidden")).toBe(false)
+  })
+
   it("sets class to hidden when picker is hidden", () => {
     const wrapper = mount(
       <BrowserRouter>
@@ -12,5 +35,24 @@ describe("tabBar", () => {
     )
     expect(wrapper.find(".m-tab-bar").hasClass("hidden")).toBe(true)
     expect(wrapper.find(".m-tab-bar").hasClass("visible")).toBe(false)
+  })
+
+  it("reloads the page when you click on the logo", () => {
+    const reloadSpy = jest
+      .spyOn(window.location, "reload")
+      .mockImplementationOnce(() => ({}))
+
+    const wrapper = mount(
+      <BrowserRouter>
+        <TabBar pickerContainerIsVisible={false} />
+      </BrowserRouter>
+    )
+
+    wrapper
+      .find(".m-tab-bar__logo")
+      .first()
+      .simulate("click")
+
+    expect(reloadSpy).toHaveBeenCalled()
   })
 })
