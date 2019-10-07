ff343a2bc8cfed2a1a3c7bf649b890404e908874
Sky Rose <srose@mbta.com>
2019-10-07 11:09:51 -0400

    load all env vars at init (#211)
    This removes `Skate.Application.get_config_string()`,
    which checked for environment variables at call time,
    and was redundant with the init time check.
    All calls now use `Application.get_env()`
    
    This removes the special case for static_url
    by making the general case recursive.
    
    Because there's no way to recurse over all the config,
    the top-level keys to check have to be listed in `application.ex`.

--- lib/skate/application.ex
+++ lib/skate/application.ex
@@ -5,15 +5,9 @@ defmodule Skate.Application do
 
   use Application
 
+  @impl true
   def start(_type, _args) do
-    runtime_config()
-
-    # Pull the STATIC_SCHEME variable out of the environment
-    Application.put_env(
-      :skate,
-      SkateWeb.Endpoint,
-      update_static_url(Application.get_env(:skate, SkateWeb.Endpoint))
-    )
+    load_runtime_config()
 
     # List all child processes to be supervised
     children =
@@ -33,54 +27,60 @@ defmodule Skate.Application do
     Supervisor.start_link(children, strategy: :one_for_all, name: Skate.Supervisor)
   end
 
-  def runtime_config() do
-    environment_variables = [
-      gtfs_url: "SKATE_GTFS_URL",
-      swiftly_authorization_key: "SWIFTLY_AUTHORIZATION_KEY",
-      swiftly_realtime_vehicles_url: "SWIFTLY_REALTIME_VEHICLES_URL"
-    ]
-
-    for {application_key, environment_key} <- environment_variables do
-      if value = System.get_env(environment_key) do
-        Application.put_env(:skate, application_key, value)
-      end
-    end
-
-    :ok
-  end
-
   # Tell Phoenix to update the endpoint configuration
   # whenever the application is updated.
+  @impl true
   @spec config_change(any, any, any) :: :ok
   def config_change(changed, _new, removed) do
     SkateWeb.Endpoint.config_change(changed, removed)
     :ok
   end
 
-  @spec get_config_string(atom) :: String.t() | nil
-  def get_config_string(name) do
-    case Application.get_env(:skate, name) do
-      {:system, env_var} -> System.get_env(env_var)
-      value -> value
+  @doc """
+  Check system environment variables at runtime and load them into the application environment.
+
+  Will recursively check the keys below in the application config for any {:system, "ENVIRONMENT_VARIABLE"},
+  and replace them with the value in the given environment variable.
+  """
+  @spec load_runtime_config() :: :ok
+  def load_runtime_config() do
+    application_keys = [
+      :gtfs_url,
+      :busloc_url,
+      :swiftly_authorization_key,
+      :swiftly_realtime_vehicles_url,
+      SkateWeb.Endpoint
+    ]
+
+    for application_key <- application_keys do
+      Application.put_env(
+        :skate,
+        application_key,
+        runtime_config(Application.get_env(:skate, application_key))
+      )
     end
+
+    :ok
   end
 
-  @spec update_static_url(list(tuple())) :: list(tuple())
-  def update_static_url([{:static_url, static_url_parts} | rest]) do
-    static_url_parts =
-      Enum.map(static_url_parts, fn {key, value} -> {key, update_static_url_part(value)} end)
+  @spec runtime_config(any()) :: any()
+  defp runtime_config(list) when is_list(list) do
+    Enum.map(list, fn elem ->
+      case elem do
+        {key, value} when is_atom(key) ->
+          {key, runtime_config(value)}
 
-    [{:static_url, static_url_parts} | update_static_url(rest)]
+        _ ->
+          elem
+      end
+    end)
   end
 
-  def update_static_url([first | rest]) do
-    [first | update_static_url(rest)]
+  defp runtime_config({:system, environment_variable}) do
+    System.get_env(environment_variable)
   end
 
-  def update_static_url([]) do
-    []
+  defp runtime_config(value) do
+    value
   end
-
-  defp update_static_url_part({:system, env_var}), do: System.get_env(env_var)
-  defp update_static_url_part(value), do: value
 end
