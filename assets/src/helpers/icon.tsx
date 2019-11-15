29fdbc544db7836cb5b3725b0b74de20b5f2f495
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2019-11-15 11:38:49 -0500

    Add subway line and CR icons

--- assets/src/helpers/icon.tsx
+++ assets/src/helpers/icon.tsx
@@ -1,4 +1,6 @@
 // @ts-ignore
+import blueLineIconSvg from "../../static/images/icon-blue-line.svg"
+// @ts-ignore
 import collapseIconSvg from "../../static/images/icon-caret-left.svg"
 // @ts-ignore
 import expandIconSvg from "../../static/images/icon-caret-right.svg"
@@ -7,9 +9,27 @@ import circleXIconSvg from "../../static/images/icon-circle-x.svg"
 // @ts-ignore
 import closeIconSvg from "../../static/images/icon-close-x.svg"
 // @ts-ignore
+import commuterRailIconSvg from "../../static/images/icon-commuter-rail.svg"
+// @ts-ignore
+import greenLineBIconSvg from "../../static/images/icon-green-line-b.svg"
+// @ts-ignore
+import greenLineCIconSvg from "../../static/images/icon-green-line-c.svg"
+// @ts-ignore
+import greenLineDIconSvg from "../../static/images/icon-green-line-d.svg"
+// @ts-ignore
+import greenLineEIconSvg from "../../static/images/icon-green-line-e.svg"
+// @ts-ignore
+import greenLineIconSvg from "../../static/images/icon-green-line.svg"
+// @ts-ignore
 import ladderIconSvg from "../../static/images/icon-ladder.svg"
 // @ts-ignore
 import mapIconSvg from "../../static/images/icon-map.svg"
+// @ts-ignore
+import mattapanLineIconSvg from "../../static/images/icon-mattapan-line.svg"
+// @ts-ignore
+import orangeLineIconSvg from "../../static/images/icon-orange-line.svg"
+// @ts-ignore
+import redLineIconSvg from "../../static/images/icon-red-line.svg"
 // @ts-ignore
 import reverseIconSvg from "../../static/images/icon-reverse-normal.svg"
 // @ts-ignore
@@ -18,19 +38,49 @@ import reverseIconReversedSvg from "../../static/images/icon-reverse-reversed.sv
 import searchIconSvg from "../../static/images/icon-search.svg"
 import renderSvg from "./renderSvg"
 
+export const blueLineIcon = (className: string = ""): JSX.Element =>
+  renderSvg(className, blueLineIconSvg)
+
 export const circleXIcon = (): JSX.Element => renderSvg("", circleXIconSvg)
 
 export const closeIcon = (): JSX.Element => renderSvg("", closeIconSvg)
 
 export const collapseIcon = (): JSX.Element => renderSvg("", collapseIconSvg)
 
+export const commuterRailIcon = (className: string = ""): JSX.Element =>
+  renderSvg(className, commuterRailIconSvg)
+
 export const expandIcon = (): JSX.Element => renderSvg("", expandIconSvg)
+
+export const greenLineBIcon = (className: string = ""): JSX.Element =>
+  renderSvg(className, greenLineBIconSvg)
+
+export const greenLineCIcon = (className: string = ""): JSX.Element =>
+  renderSvg(className, greenLineCIconSvg)
+
+export const greenLineDIcon = (className: string = ""): JSX.Element =>
+  renderSvg(className, greenLineDIconSvg)
+
+export const greenLineEIcon = (className: string = ""): JSX.Element =>
+  renderSvg(className, greenLineEIconSvg)
+
+export const greenLineIcon = (className: string = ""): JSX.Element =>
+  renderSvg(className, greenLineIconSvg)
 
 export const ladderIcon = (className: string = ""): JSX.Element =>
   renderSvg(className, ladderIconSvg)
 
 export const mapIcon = (className: string = ""): JSX.Element =>
   renderSvg(className, mapIconSvg)
+
+export const mattapanLineIcon = (className: string = ""): JSX.Element =>
+  renderSvg(className, mattapanLineIconSvg)
+
+export const orangeLineIcon = (className: string = ""): JSX.Element =>
+  renderSvg(className, orangeLineIconSvg)
+
+export const redLineIcon = (className: string = ""): JSX.Element =>
+  renderSvg(className, redLineIconSvg)
 
 export const reverseIcon = (className: string = ""): JSX.Element =>
   renderSvg(className, reverseIconSvg)
--- assets/tests/helpers/icon.test.tsx
+++ assets/tests/helpers/icon.test.tsx
@@ -1,10 +1,54 @@
 import React from "react"
 import {
+  blueLineIcon,
   closeIcon,
+  commuterRailIcon,
+  greenLineBIcon,
+  greenLineCIcon,
+  greenLineDIcon,
+  greenLineEIcon,
+  greenLineIcon,
   ladderIcon,
   mapIcon,
+  mattapanLineIcon,
+  orangeLineIcon,
+  redLineIcon,
   reverseIcon,
 } from "../../src/helpers/icon"
+
+describe("blueLineIcon", () => {
+  it("renders an icon with a class name", () => {
+    const className = "test-class-name"
+
+    const expected = (
+      <span
+        className={className}
+        dangerouslySetInnerHTML={{
+          __html: "SVG",
+        }}
+      />
+    )
+
+    const result = blueLineIcon(className)
+
+    expect(result).toEqual(expected)
+  })
+
+  it("renders without a class name", () => {
+    const expected = (
+      <span
+        className=""
+        dangerouslySetInnerHTML={{
+          __html: "SVG",
+        }}
+      />
+    )
+
+    const result = blueLineIcon()
+
+    expect(result).toEqual(expected)
+  })
+})
 
 describe("closeIcon", () => {
   it("renders", () => {
@@ -23,8 +67,212 @@ describe("closeIcon", () => {
   })
 })
 
+describe("commuterRailIcon", () => {
+  it("renders an icon with a class name", () => {
+    const className = "test-class-name"
+
+    const expected = (
+      <span
+        className={className}
+        dangerouslySetInnerHTML={{
+          __html: "SVG",
+        }}
+      />
+    )
+
+    const result = commuterRailIcon(className)
+
+    expect(result).toEqual(expected)
+  })
+
+  it("renders without a class name", () => {
+    const expected = (
+      <span
+        className=""
+        dangerouslySetInnerHTML={{
+          __html: "SVG",
+        }}
+      />
+    )
+
+    const result = commuterRailIcon()
+
+    expect(result).toEqual(expected)
+  })
+})
+
+describe("greenLineBIcon", () => {
+  it("renders an icon with a class name", () => {
+    const className = "test-class-name"
+
+    const expected = (
+      <span
+        className={className}
+        dangerouslySetInnerHTML={{
+          __html: "SVG",
+        }}
+      />
+    )
+
+    const result = greenLineBIcon(className)
+
+    expect(result).toEqual(expected)
+  })
+
+  it("renders without a class name", () => {
+    const expected = (
+      <span
+        className=""
+        dangerouslySetInnerHTML={{
+          __html: "SVG",
+        }}
+      />
+    )
+
+    const result = greenLineBIcon()
+
+    expect(result).toEqual(expected)
+  })
+})
+
+describe("greenLineCIcon", () => {
+  it("renders an icon with a class name", () => {
+    const className = "test-class-name"
+
+    const expected = (
+      <span
+        className={className}
+        dangerouslySetInnerHTML={{
+          __html: "SVG",
+        }}
+      />
+    )
+
+    const result = greenLineCIcon(className)
+
+    expect(result).toEqual(expected)
+  })
+
+  it("renders without a class name", () => {
+    const expected = (
+      <span
+        className=""
+        dangerouslySetInnerHTML={{
+          __html: "SVG",
+        }}
+      />
+    )
+
+    const result = greenLineCIcon()
+
+    expect(result).toEqual(expected)
+  })
+})
+
+describe("greenLineDIcon", () => {
+  it("renders an icon with a class name", () => {
+    const className = "test-class-name"
+
+    const expected = (
+      <span
+        className={className}
+        dangerouslySetInnerHTML={{
+          __html: "SVG",
+        }}
+      />
+    )
+
+    const result = greenLineDIcon(className)
+
+    expect(result).toEqual(expected)
+  })
+
+  it("renders without a class name", () => {
+    const expected = (
+      <span
+        className=""
+        dangerouslySetInnerHTML={{
+          __html: "SVG",
+        }}
+      />
+    )
+
+    const result = greenLineDIcon()
+
+    expect(result).toEqual(expected)
+  })
+})
+
+describe("greenLineEIcon", () => {
+  it("renders an icon with a class name", () => {
+    const className = "test-class-name"
+
+    const expected = (
+      <span
+        className={className}
+        dangerouslySetInnerHTML={{
+          __html: "SVG",
+        }}
+      />
+    )
+
+    const result = greenLineEIcon(className)
+
+    expect(result).toEqual(expected)
+  })
+
+  it("renders without a class name", () => {
+    const expected = (
+      <span
+        className=""
+        dangerouslySetInnerHTML={{
+          __html: "SVG",
+        }}
+      />
+    )
+
+    const result = greenLineEIcon()
+
+    expect(result).toEqual(expected)
+  })
+})
+
+describe("greenLineIcon", () => {
+  it("renders an icon with a class name", () => {
+    const className = "test-class-name"
+
+    const expected = (
+      <span
+        className={className}
+        dangerouslySetInnerHTML={{
+          __html: "SVG",
+        }}
+      />
+    )
+
+    const result = greenLineIcon(className)
+
+    expect(result).toEqual(expected)
+  })
+
+  it("renders without a class name", () => {
+    const expected = (
+      <span
+        className=""
+        dangerouslySetInnerHTML={{
+          __html: "SVG",
+        }}
+      />
+    )
+
+    const result = greenLineIcon()
+
+    expect(result).toEqual(expected)
+  })
+})
+
 describe("ladderIcon", () => {
-  it("renders an accessibility icon with a class name", () => {
+  it("renders an icon with a class name", () => {
     const className = "test-class-name"
 
     const expected = (
@@ -58,7 +306,7 @@ describe("ladderIcon", () => {
 })
 
 describe("mapIcon", () => {
-  it("renders an accessibility icon with a class name", () => {
+  it("renders an icon with a class name", () => {
     const className = "test-class-name"
 
     const expected = (
@@ -91,8 +339,110 @@ describe("mapIcon", () => {
   })
 })
 
+describe("mattapanLineIcon", () => {
+  it("renders an icon with a class name", () => {
+    const className = "test-class-name"
+
+    const expected = (
+      <span
+        className={className}
+        dangerouslySetInnerHTML={{
+          __html: "SVG",
+        }}
+      />
+    )
+
+    const result = mattapanLineIcon(className)
+
+    expect(result).toEqual(expected)
+  })
+
+  it("renders without a class name", () => {
+    const expected = (
+      <span
+        className=""
+        dangerouslySetInnerHTML={{
+          __html: "SVG",
+        }}
+      />
+    )
+
+    const result = mattapanLineIcon()
+
+    expect(result).toEqual(expected)
+  })
+})
+
+describe("orangeLineIcon", () => {
+  it("renders an icon with a class name", () => {
+    const className = "test-class-name"
+
+    const expected = (
+      <span
+        className={className}
+        dangerouslySetInnerHTML={{
+          __html: "SVG",
+        }}
+      />
+    )
+
+    const result = orangeLineIcon(className)
+
+    expect(result).toEqual(expected)
+  })
+
+  it("renders without a class name", () => {
+    const expected = (
+      <span
+        className=""
+        dangerouslySetInnerHTML={{
+          __html: "SVG",
+        }}
+      />
+    )
+
+    const result = orangeLineIcon()
+
+    expect(result).toEqual(expected)
+  })
+})
+
+describe("redLineIcon", () => {
+  it("renders an icon with a class name", () => {
+    const className = "test-class-name"
+
+    const expected = (
+      <span
+        className={className}
+        dangerouslySetInnerHTML={{
+          __html: "SVG",
+        }}
+      />
+    )
+
+    const result = redLineIcon(className)
+
+    expect(result).toEqual(expected)
+  })
+
+  it("renders without a class name", () => {
+    const expected = (
+      <span
+        className=""
+        dangerouslySetInnerHTML={{
+          __html: "SVG",
+        }}
+      />
+    )
+
+    const result = redLineIcon()
+
+    expect(result).toEqual(expected)
+  })
+})
+
 describe("reverseIcon", () => {
-  it("renders an accessibility icon with a class name", () => {
+  it("renders an icon with a class name", () => {
     const className = "test-class-name"
 
     const expected = (
