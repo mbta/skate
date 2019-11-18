84cd34096c5890f9eeb4bd55156c163bfd8863c0
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2019-11-18 11:26:05 -0500

    Add a map to the search results page
    Display all of the search results (excluding ghosts) as live vehicles.

--- assets/tests/components/searchPage.test.tsx
+++ assets/tests/components/searchPage.test.tsx
@@ -1,10 +1,107 @@
 import React from "react"
 import renderer from "react-test-renderer"
 import SearchPage from "../../src/components/searchPage"
+import { StateDispatchProvider } from "../../src/contexts/stateDispatchContext"
+import { HeadwaySpacing } from "../../src/models/vehicleStatus"
+import { Ghost, Vehicle, VehicleOrGhost } from "../../src/realtime"
+import { initialState } from "../../src/state"
+
+const vehicle: Vehicle = {
+  id: "v1",
+  label: "v1-label",
+  runId: "run-1",
+  timestamp: 123,
+  latitude: 0,
+  longitude: 0,
+  directionId: 0,
+  routeId: "39",
+  tripId: "t1",
+  headsign: "Forest Hills",
+  viaVariant: "X",
+  operatorId: "op1",
+  operatorName: "SMITH",
+  bearing: 33,
+  blockId: "block-1",
+  headwaySecs: 859.1,
+  headwaySpacing: HeadwaySpacing.Ok,
+  previousVehicleId: "v2",
+  scheduleAdherenceSecs: 0,
+  scheduleAdherenceString: "0.0 sec (ontime)",
+  scheduledHeadwaySecs: 120,
+  isOffCourse: false,
+  isLayingOver: false,
+  layoverDepartureTime: null,
+  blockIsActive: false,
+  dataDiscrepancies: [
+    {
+      attribute: "trip_id",
+      sources: [
+        {
+          id: "swiftly",
+          value: "swiftly-trip-id",
+        },
+        {
+          id: "busloc",
+          value: "busloc-trip-id",
+        },
+      ],
+    },
+  ],
+  stopStatus: {
+    stopId: "s1",
+    stopName: "Stop Name",
+  },
+  timepointStatus: {
+    timepointId: "tp1",
+    fractionUntilTimepoint: 0.5,
+  },
+  scheduledLocation: null,
+  isOnRoute: true,
+}
+const ghost: Ghost = {
+  id: "ghost-trip",
+  directionId: 0,
+  routeId: "39",
+  tripId: "trip",
+  headsign: "headsign",
+  blockId: "block",
+  runId: "123-0123",
+  viaVariant: "X",
+  scheduledTimepointStatus: {
+    timepointId: "t0",
+    fractionUntilTimepoint: 0.0,
+  },
+}
+jest.mock("../../src/hooks/useSearchResults", () => ({
+  __esModule: true,
+  default: jest
+    .fn()
+    // Ipmlementation sequence matches tests
+    .mockImplementationOnce(() => null)
+    .mockImplementation(() => [vehicle, ghost] as VehicleOrGhost[]),
+}))
 
 describe("SearchPage", () => {
-  test("renders", () => {
-    const tree = renderer.create(<SearchPage />).toJSON()
+  test("renders the empty state", () => {
+    const tree = renderer
+      .create(
+        <StateDispatchProvider state={initialState} dispatch={jest.fn()}>
+          <SearchPage />
+        </StateDispatchProvider>
+      )
+      .toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  test("renders vehicle data", () => {
+    const tree = renderer
+      .create(
+        <StateDispatchProvider state={initialState} dispatch={jest.fn()}>
+          <SearchPage />
+        </StateDispatchProvider>
+      )
+      .toJSON()
 
     expect(tree).toMatchSnapshot()
   })
