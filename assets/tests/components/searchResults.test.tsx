fa8ab7b99d1b727059673baefcf0463a4739c599
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2019-11-21 13:28:58 -0500

    Add properties panel to the search page (#313)
    Allow the user to click on the result card.

--- assets/tests/components/searchResults.test.tsx
+++ assets/tests/components/searchResults.test.tsx
@@ -6,7 +6,7 @@ import { StateDispatchProvider } from "../../src/contexts/stateDispatchContext"
 import { setSearchText } from "../../src/models/search"
 import { HeadwaySpacing } from "../../src/models/vehicleStatus"
 import { Ghost, Vehicle } from "../../src/realtime"
-import { initialState, State } from "../../src/state"
+import { initialState, selectVehicle, State } from "../../src/state"
 
 const state: State = {
   ...initialState,
@@ -106,6 +106,153 @@ describe("SearchResults", () => {
       .toJSON()
 
     expect(tree).toMatchSnapshot()
+  })
+
+  test("renders a selected result card", () => {
+    const vehicle: Vehicle = {
+      id: "v1",
+      label: "v1-label",
+      runId: "run-1",
+      timestamp: 123,
+      latitude: 0,
+      longitude: 0,
+      directionId: 0,
+      routeId: "39",
+      tripId: "t1",
+      headsign: "Forest Hills",
+      viaVariant: "X",
+      operatorId: "op1",
+      operatorName: "SMITH",
+      bearing: 33,
+      blockId: "block-1",
+      headwaySecs: 859.1,
+      headwaySpacing: HeadwaySpacing.Ok,
+      previousVehicleId: "v2",
+      scheduleAdherenceSecs: 0,
+      scheduleAdherenceString: "0.0 sec (ontime)",
+      scheduledHeadwaySecs: 120,
+      isOffCourse: false,
+      layoverDepartureTime: null,
+      blockIsActive: false,
+      dataDiscrepancies: [
+        {
+          attribute: "trip_id",
+          sources: [
+            {
+              id: "swiftly",
+              value: "swiftly-trip-id",
+            },
+            {
+              id: "busloc",
+              value: "busloc-trip-id",
+            },
+          ],
+        },
+      ],
+      stopStatus: {
+        stopId: "s1",
+        stopName: "Stop Name",
+      },
+      timepointStatus: {
+        fractionUntilTimepoint: 0.5,
+        timepointId: "tp1",
+      },
+      scheduledLocation: null,
+      routeStatus: "on_route",
+    }
+    const ghost: Ghost = {
+      id: "ghost-trip",
+      directionId: 0,
+      routeId: "39",
+      tripId: "trip",
+      headsign: "headsign",
+      blockId: "block",
+      runId: "123-0123",
+      viaVariant: "X",
+      scheduledTimepointStatus: {
+        timepointId: "t0",
+        fractionUntilTimepoint: 0.0,
+      },
+    }
+
+    const stateWithSelected = {
+      ...state,
+      selectedVehicleId: vehicle.id,
+    }
+
+    const tree = renderer
+      .create(
+        <StateDispatchProvider state={stateWithSelected} dispatch={jest.fn()}>
+          <SearchResults vehicles={[vehicle, ghost]} />
+        </StateDispatchProvider>
+      )
+      .toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  test("clicking a result card selects that vehicle", () => {
+    const testDispatch = jest.fn()
+    const vehicle: Vehicle = {
+      id: "v1",
+      label: "v1-label",
+      runId: "run-1",
+      timestamp: 123,
+      latitude: 0,
+      longitude: 0,
+      directionId: 0,
+      routeId: "39",
+      tripId: "t1",
+      headsign: "Forest Hills",
+      viaVariant: "X",
+      operatorId: "op1",
+      operatorName: "SMITH",
+      bearing: 33,
+      blockId: "block-1",
+      headwaySecs: 859.1,
+      headwaySpacing: HeadwaySpacing.Ok,
+      previousVehicleId: "v2",
+      scheduleAdherenceSecs: 0,
+      scheduleAdherenceString: "0.0 sec (ontime)",
+      scheduledHeadwaySecs: 120,
+      isOffCourse: false,
+      layoverDepartureTime: null,
+      blockIsActive: false,
+      dataDiscrepancies: [
+        {
+          attribute: "trip_id",
+          sources: [
+            {
+              id: "swiftly",
+              value: "swiftly-trip-id",
+            },
+            {
+              id: "busloc",
+              value: "busloc-trip-id",
+            },
+          ],
+        },
+      ],
+      stopStatus: {
+        stopId: "s1",
+        stopName: "Stop Name",
+      },
+      timepointStatus: {
+        timepointId: "tp1",
+        fractionUntilTimepoint: 0.5,
+      },
+      scheduledLocation: null,
+      routeStatus: "on_route",
+    }
+    const wrapper = mount(
+      <StateDispatchProvider state={state} dispatch={testDispatch}>
+        <SearchResults vehicles={[vehicle]} />
+      </StateDispatchProvider>
+    )
+
+    wrapper.find(".m-search-results__card").simulate("click")
+
+    expect(testDispatch).toHaveBeenCalledWith(selectVehicle("v1"))
   })
 
   test("clicking the clear search button empties the search text", () => {
