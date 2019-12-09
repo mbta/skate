efad0c24e4ed086a8c0d4815fbafafcc927c8a2a
Sky Rose <srose@mbta.com>
2019-12-09 16:39:04 -0500

    rename test names

--- assets/tests/models/searchQuery.test.ts
+++ assets/tests/models/searchQuery.test.ts
@@ -1,7 +1,7 @@
 import { SearchQuery, isValidSearchQuery } from "../../src/models/searchQuery"
 
 describe("isValidSearchQuery", () => {
-  test("returns true if the search text contains at least 2 characters", () => {
+  test("returns true if the query text contains at least 2 characters", () => {
     const validQuery: SearchQuery = {
       text: "12",
       property: "run",
@@ -10,7 +10,7 @@ describe("isValidSearchQuery", () => {
     expect(isValidSearchQuery(validQuery)).toBeTruthy()
   })
 
-  test("returns false if the search text contains fewer than 2 characters", () => {
+  test("returns false if the query text contains fewer than 2 characters", () => {
     const invalidQuery: SearchQuery = {
       text: "1",
       property: "run",
@@ -19,7 +19,7 @@ describe("isValidSearchQuery", () => {
     expect(isValidSearchQuery(invalidQuery)).toBeFalsy()
   })
 
-  test("returns false if the search contains more than 2 characters but they're not alphanumeric", () => {
+  test("returns false if the query contains more than 2 characters but they're not alphanumeric", () => {
     const invalidQuery: SearchQuery = {
       text: " -1 -",
       property: "run",
