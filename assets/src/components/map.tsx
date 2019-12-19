c4efa71b3b407dcbb31b55de8068714235cc03b5
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2019-12-19 16:30:52 -0500

    Use existing class instead

--- assets/src/components/map.tsx
+++ assets/src/components/map.tsx
@@ -63,7 +63,7 @@ const makeVehicleIcon = (vehicle: Vehicle): Leaflet.DivIcon => {
         xmlns="http://www.w3.org/2000/svg"
       >
         <path
-          class="m-vehicle-icon__triangle ${statusClass(drawnStatus(vehicle))}"
+          class="${statusClass(drawnStatus(vehicle))}"
           d="m10 2.7-6.21 16.94a2.33 2.33 0 0 0 1.38 3 2.36 2.36 0 0 0 1.93-.14l4.9-2.67 4.89 2.71a2.34 2.34 0 0 0 3.34-2.8l-5.81-17a2.34 2.34 0 0 0 -4.4 0z"
           transform="rotate(${vehicle.bearing}, ${centerX}, ${centerY})"
         />
