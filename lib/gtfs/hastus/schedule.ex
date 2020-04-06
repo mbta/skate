606feef5f79fc6eb1fc7abc2c2ba3ea3405637e4
Sky Rose <srose@mbta.com>
2020-04-06 11:43:01 -0400

    hastus.schedule moduledoc

--- lib/gtfs/hastus/schedule.ex
+++ lib/gtfs/hastus/schedule.ex
@@ -1,3 +1,9 @@
 defmodule Gtfs.Hastus.Schedule do
+  @moduledoc """
+  A HASTUS schedule id refers to the same idea as a GTFS service id:
+  a particular version of service that determines what trips run on a day:
+  But the ids are incompatible between HASTUS and GTFS,
+  so they get different types so it's clear they aren't interchangable.
+  """
   @type id :: String.t()
 end
