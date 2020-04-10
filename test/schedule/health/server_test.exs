a5d880bc120696281846696c2b1960d7b61bda33
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2020-04-10 10:17:32 -0400

    Make alias nicer

--- test/schedule/health/server_test.exs
+++ test/schedule/health/server_test.exs
@@ -2,39 +2,39 @@ defmodule Schedule.Health.ServerTest do
   use ExUnit.Case
   import Test.Support.Helpers
 
-  alias Schedule.Health
+  alias Schedule.Health.Server, as: HealthServer
 
   test "becomes ready after calling loaded and checker reports healthy" do
     reassign_env(:skate, :checker_healthy_fn, fn -> true end)
 
-    pid = Health.Server.start_mocked()
-    refute Health.Server.ready?(pid)
-    Health.Server.loaded(pid)
+    pid = HealthServer.start_mocked()
+    refute HealthServer.ready?(pid)
+    HealthServer.loaded(pid)
 
-    assert Health.Server.ready?(pid)
+    assert HealthServer.ready?(pid)
   end
 
   test "not ready after calling loaded if checker reports not healthy" do
     reassign_env(:skate, :checker_healthy_fn, fn -> false end)
 
-    pid = Health.Server.start_mocked()
-    Health.Server.loaded(pid)
-    refute Health.Server.ready?(pid)
+    pid = HealthServer.start_mocked()
+    HealthServer.loaded(pid)
+    refute HealthServer.ready?(pid)
   end
 
   test "gtfs server tells the health server it's loaded" do
     reassign_env(:skate, :checker_healthy_fn, fn -> true end)
-    health_server_pid = Health.Server.start_mocked()
-    refute Health.Server.ready?(health_server_pid)
+    health_server_pid = HealthServer.start_mocked()
+    refute HealthServer.ready?(health_server_pid)
     gtfs_pid = Schedule.start_mocked(%{}, health_server_pid)
     # Make a synchronous call to make sure the data is loaded
     Schedule.all_routes(gtfs_pid)
-    assert Health.Server.ready?(health_server_pid)
+    assert HealthServer.ready?(health_server_pid)
   end
 
   test "can start in supervision tree with default name" do
-    start_supervised({Schedule.Health.Server, []})
+    start_supervised({HealthServer, []})
     # No Schedule server points to this health server, so it won't be ready
-    refute Health.Server.ready?(Health.Server.default_server())
+    refute HealthServer.ready?(HealthServer.default_server())
   end
 end
