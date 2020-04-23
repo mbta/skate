10676e850bcc14a29235d43258739533722ef15c
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2020-04-23 10:54:35 -0400

    Update semaphore setup
    Stolen from screens.

--- semaphore/setup.sh
+++ semaphore/setup.sh
@@ -2,13 +2,20 @@
 set -e
 
 ELIXIR_VERSION=1.9.4
-ERLANG_VERSION=22
+ERLANG_VERSION=22.3.2
 NODEJS_VERSION=10.15
 
 change-phantomjs-version 2.1.1
 nvm use $NODEJS_VERSION
 
-. /home/runner/.kerl/installs/$ERLANG_VERSION/activate
+export ERL_HOME="${SEMAPHORE_CACHE_DIR}/.kerl/installs/${ERLANG_VERSION}"
+if [ ! -d "${ERL_HOME}" ]; then
+    mkdir -p "${ERL_HOME}"
+    KERL_BUILD_BACKEND=git kerl build $ERLANG_VERSION $ERLANG_VERSION
+    kerl install $ERLANG_VERSION $ERL_HOME
+fi
+. $ERL_HOME/activate
+
 kiex use $ELIXIR_VERSION || kiex install $ELIXIR_VERSION && kiex use $ELIXIR_VERSION
 mix local.hex --force
 mix local.rebar --force
