3026b05d49be4b94accb979f644a8199d448f8ab
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2020-04-30 14:45:45 -0400

    Configure existing secret keys for SecretsManager
    Use SecretsManager in production, continue to use system environment
    variables elsewhere.
    
    Add api variables.

--- config/prod.exs
+++ config/prod.exs
@@ -1,8 +1,13 @@
 use Mix.Config
 
 config :skate,
+  api_key: {:secret, "API_KEY"},
   redirect_http?: true,
-  static_href: {SkateWeb.Router.Helpers, :static_url}
+  record_fullstory: true,
+  record_appcues: true,
+  secret_key_base: SecretsManager.fetch!("SECRET_KEY_BASE"),
+  static_href: {SkateWeb.Router.Helpers, :static_url},
+  swiftly_authorization_key: {:secret, "SWIFTLY_AUTHORIZATION_KEY"}
 
 # For production, don't forget to configure the url host
 # to something meaningful, Phoenix uses this information
@@ -46,15 +51,11 @@ config :ueberauth, Ueberauth,
 config :ueberauth, Ueberauth.Strategy.Cognito,
   auth_domain: {System, :get_env, ["COGNITO_DOMAIN"]},
   client_id: {System, :get_env, ["COGNITO_CLIENT_ID"]},
-  client_secret: {System, :get_env, ["COGNITO_CLIENT_SECRET"]},
+  client_secret: SecretsManager.fetch!("COGNITO_CLIENT_SECRET"),
   user_pool_id: {System, :get_env, ["COGNITO_USER_POOL_ID"]},
   aws_region: {System, :get_env, ["COGNITO_AWS_REGION"]}
 
-config :skate, SkateWeb.AuthManager, secret_key: {System, :get_env, ["GUARDIAN_SECRET_KEY"]}
-
-config :skate,
-  record_fullstory: true,
-  record_appcues: true
+config :skate, SkateWeb.AuthManager, secret_key: SecretsManager.fetch!("GUARDIAN_SECRET_KEY")
 
 config :ex_aws, json_codec: Jason
 
