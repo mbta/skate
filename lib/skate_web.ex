3c65a6386edd53f4fd16164e7b9fb4c248075934
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2020-05-06 11:58:35 -0400

    Switch away from deprecated Phoenix-specific assigns
    Instead of :view_module and :view_template use view_module(@conn) and
    view_template(@conn).
    
    See:
    https://github.com/phoenixframework/phoenix_html/blob/master/CHANGELOG.md#v2142-2020-04-30

--- lib/skate_web.ex
+++ lib/skate_web.ex
@@ -33,7 +33,8 @@ defmodule SkateWeb do
         namespace: SkateWeb
 
       # Import convenience functions from controllers
-      import Phoenix.Controller, only: [get_flash: 1, get_flash: 2, view_module: 1]
+      import Phoenix.Controller,
+        only: [get_flash: 1, get_flash: 2, view_module: 1, view_template: 1]
 
       # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML
