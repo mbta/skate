4be36e1bd6d6874f199c2bcefaa76dcf9e9fda54
Sky Rose <srose@mbta.com>
2020-05-07 10:22:20 -0400

    list of breaks and trips

--- assets/src/components/propertiesPanel/minischedule.tsx
+++ assets/src/components/propertiesPanel/minischedule.tsx
@@ -1,33 +1,68 @@
-import React from "react"
+import React, { ReactElement } from "react"
 import {
   useMinischeduleBlock,
   useMinischeduleRun,
 } from "../../hooks/useMinischedule"
-import { Block, Run } from "../../minischedule"
+import { Block, Break, Piece, Run, Trip } from "../../minischedule"
 import { TripId } from "../../schedule"
 import Loading from "../loading"
 
 export interface Props {
   activeTripId: TripId
 }
-export const MinischeduleRun = ({ activeTripId }: Props) => {
+export const MinischeduleRun = ({ activeTripId }: Props): ReactElement => {
   const run: Run | null | undefined = useMinischeduleRun(activeTripId)
   if (run === undefined) {
     return <Loading />
   } else if (run === null) {
     return <>No run found</>
   } else {
-    return <div>{JSON.stringify(run)}</div>
+    return (
+      <>
+        {run.activities.map((activity) =>
+          isPiece(activity) ? (
+            <Piece piece={activity} key={activity.start.time} />
+          ) : (
+            <Break break={activity} key={activity.startTime} />
+          )
+        )}
+      </>
+    )
   }
 }
 
-export const MinischeduleBlock = ({ activeTripId }: Props) => {
+export const MinischeduleBlock = ({ activeTripId }: Props): ReactElement => {
   const block: Block | null | undefined = useMinischeduleBlock(activeTripId)
   if (block === undefined) {
     return <Loading />
   } else if (block === null) {
     return <>No block found</>
   } else {
-    return <div>{JSON.stringify(block)}</div>
+    return (
+      <>
+        {block.pieces.map((piece) => (
+          <Piece piece={piece} key={piece.start.time} />
+        ))}
+      </>
+    )
   }
 }
+
+const Break = ({ break: breakk }: { break: Break }) => (
+  <div className="m-minischedule__row">{JSON.stringify(breakk)}</div>
+)
+
+const Piece = ({ piece }: { piece: Piece }) => (
+  <>
+    {piece.trips.map((trip) => (
+      <Trip trip={trip} key={trip.id} />
+    ))}
+  </>
+)
+
+const Trip = ({ trip }: { trip: Trip }) => (
+  <div className="m-minischedule__row">{JSON.stringify(trip)}</div>
+)
+
+const isPiece = (activity: Piece | Break): activity is Piece =>
+  activity.hasOwnProperty("trips")
