a22dcf9bf8c5c5dc30c1ebf19f02866664c7d061
Sky Rose <srose@mbta.com>
2020-05-14 10:56:44 -0400

    ts-nocheck in test setup

--- assets/tests/setup.tsx
+++ assets/tests/setup.tsx
@@ -1,4 +1,4 @@
-// setup file
+// @ts-nocheck
 import { configure } from "enzyme"
 import React from "react"
 import Adapter from "enzyme-adapter-react-16"
@@ -15,19 +15,16 @@ jest.mock("react-tooltip", () => ({
 // JSDOM doesn't support part of SVG that's needed for Leaflet to run in tests.
 // https://stackoverflow.com/questions/54382414/fixing-react-leaflet-testing-error-cannot-read-property-layeradd-of-null
 const createElementNSOrig = document.createElementNS
-// @ts-ignore
 // tslint:disable-next-line only-arrow-functions
 document.createElementNS = function (namespaceURI, qualifiedName) {
   if (
     namespaceURI === "http://www.w3.org/2000/svg" &&
     qualifiedName === "svg"
   ) {
-    // @ts-ignore
     const element = createElementNSOrig.apply(this, arguments)
     // tslint:disable-next-line no-empty
     element.createSVGRect = () => {}
     return element
   }
-  // @ts-ignore
   return createElementNSOrig.apply(this, arguments)
 }
