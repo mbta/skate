b87ccaafd413498d46b5a9176a3c4fdfce59a542
Phil Darnowsky <pdarnowsky@mbta.com>
2020-07-14 08:50:51 -0400

    Improve tappability of crowding icons

--- assets/src/components/crowdingIcon.tsx
+++ assets/src/components/crowdingIcon.tsx
@@ -48,6 +48,7 @@ const Crowd = ({ size, orientation, occupancyStatus, isLayingOver }: Props) => {
       transform={`scale(${scale}) translate(-24,${yOffset - 22})`}
       className={className(classNames)}
     >
+      <rect x="-2" y="-2" width="52" height="52" fill="transparent" />
       <path d="M6.74,16.17A6.86,6.86,0,0,0-.12,23h0V40.8a3.43,3.43,0,0,0,3.43,3.42h6.86a3.42,3.42,0,0,0,3.42-3.42V23A6.87,6.87,0,0,0,6.74,16.17Z" />
       <circle cx="6.74" cy="8.54" r="4.93" />
       <path d="M23.88,16.17A6.86,6.86,0,0,0,17,23h0V40.8a3.43,3.43,0,0,0,3.43,3.42h6.86a3.43,3.43,0,0,0,3.43-3.42V23a6.86,6.86,0,0,0-6.86-6.86Z" />
