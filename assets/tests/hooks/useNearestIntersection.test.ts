f4be815ba3915bbb0d242a8dfa6c1c6f3fd627bc
Sky Rose <srose@mbta.com>
2020-07-16 10:57:00 -0400

    add tests for useNearestIntersection

---
+++ assets/tests/hooks/useNearestIntersection.test.ts
@@ -0,0 +1,41 @@
+import { renderHook } from "@testing-library/react-hooks"
+import * as Api from "../../src/api"
+import { useNearestIntersection } from "../../src/hooks/useNearestIntersection"
+import { neverPromise } from "../../tests/testHelpers/mockHelpers"
+
+// tslint:disable: react-hooks-nesting
+
+jest.mock("../../src/api", () => ({
+  __esModule: true,
+  fetchNearestIntersection: jest.fn(() => neverPromise()),
+}))
+
+describe("useNearestIntersection", () => {
+  test("returns null when loading", () => {
+    const { result } = renderHook(() => useNearestIntersection(40, -70))
+    expect(result.current).toEqual(null)
+  })
+
+  test("makes an api call", () => {
+    const mockNearestIntersection: jest.Mock = Api.fetchNearestIntersection as jest.Mock
+    renderHook(() => useNearestIntersection(40, -70))
+    expect(mockNearestIntersection).toHaveBeenCalledTimes(1)
+  })
+
+  test("makes another api call if the input chnages", () => {
+    const mockNearestIntersection: jest.Mock = Api.fetchNearestIntersection as jest.Mock
+    const { rerender } = renderHook(
+      ({ lat, lng }) => useNearestIntersection(lat, lng),
+      { initialProps: { lat: 40, lng: -70 } }
+    )
+    rerender({ lat: 41, lng: -71 })
+    expect(mockNearestIntersection).toHaveBeenCalledTimes(2)
+  })
+
+  test("doesn't remake an api call if the input stays the same", () => {
+    const mockNearestIntersection: jest.Mock = Api.fetchNearestIntersection as jest.Mock
+    const { rerender } = renderHook(() => useNearestIntersection(40, -70))
+    rerender()
+    expect(mockNearestIntersection).toHaveBeenCalledTimes(1)
+  })
+})
