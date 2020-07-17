af282561959660f3be92e15066a42b7141c4b8e3
Sky Rose <srose@mbta.com>
2020-07-17 10:16:03 -0400

    remove io.inspect

--- lib/skate_web/controllers/intersection_controller.ex
+++ lib/skate_web/controllers/intersection_controller.ex
@@ -3,13 +3,10 @@ defmodule SkateWeb.IntersectionController do
 
   @spec intersection(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def intersection(conn, %{"latitude" => latitude, "longitude" => longitude}) do
-    IO.inspect({latitude, longitude}, label: :intersection_request)
-
     intersection_fn =
       Application.get_env(:skate, :intersection_fn, &Geonames.nearest_intersection/2)
 
     result = intersection_fn.(latitude, longitude)
-    IO.inspect(result, label: :intersection_result)
     json(conn, %{data: result})
   end
 end
