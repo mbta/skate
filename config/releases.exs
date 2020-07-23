0963f7ba69cc636a7243ca3e76cdb3fb24c731da
Sky Rose <srose@mbta.com>
2020-07-23 16:35:30 -0400

    update more secret key names

--- config/releases.exs
+++ config/releases.exs
@@ -8,9 +8,9 @@ Application.ensure_all_started(:ex_aws)
 Application.ensure_all_started(:ex_aws_secretsmanager)
 
 config :skate,
-  secret_key_base: SecretsManager.fetch!("SECRET_KEY_BASE")
+  secret_key_base: SecretsManager.fetch!("ENV-secret-key-base")
 
 config :ueberauth, Ueberauth.Strategy.Cognito,
-  client_secret: SecretsManager.fetch!("COGNITO_CLIENT_SECRET")
+  client_secret: SecretsManager.fetch!("ENV-cognito-client-secret")
 
-config :skate, SkateWeb.AuthManager, secret_key: SecretsManager.fetch!("GUARDIAN_SECRET_KEY")
+config :skate, SkateWeb.AuthManager, secret_key: SecretsManager.fetch!("ENV-guardian-secret-key")
