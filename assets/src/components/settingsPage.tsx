637bd45a0c13853ea2e3b2a120a71f66bbe21f98
Sky Rose <srose@mbta.com>
2020-08-07 09:55:11 -0400

    Allow settings page to shrink more responsively (#795)
    * move settings_page css rules to their own file
    
    * clean up settingsPage.tsx
    
    * adjust settings page flexbox sizes

--- assets/src/components/settingsPage.tsx
+++ assets/src/components/settingsPage.tsx
@@ -3,24 +3,9 @@ import { StateDispatchContext } from "../contexts/stateDispatchContext"
 import { ladderIcon, mapIcon } from "../helpers/icon"
 import { VehicleLabelSetting } from "../settings"
 import {
-  Dispatch,
   setLadderVehicleLabelSetting,
   setShuttleVehicleLabelSetting,
 } from "../state"
-
-const setLadderVehicleLabel = (dispatch: Dispatch) => (
-  event: React.FormEvent<HTMLSelectElement>
-) =>
-  dispatch(
-    setLadderVehicleLabelSetting(parseInt(event.currentTarget.value, 10))
-  )
-
-const setShuttleVehicleLabel = (dispatch: Dispatch) => (
-  event: React.FormEvent<HTMLSelectElement>
-) =>
-  dispatch(
-    setShuttleVehicleLabelSetting(parseInt(event.currentTarget.value, 10))
-  )
 
 const SettingsPage = (): ReactElement<HTMLDivElement> => {
   const [{ settings }, dispatch] = useContext(StateDispatchContext)
@@ -33,45 +18,71 @@ const SettingsPage = (): ReactElement<HTMLDivElement> => {
         <div className="c-page__section">
           <h2 className="c-page__header">Vehicle Label</h2>
 
-          <div className="c-page__section-row">
-            <div className="c-page__section-row-icon">
-              {ladderIcon("c-page__section-row-icon-path")}
-            </div>
-            <div className="c-page__section-row-label">Route Ladders</div>
-            <select
-              id="ladder-vehicle-label-setting"
-              className="c-page__select"
-              value={settings.ladderVehicleLabel}
-              onChange={setLadderVehicleLabel(dispatch)}
-            >
-              <option value={VehicleLabelSetting.RunNumber}>Run #</option>
-              <option value={VehicleLabelSetting.VehicleNumber}>
-                Vehicle #
-              </option>
-            </select>
-          </div>
-
-          <div className="c-page__section-row">
-            <div className="c-page__section-row-icon">
-              {mapIcon("c-page__section-row-icon-path")}
-            </div>
-            <div className="c-page__section-row-label">Map</div>
-            <select
-              id="map-vehicle-label-setting"
-              className="c-page__select"
-              value={settings.shuttleVehicleLabel}
-              onChange={setShuttleVehicleLabel(dispatch)}
-            >
-              <option value={VehicleLabelSetting.RunNumber}>Run #</option>
-              <option value={VehicleLabelSetting.VehicleNumber}>
-                Vehicle #
-              </option>
-            </select>
-          </div>
+          <DropdownSetting
+            icon={ladderIcon}
+            label="Route Ladders"
+            selectId="ladder-vehicle-label-setting"
+            value={settings.ladderVehicleLabel}
+            onChange={(value) => {
+              dispatch(setLadderVehicleLabelSetting(parseInt(value, 10)))
+            }}
+            options={[
+              { label: "Run #", value: VehicleLabelSetting.RunNumber },
+              { label: "Vehicle #", value: VehicleLabelSetting.VehicleNumber },
+            ]}
+          />
+          <DropdownSetting
+            icon={mapIcon}
+            label="Map"
+            selectId="map-vehicle-label-setting"
+            value={settings.shuttleVehicleLabel}
+            onChange={(value) => {
+              dispatch(setShuttleVehicleLabelSetting(parseInt(value, 10)))
+            }}
+            options={[
+              { label: "Run #", value: VehicleLabelSetting.RunNumber },
+              { label: "Vehicle #", value: VehicleLabelSetting.VehicleNumber },
+            ]}
+          />
         </div>
       </div>
     </div>
   )
 }
+
+const DropdownSetting = ({
+  icon,
+  label,
+  selectId,
+  value,
+  onChange,
+  options,
+}: {
+  icon: (className: string) => ReactElement
+  label: string
+  selectId: string
+  value: string | number
+  onChange: (value: string) => void
+  options: { label: string; value: string | number }[]
+}) => (
+  <div className="m-settings-page__row">
+    <div className="m-settings-page__icon">
+      {icon("m-settings-page__icon-path")}
+    </div>
+    <div className="m-settings-page__label">{label}</div>
+    <select
+      id={selectId}
+      className="m-settings-page__select"
+      value={value}
+      onChange={(event) => onChange(event.currentTarget.value)}
+    >
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </select>
+  </div>
+)
 
 export default SettingsPage
