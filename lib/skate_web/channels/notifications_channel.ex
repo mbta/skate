41cddcfd3084d2a77753af1e2c25dc28d37ca4b1
Phil Darnowsky <pdarnowsky@mbta.com>
2020-08-17 09:53:46 -0400

    Tests for channel

--- lib/skate_web/channels/notifications_channel.ex
+++ lib/skate_web/channels/notifications_channel.ex
@@ -7,7 +7,6 @@ defmodule SkateWeb.NotificationsChannel do
       Application.get_env(:skate, :valid_token?, &SkateWeb.ChannelAuth.valid_token?/1)
 
     if valid_token?.(socket) do
-      IO.inspect(message, label: "NOTIFICATION MESSAGE")
       :ok = push(socket, "notification", %{data: message})
       {:noreply, socket}
     else
@@ -21,30 +20,4 @@ defmodule SkateWeb.NotificationsChannel do
     Notifications.NotificationServer.subscribe()
     {:ok, socket}
   end
-
-  #  def join("notifications:lobby", payload, socket) do
-  #    if authorized?(payload) do
-  #      {:ok, socket}
-  #    else
-  #      {:error, %{reason: "unauthorized"}}
-  #    end
-  #  end
-  #
-  #  # Channels can be used in a request/response fashion
-  #  # by sending replies to requests from the client
-  #  def handle_in("ping", payload, socket) do
-  #    {:reply, {:ok, payload}, socket}
-  #  end
-  #
-  #  # It is also common to receive messages from the client and
-  #  # broadcast to everyone in the current topic (notifications:lobby).
-  #  def handle_in("shout", payload, socket) do
-  #    broadcast socket, "shout", payload
-  #    {:noreply, socket}
-  #  end
-  #
-  #  # Add authorization logic here as required.
-  #  defp authorized?(_payload) do
-  #    true
-  #  end
 end
---
+++ test/channels/notifications_channel_test.exs
@@ -0,0 +1,58 @@
+defmodule SkateWeb.NotificationsChannelTest do
+  use SkateWeb.ChannelCase
+  import Test.Support.Helpers
+
+  alias Phoenix.Socket
+  alias SkateWeb.{NotificationsChannel, UserSocket}
+
+  setup do
+    reassign_env(:skate, :valid_token?, fn _socket -> true end)
+
+    socket = socket(UserSocket, "", %{guardian_default_resource: "test_uid"})
+
+    start_supervised({Registry, keys: :duplicate, name: Notifications.Supervisor.registry_name()})
+
+    start_supervised(
+      {Notifications.NotificationServer, name: Notifications.NotificationServer.default_name()}
+    )
+
+    {:ok, socket: socket}
+  end
+
+  describe "join/3" do
+    test "subscribes to notifications", %{
+      socket: socket
+    } do
+      assert {:ok, %{}, %Socket{}} =
+               subscribe_and_join(socket, NotificationsChannel, "notifications")
+    end
+  end
+
+  describe "handle_info/2" do
+    test "pushes new data onto the socket", %{socket: socket} do
+      {:ok, _, socket} = subscribe_and_join(socket, NotificationsChannel, "notifications")
+
+      assert {:noreply, socket} =
+               NotificationsChannel.handle_info(
+                 {:notification, "bad thing happen on bus"},
+                 socket
+               )
+
+      assert_push("notification", %{data: "bad thing happen on bus"})
+    end
+
+    test "rejects sending vehicle data when socket is not authenticated", %{socket: socket} do
+      reassign_env(:skate, :valid_token?, fn _socket -> false end)
+
+      {:ok, _, socket} = subscribe_and_join(socket, NotificationsChannel, "notifications")
+
+      assert {:stop, :normal, _socket} =
+               NotificationsChannel.handle_info(
+                 {:notification, "bad thing happen on bus"},
+                 socket
+               )
+
+      assert_push("auth_expired", _)
+    end
+  end
+end
