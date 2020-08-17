24a95d16ccef9a81ba68221099f27313b3c8071e
Phil Darnowsky <pdarnowsky@mbta.com>
2020-08-17 07:06:24 -0400

    channel subscribes to pubsub

--- lib/skate_web/channels/notifications_channel.ex
+++ lib/skate_web/channels/notifications_channel.ex
@@ -1,8 +1,25 @@
 defmodule SkateWeb.NotificationsChannel do
   use SkateWeb, :channel
 
-  def join(_, _, _) do
-    {:error, %{reason: "not implemented"}}
+  @impl Phoenix.Channel
+  def handle_info({:notification, message}, socket) do
+    valid_token? =
+      Application.get_env(:skate, :valid_token?, &SkateWeb.ChannelAuth.valid_token?/1)
+
+    if valid_token?.(socket) do
+      IO.inspect(message, label: "NOTIFICATION MESSAGE")
+      :ok = push(socket, "notification", %{data: message})
+      {:noreply, socket}
+    else
+      :ok = push(socket, "auth_expired", %{})
+      {:stop, :normal, socket}
+    end
+  end
+
+  @impl true
+  def join("notifications", _message, socket) do
+    Notifications.NotificationServer.subscribe()
+    {:ok, socket}
   end
 
   #  def join("notifications:lobby", payload, socket) do
