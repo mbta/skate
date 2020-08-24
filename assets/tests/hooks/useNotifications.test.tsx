c02a17de769101a6251b08059f47c63d87fb9fba
Sky Rose <srose@mbta.com>
2020-08-24 12:40:24 -0400

    increase test coverage

--- assets/tests/hooks/useNotifications.test.tsx
+++ assets/tests/hooks/useNotifications.test.tsx
@@ -1,14 +1,15 @@
 import { renderHook } from "@testing-library/react-hooks"
-import { makeMockChannel, makeMockSocket } from "../testHelpers/socketHelpers"
-import { useNotifications } from "../../src/hooks/useNotifications"
 import { Socket } from "phoenix"
-import { RouteId } from "../../src/schedule"
-import { ConnectionStatus } from "../../src/hooks/useSocket"
 import React, { ReactElement } from "react"
 import { SocketProvider } from "../../src/contexts/socketContext"
 import { StateDispatchProvider } from "../../src/contexts/stateDispatchContext"
-import { initialState } from "../../src/state"
+import { useNotifications } from "../../src/hooks/useNotifications"
+import { ConnectionStatus } from "../../src/hooks/useSocket"
+import * as browser from "../../src/models/browser"
 import { NotificationData } from "../../src/models/notificationData"
+import { RouteId } from "../../src/schedule"
+import { initialState } from "../../src/state"
+import { makeMockChannel, makeMockSocket } from "../testHelpers/socketHelpers"
 
 // tslint:disable: react-hooks-nesting
 
@@ -134,6 +135,44 @@ describe("useNotifications", () => {
     unmount()
 
     expect(mockChannel.leave).toHaveBeenCalled()
+  })
+
+  test("console.error on join error", async () => {
+    const spyConsoleError = jest.spyOn(console, "error")
+    spyConsoleError.mockImplementationOnce((msg) => msg)
+    const handler = jest.fn()
+    const mockSocket = makeMockSocket()
+    const mockChannel = makeMockChannel("error")
+    mockSocket.channel.mockImplementationOnce(() => mockChannel)
+
+    renderHook(
+      () => {
+        useNotifications(handler)
+      },
+      { wrapper: wrapper(mockSocket, []) }
+    )
+
+    expect(spyConsoleError).toHaveBeenCalled()
+    spyConsoleError.mockRestore()
+  })
+
+  test("reloads the window on channel timeout", async () => {
+    const reloadSpy = jest.spyOn(browser, "reload")
+    reloadSpy.mockImplementationOnce(() => ({}))
+    const handler = jest.fn()
+    const mockSocket = makeMockSocket()
+    const mockChannel = makeMockChannel("timeout")
+    mockSocket.channel.mockImplementationOnce(() => mockChannel)
+
+    renderHook(
+      () => {
+        useNotifications(handler)
+      },
+      { wrapper: wrapper(mockSocket, []) }
+    )
+
+    expect(reloadSpy).toHaveBeenCalled()
+    reloadSpy.mockRestore()
   })
 })
 
