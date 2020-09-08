4cf0c7cba13000debc48c0517c0841efa7a2ec6d
Phil Darnowsky <pdarnowsky@mbta.com>
2020-09-08 08:36:36 -0400

    delete env vars in test when appropriate

--- test/skate/repo_test.exs
+++ test/skate/repo_test.exs
@@ -3,18 +3,32 @@ defmodule Skate.RepoTest do
 
   describe "add_prod_credentials/2" do
     setup do
-      original_hostname = System.get_env("POSTGRES_HOSTNAME", "localhost")
-      original_port = System.get_env("POSTGRES_PORT", "5432")
-      original_username = System.get_env("POSTGRES_USERNAME", System.get_env("USER"))
+      original_hostname = System.get_env("POSTGRES_HOSTNAME")
+      original_port = System.get_env("POSTGRES_PORT")
+      original_username = System.get_env("POSTGRES_USERNAME")
 
       System.put_env("POSTGRES_HOSTNAME", "db_server_hostname")
       System.put_env("POSTGRES_USERNAME", "my_username")
       System.put_env("POSTGRES_PORT", "6789")
 
       on_exit(fn ->
-        System.put_env("POSTGRES_HOSTNAME", original_hostname)
-        System.put_env("POSTGRES_USERNAME", original_username)
-        System.put_env("POSTGRES_PORT", original_port)
+        if original_hostname do
+          System.put_env("POSTGRES_HOSTNAME", original_hostname)
+        else
+          System.delete_env("POSTGRES_HOSTNAME")
+        end
+
+        if original_username do
+          System.put_env("POSTGRES_USERNAME", original_username)
+        else
+          System.delete_env("POSTGRES_USERNAME")
+        end
+
+        if original_port do
+          System.put_env("POSTGRES_PORT", original_port)
+        else
+          System.delete_env("POSTGRES_PORT")
+        end
       end)
 
       :ok
