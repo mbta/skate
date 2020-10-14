5588f5cca6bfe473b5a8d31083a54caa9fe94dad
Sky Rose <srose@mbta.com>
2020-10-14 10:15:25 -0400

    don't rollback migrations before tests

--- lib/skate/migrate.ex
+++ lib/skate/migrate.ex
@@ -2,8 +2,4 @@ defmodule Skate.Migrate do
   def up() do
     Ecto.Migrator.run(Skate.Repo, :up, all: true)
   end
-
-  def down() do
-    Ecto.Migrator.run(Skate.Repo, :down, all: true)
-  end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,3 @@
 Application.ensure_all_started(:stream_data)
-Skate.Migrate.down()
 Skate.Migrate.up()
 ExUnit.start(capture_log: true)
