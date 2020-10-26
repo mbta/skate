54b8a93b03959ddcb22620d9f937ecbd9dccadd8
Sky Rose <srose@mbta.com>
2020-10-26 17:08:35 -0400

    style notification drawer title bar

--- assets/src/components/notificationDrawer.tsx
+++ assets/src/components/notificationDrawer.tsx
@@ -1,7 +1,24 @@
-import React from "react"
+import React, { useContext } from "react"
+import { StateDispatchContext } from "../contexts/stateDispatchContext"
+import { closeNotificationDrawer } from "../state"
+import CloseButton from "./closeButton"
+import NotificationBellIcon from "./notificationBellIcon"
 
 const NotificationDrawer = () => {
-  return <div className="m-notification-drawer">Notification Drawer</div>
+  const [, dispatch] = useContext(StateDispatchContext)
+  return (
+    <div className="m-notification-drawer">
+      <div className="m-notification-drawer__title-bar">
+        <NotificationBellIcon />
+        <div className="m-notification-drawer__title">Notifications</div>
+        <CloseButton
+          onClick={() => {
+            dispatch(closeNotificationDrawer())
+          }}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default NotificationDrawer
