402aca1d34738b64e7f9494e99e736b76980a20a
Sky Rose <srose@mbta.com>
2020-11-05 15:55:27 -0500

    basic list of notifications in drawer

--- assets/src/components/notificationDrawer.tsx
+++ assets/src/components/notificationDrawer.tsx
@@ -1,24 +1,58 @@
 import React, { useContext } from "react"
+import { NotificationsContext } from "../contexts/notificationsContext"
 import { StateDispatchContext } from "../contexts/stateDispatchContext"
 import { closeNotificationDrawer } from "../state"
 import CloseButton from "./closeButton"
 import NotificationBellIcon from "./notificationBellIcon"
 
 const NotificationDrawer = () => {
-  const [, dispatch] = useContext(StateDispatchContext)
   return (
     <div className="m-notification-drawer">
-      <div className="m-notification-drawer__title-bar">
-        <NotificationBellIcon />
-        <div className="m-notification-drawer__title">Notifications</div>
-        <CloseButton
-          onClick={() => {
-            dispatch(closeNotificationDrawer())
-          }}
-        />
+      <TitleBar />
+      <div className="m-notification-drawer__content">
+        <Content />
       </div>
     </div>
   )
 }
+
+const TitleBar = () => {
+  const [, dispatch] = useContext(StateDispatchContext)
+  return (
+    <div className="m-notification-drawer__title-bar">
+      <NotificationBellIcon />
+      <div className="m-notification-drawer__title">Notifications</div>
+      <CloseButton
+        onClick={() => {
+          dispatch(closeNotificationDrawer())
+        }}
+      />
+    </div>
+  )
+}
+
+const Content = () => {
+  const { notifications } = useContext(NotificationsContext)
+
+  if (notifications.length === 0) return <EmptyMessage />
+
+  return (
+    <div className="m-notification-drawer__cards">
+      {notifications.map((notification) => (
+        <div key={notification.id}>{notification.reason}</div>
+      ))}
+    </div>
+  )
+}
+
+const EmptyMessage = () => (
+  <>
+    <p>You have no notifications currently.</p>
+    <p>
+      Here you'll be notified about events like accidents and ghost vehicles on
+      any bus routes you have selected on the Route Ladders page.
+    </p>
+  </>
+)
 
 export default NotificationDrawer
