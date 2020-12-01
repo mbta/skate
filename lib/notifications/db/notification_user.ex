1926e2452529550800b54b914d939993ee6f83b7
Phil Darnowsky <pdarnowsky@mbta.com>
2020-12-01 15:30:02 -0500

    Schema and migrations to join notifications to users

---
+++ lib/notifications/db/notification_user.ex
@@ -0,0 +1,21 @@
+defmodule Notifications.Db.NotificationUser do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Notifications.NotificationState
+
+  @type t() :: %__MODULE__{}
+
+  schema "notifications_users" do
+    field(:notification_id, :integer)
+    field(:user_id, :integer)
+    field(:state, NotificationState)
+  end
+
+  def changeset(notification_user, attrs \\ %{}) do
+    notification_user
+    |> cast(attrs, [:notification_id, :user_id, :state])
+    |> validate_required([:notification_id, :user_id, :state])
+    |> unique_constraint([:notification_id, :user_id], name: "notifications_users_pkey")
+  end
+end
