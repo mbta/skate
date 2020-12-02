ad07180c177a485664e285661e7957b595d7222b
Phil Darnowsky <pdarnowsky@mbta.com>
2020-12-02 09:56:44 -0500

    fixup: remove unused changeset

--- lib/notifications/db/notification_user.ex
+++ lib/notifications/db/notification_user.ex
@@ -1,6 +1,5 @@
 defmodule Notifications.Db.NotificationUser do
   use Ecto.Schema
-  import Ecto.Changeset
 
   alias Notifications.NotificationState
 
@@ -13,12 +12,5 @@ defmodule Notifications.Db.NotificationUser do
     field(:user_id, :integer, primary_key: true)
     field(:state, NotificationState)
     timestamps()
-  end
-
-  def changeset(notification_user, attrs \\ %{}) do
-    notification_user
-    |> cast(attrs, [:notification_id, :user_id, :state])
-    |> validate_required([:notification_id, :user_id, :state])
-    |> unique_constraint([:notification_id, :user_id], name: "notifications_users_pkey")
   end
 end
