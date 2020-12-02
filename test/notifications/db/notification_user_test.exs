2db585412317d132ab65bc690075038640497382
Phil Darnowsky <pdarnowsky@mbta.com>
2020-12-02 10:36:31 -0500

    fixup: and more coverage

--- test/notifications/db/notification_user_test.exs
+++ test/notifications/db/notification_user_test.exs
@@ -25,11 +25,17 @@ defmodule Notifications.Db.NotificationUserTest do
         DbUser.changeset(%DbUser{}, %{username: "charlie"})
         |> Skate.Repo.insert()
 
-      Skate.Repo.insert!(%DbNotificationUser{
-        notification_id: notification.id,
-        user_id: user.id,
-        state: :deleted
-      })
+      notification_user =
+        Skate.Repo.insert!(%DbNotificationUser{
+          notification_id: notification.id,
+          user_id: user.id,
+          state: :deleted
+        })
+
+      assert Skate.Repo.one(DbNotificationUser,
+               notification_id: notification.id,
+               user_id: user.id
+             ).state == :deleted
     end
   end
 end
