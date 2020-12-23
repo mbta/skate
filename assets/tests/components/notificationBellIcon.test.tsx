1184cffa3ded464871b38a481c6cdd3ec9c27aab
Phil Darnowsky <pdarnowsky@mbta.com>
2020-12-23 14:41:03 -0500

    Show badge on bell if new notifications

--- assets/tests/components/notificationBellIcon.test.tsx
+++ assets/tests/components/notificationBellIcon.test.tsx
@@ -1,28 +1,92 @@
 import React from "react"
 import renderer from "react-test-renderer"
 import NotificationBellIcon from "../../src/components/notificationBellIcon"
+import {
+  NotificationsContext,
+  State as NotificationsState,
+} from "../../src/contexts/notificationsContext"
 import { StateDispatchProvider } from "../../src/contexts/stateDispatchContext"
+import { Notification } from "../../src/realtime"
 import { initialState } from "../../src/state"
 
+const unreadNotification: Notification = {
+  id: "1",
+  createdAt: new Date(0),
+  reason: "other",
+  routeIds: [],
+  runIds: [],
+  tripIds: [],
+  startTime: new Date(0),
+  operatorName: null,
+  operatorId: null,
+  routeIdAtCreation: null,
+  state: "unread",
+}
+
+const readNotification: Notification = { ...unreadNotification, state: "read" }
+
+const unreadNotificationState: NotificationsState = {
+  notifications: [unreadNotification],
+  showLatestNotification: true,
+  dispatch: jest.fn(),
+}
+
+const readNotificationState: NotificationsState = {
+  ...unreadNotificationState,
+  notifications: [readNotification],
+}
+
 describe("NotificationBellIcon", () => {
-  test("renders when the drawer is closed", () => {
+  test("renders when the drawer is closed and there are new notifications", () => {
     const state = { ...initialState, notificationDrawerIsOpen: false }
     const tree = renderer
       .create(
         <StateDispatchProvider state={state} dispatch={jest.fn()}>
-          <NotificationBellIcon />
+          <NotificationsContext.Provider value={unreadNotificationState}>
+            <NotificationBellIcon />
+          </NotificationsContext.Provider>
         </StateDispatchProvider>
       )
       .toJSON()
     expect(tree).toMatchSnapshot()
   })
 
-  test("renders when the drawer is open", () => {
+  test("renders when the drawer is open and there are new notifications", () => {
     const state = { ...initialState, notificationDrawerIsOpen: true }
     const tree = renderer
       .create(
         <StateDispatchProvider state={state} dispatch={jest.fn()}>
-          <NotificationBellIcon />
+          <NotificationsContext.Provider value={unreadNotificationState}>
+            <NotificationBellIcon />
+          </NotificationsContext.Provider>
+        </StateDispatchProvider>
+      )
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  test("renders when the drawer is closed and there are not new notifications", () => {
+    const state = { ...initialState, notificationDrawerIsOpen: false }
+    const tree = renderer
+      .create(
+        <StateDispatchProvider state={state} dispatch={jest.fn()}>
+          <NotificationsContext.Provider value={readNotificationState}>
+            <NotificationBellIcon />
+          </NotificationsContext.Provider>
+        </StateDispatchProvider>
+      )
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  test("renders when the drawer is open and there are not new notifications", () => {
+    const state = { ...initialState, notificationDrawerIsOpen: true }
+    const tree = renderer
+      .create(
+        <StateDispatchProvider state={state} dispatch={jest.fn()}>
+          <NotificationsContext.Provider value={readNotificationState}>
+            <NotificationBellIcon />
+          </NotificationsContext.Provider>
         </StateDispatchProvider>
       )
       .toJSON()
