41c1385f2e96a5e275172c95ad12121abea6c508
Phil Darnowsky <pdarnowsky@mbta.com>
2021-02-01 15:20:50 -0500

    Add migration for minischedules trip label

---
+++ priv/repo/migrations/20210201201440_add_trip_label_to_user_settings.exs
@@ -0,0 +1,16 @@
+defmodule Skate.Repo.Migrations.AddTripLabelToUserSettings do
+  use Ecto.Migration
+
+  def change do
+    execute(
+      "CREATE TYPE trip_label AS ENUM ('origin', 'destination')",
+      "DROP TYPE trip_label"
+    )
+
+    alter table(:user_settings) do
+      add(:minischedules_trip_label, :trip_label, default: "destination")
+    end
+
+    execute("UPDATE user_settings SET minischedules_trip_label = 'destination'", fn -> end)
+  end
+end
