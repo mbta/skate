1c37032a8f107eb7873ab2472fb2dedc4ebdaa76
Phil Darnowsky <phil@darnowsky.com>
2021-03-12 08:38:47 -0500

    fixup: coverage

---
+++ assets/tests/models/notificationData.test.tsx
@@ -0,0 +1,27 @@
+import { Notification } from "../../src/realtime"
+import {
+  NotificationData,
+  notificationFromData,
+} from "../../src/models/notificationData"
+
+describe("notificationFromData", () => {
+  test("handles a null endTime", () => {
+    const data: NotificationData = {
+      id: "1",
+      created_at: 0,
+      reason: "manpower",
+      route_ids: [],
+      run_ids: [],
+      trip_ids: [],
+      operator_name: null,
+      operator_id: null,
+      route_id_at_creation: null,
+      start_time: 1234,
+      end_time: null,
+      state: "unread",
+    }
+
+    const notification: Notification = notificationFromData(data)
+    expect(notification.endTime).toEqual(null)
+  })
+})
