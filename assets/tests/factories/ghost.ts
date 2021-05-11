1de88d814e2467e916d33a9bcd415aa08edc3872
Eddie Maldonado <emaldonado@mbta.com>
2021-05-11 08:36:58 -0400

    Use test factory libraries (#1106)
    * test: start using Fishery as factory library for front-end tests
    
    * test: start using ExMachine for back-end test factories

---
+++ assets/tests/factories/ghost.ts
@@ -0,0 +1,20 @@
+import { Factory } from "fishery"
+import { Ghost } from "../../src/realtime"
+
+export default Factory.define<Ghost>(() => ({
+  id: "ghost-trip",
+  directionId: 0,
+  routeId: "1",
+  tripId: "trip",
+  headsign: "headsign",
+  blockId: "block",
+  runId: null,
+  viaVariant: null,
+  layoverDepartureTime: null,
+  scheduledTimepointStatus: {
+    timepointId: "t0",
+    fractionUntilTimepoint: 0.0,
+  },
+  routeStatus: "on_route",
+  blockWaivers: [],
+}))
--- mix.exs
+++ mix.exs
@@ -81,7 +81,8 @@ defmodule Skate.MixProject do
       {:ecto_sql, "~> 3.4"},
       {:postgrex, "~> 0.15"},
       {:ex_aws_rds, "~> 2.0.2"},
-      {:fast_local_datetime, "~> 1.0"}
+      {:fast_local_datetime, "~> 1.0"},
+      {:ex_machina, "~> 2.7.0", only: :test}
     ]
   end
 end
