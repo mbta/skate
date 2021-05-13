8922e5aa1bc6b82e1b94d4dc42efe23a5160cc5b
Eddie Maldonado <emaldonado@mbta.com>
2021-05-13 14:54:41 -0400

    feat: move from Inter UI to Inter (#1105)

--- upload_assets.sh
+++ upload_assets.sh
@@ -14,6 +14,10 @@ unzip $BUILD_ARTIFACT "lib/$APP-$VERSION/priv/static/*" -d $TEMP_DIR
 # sync the digested files with a cache control header
 aws s3 sync $STATIC_DIR $S3_DIR --size-only --exclude "*" --include "*-*" --cache-control=$CACHE_CONTROL
 
+# font content-types need to be specified manually
+aws s3 sync $STATIC_DIR/fonts $S3_DIR/fonts --size-only --exclude "*" --include "*.woff" --cache-control=$CACHE_CONTROL --content-type "font/woff"
+aws s3 sync $STATIC_DIR/fonts $S3_DIR/fonts --size-only --exclude "*" --include "*.woff2" --cache-control=$CACHE_CONTROL --content-type "font/woff2"
+
 # sync everything else normally
 aws s3 sync $STATIC_DIR $S3_DIR --size-only
 
