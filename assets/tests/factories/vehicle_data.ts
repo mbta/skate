70e910a63f19322addd48e97d6031880d7f75d1f
Maldonado <emaldonado@mbta.com>
2021-05-21 14:58:01 -0400

    fixup! feat: front-end changes to use vehicles by block for swings

---
+++ assets/tests/factories/vehicle_data.ts
@@ -0,0 +1,88 @@
+import { Factory } from "fishery"
+import { VehicleData } from "../../src/models/vehicleData"
+
+export default Factory.define<VehicleData>(({ sequence }) => ({
+  id: `v${sequence}`,
+  label: `v${sequence}-label`,
+  run_id: `run-${sequence}`,
+  timestamp: 123,
+  latitude: 0,
+  longitude: 0,
+  direction_id: 0,
+  route_id: "39",
+  trip_id: `t${sequence}`,
+  headsign: "Forest Hills",
+  via_variant: "X",
+  operator_id: `op${sequence}`,
+  operator_first_name: "WILL",
+  operator_last_name: "SMITH",
+  operator_logon_time: Math.floor(
+    new Date("2018-08-15T13:38:21.000Z").getTime() / 1000
+  ),
+  bearing: 33,
+  block_id: `block-${sequence}`,
+  headway_secs: 859.1,
+  headway_spacing: null,
+  previous_vehicle_id: `v${sequence + 1}`,
+  schedule_adherence_secs: 0,
+  scheduled_headway_secs: 120,
+  is_shuttle: false,
+  is_overload: false,
+  is_off_course: false,
+  is_revenue: true,
+  layover_departure_time: null,
+  sources: [],
+  data_discrepancies: [
+    {
+      attribute: "trip_id",
+      sources: [
+        {
+          id: "swiftly",
+          value: "swiftly-trip-id",
+        },
+        {
+          id: "busloc",
+          value: "busloc-trip-id",
+        },
+      ],
+    },
+    {
+      attribute: "route_id",
+      sources: [
+        {
+          id: "swiftly",
+          value: null,
+        },
+        {
+          id: "busloc",
+          value: "busloc-route-id",
+        },
+      ],
+    },
+  ],
+  stop_status: {
+    stop_id: "s1",
+    stop_name: "Stop Name",
+  },
+  timepoint_status: {
+    timepoint_id: "tp1",
+    fraction_until_timepoint: 0.5,
+  },
+  scheduled_location: {
+    route_id: "39",
+    direction_id: 0,
+    trip_id: "scheduled trip",
+    run_id: "scheduled run",
+    time_since_trip_start_time: 0,
+    headsign: "scheduled headsign",
+    via_variant: "scheduled via variant",
+    timepoint_status: {
+      timepoint_id: "tp1",
+      fraction_until_timepoint: 0.5,
+    },
+  },
+  route_status: "on_route",
+  end_of_trip_type: "another_trip",
+  block_waivers: [],
+  crowding: null,
+}))
