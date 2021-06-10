ce20bea3060dd923045b04936c3580b2cd57caf3
Phil Darnowsky <pdarnowsky@mbta.com>
2021-06-10 07:07:11 -0400

    Got a Pigeon map of downtown Boston

--- assets/src/components/otherMap.tsx
+++ assets/src/components/otherMap.tsx
@@ -1,6 +1,11 @@
 import React from "react"
+import { Map } from "pigeon-maps"
+
 import { TrainVehicle, Vehicle } from "../realtime.d"
 import { Shape } from "../schedule"
+
+const tileProvider = (x: number, y: number, z: number, _dpr?: number): string =>
+  `https://mbta-map-tiles-dev.s3.amazonaws.com/osm_tiles/${z}/${x}/${y}.png`
 
 export interface Props {
   vehicles: Vehicle[]
@@ -11,6 +16,12 @@ export interface Props {
   trainVehicles?: TrainVehicle[]
   //  reactLeafletRef?: MutableRefObject<ReactLeafletMap | null>
 }
-const OtherMap = (_props: Props) => <div>fake map</div>
+const OtherMap = (_props: Props) => (
+  <Map
+    provider={tileProvider}
+    defaultCenter={[42.360718, -71.05891]}
+    defaultZoom={13}
+  />
+)
 
 export default OtherMap
