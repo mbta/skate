ba3a5d20e82c210f8c0559181e5b70f65e7f76f5
Phil Darnowsky <pdarnowsky@mbta.com>
2021-06-11 14:17:44 -0400

    Revert "Add workflow to deploy to dev-blue (#1139)" (#1151)
    This reverts commit e12f612f697ca0a831cc9bd2a2381f806b7883ba.

--- upload_assets.sh
+++ upload_assets.sh
@@ -3,9 +3,13 @@ set -e -x
 
 VERSION=$(grep -o 'version: .*"' mix.exs  | grep -E -o '([0-9]+\.)+[0-9]+')
 APP=skate
+BUILD_ARTIFACT=$APP-build.zip
+TEMP_DIR=tmp_unzip
 CACHE_CONTROL="public,max-age=31536000"
-STATIC_DIR=priv/static
+STATIC_DIR=$TEMP_DIR/lib/$APP-$VERSION/priv/static
 S3_DIR=s3://mbta-dotcom/$APP
+
+unzip $BUILD_ARTIFACT "lib/$APP-$VERSION/priv/static/*" -d $TEMP_DIR
 
 # sync the digested files with a cache control header
 aws s3 sync $STATIC_DIR $S3_DIR --size-only --exclude "*" --include "*-*" --cache-control=$CACHE_CONTROL
@@ -16,3 +20,5 @@ aws s3 sync $STATIC_DIR/fonts $S3_DIR/fonts --size-only --exclude "*" --include 
 
 # sync everything else normally
 aws s3 sync $STATIC_DIR $S3_DIR --size-only
+
+rm -rf $TEMP_DIR
