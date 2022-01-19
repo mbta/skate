398985061ce28caaa4a57b99b30e6a35db5d3a46
Maldonado <emaldonado@mbta.com>
2022-01-19 09:58:41 -0500

    feat: re-enable route tabs backend, with UUID support

--- test/skate_web/controllers/route_tabs_controller_test.exs
+++ test/skate_web/controllers/route_tabs_controller_test.exs
@@ -6,11 +6,12 @@ defmodule SkateWeb.RouteTabsControllerTest do
 
   describe "PUT /api/route_tabs" do
     @tag :authenticated
-    test "doesn't set route tabs for logged-in user (for now)", %{conn: conn, user: user} do
+    test "sets route tabs for logged-in user", %{conn: conn, user: user} do
       conn =
         put(conn, "/api/route_tabs", %{
           "route_tabs" => [
             %{
+              "uuid" => Ecto.UUID.generate(),
               "presetName" => "new preset",
               "selectedRouteIds" => ["1", "28"],
               "ordering" => "12345"
@@ -20,7 +21,13 @@ defmodule SkateWeb.RouteTabsControllerTest do
 
       response(conn, 200)
 
-      assert [] = RouteTab.get_all_for_user(user)
+      assert [
+               %RouteTab{
+                 preset_name: "new preset",
+                 selected_route_ids: ["1", "28"],
+                 ordering: 12345
+               }
+             ] = RouteTab.get_all_for_user(user)
     end
   end
 end
