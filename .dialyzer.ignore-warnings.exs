a9a32ceb017c3f45c2a3c29c14500a2d55aa2d83
Paul Swartz <paul@paulswartz.net>
2022-01-27 10:55:19 -0500

    fix: ignore Dialyzer warnings about GenServer.option
    Unfortunately, Elixir < 1.12 has the wrong type for `spawn_opt` in
    `GenServer.option`. This results in Dialyzer warnings, even though the
    code itself is correct.
    
    Dialyzer has a newer, more flexible warning format that we use here. The
    old Concentrate warnings do not appear, so we no longer ignore them.

---
+++ .dialyzer.ignore-warnings.exs
@@ -0,0 +1,5 @@
+[
+  {"lib/concentrate/consumer/vehicle_positions.ex", :no_return},
+  {"lib/concentrate/consumer/vehicle_positions.ex", :call},
+  {"lib/realtime/block_waiver_store.ex", :no_return}
+]
