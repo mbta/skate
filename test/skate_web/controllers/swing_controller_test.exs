7dcfe61ad5dad96399d7c0f09cd63d253ce0f4d3
Maldonado <emaldonado@mbta.com>
2022-02-25 10:18:10 -0500

    fix: base routes in swings view off of route_tabs table

--- test/skate_web/controllers/swing_controller_test.exs
+++ test/skate_web/controllers/swing_controller_test.exs
@@ -15,13 +15,27 @@ defmodule SkateWeb.SwingsControllerTest do
       to_route_id: "2",
       to_trip_id: "5678",
       time: 100
+    },
+    %Swing{
+      from_run_id: "124-123",
+      from_route_id: "3",
+      from_trip_id: "1235",
+      to_run_id: "124-789",
+      to_route_id: "3",
+      to_trip_id: "5679",
+      time: 100
     }
   ]
 
   describe "GET /api/swings" do
     setup %{user: username} do
       if username, do: RouteSettings.get_or_create(username)
-      reassign_env(:skate_web, :swings_fn, fn _route_id, _start_time, _end_time -> @swings end)
+
+      reassign_env(:skate_web, :swings_fn, fn route_id, _start_time, _end_time ->
+        Enum.filter(@swings, fn swing ->
+          swing.from_route_id == route_id or swing.to_route_id == route_id
+        end)
+      end)
     end
 
     test "when logged out, redirects you to cognito auth", %{conn: conn} do
@@ -38,8 +52,27 @@ defmodule SkateWeb.SwingsControllerTest do
       conn =
         conn
         |> api_headers()
-        |> put("/api/route_settings", %{
-          "selectedRouteIds" => ["1", "2"]
+        |> put("/api/route_tabs", %{
+          "route_tabs" => [
+            %{
+              uuid: Ecto.UUID.generate(),
+              ordering: 0,
+              selectedRouteIds: ["1"],
+              isCurrentTab: true
+            },
+            %{
+              uuid: Ecto.UUID.generate(),
+              ordering: 1,
+              selectedRouteIds: ["2"],
+              isCurrentTab: false
+            },
+            %{
+              uuid: Ecto.UUID.generate(),
+              ordering: nil,
+              selectedRouteIds: ["3"],
+              isCurrentTab: false
+            }
+          ]
         })
         |> get("/api/swings")
 
