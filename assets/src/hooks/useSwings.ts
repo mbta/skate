969bea9612d9127420101a7fb8e865f0b7fff09e
Maldonado <emaldonado@mbta.com>
2022-02-28 16:39:09 -0500

    refactor: helper function to determine all open routes across tabs

--- assets/src/hooks/useSwings.ts
+++ assets/src/hooks/useSwings.ts
@@ -2,18 +2,13 @@ import { useEffect, useState, useContext } from "react"
 import { fetchSwings } from "../api"
 import { Swing } from "../schedule.d"
 import { StateDispatchContext } from "../contexts/stateDispatchContext"
-import { uniq, flatten } from "../helpers/array"
-import { isOpenTab } from "../models/routeTab"
+import { allOpenRouteIds } from "../models/routeTab"
 
 const useSwings = (): Swing[] | null => {
   const [{ routeTabs }] = useContext(StateDispatchContext)
   const [swings, setSwings] = useState<Swing[] | null>(null)
 
-  const routeIds = uniq(
-    flatten(
-      routeTabs.filter(isOpenTab).map((routeTab) => routeTab.selectedRouteIds)
-    )
-  )
+  const routeIds = allOpenRouteIds(routeTabs)
 
   useEffect(() => {
     fetchSwings(routeIds).then((newSwings: Swing[] | null) => {
