02d1e33141246889f898d430808d9bf9dfc71eb6
Eddie Maldonado <emaldonado@mbta.com>
2022-03-30 13:18:26 -0400

    feat: scroll route ladders horizontally with mouse wheel (#1471)

--- assets/tests/components/routeLadders.test.tsx
+++ assets/tests/components/routeLadders.test.tsx
@@ -1,31 +1,51 @@
 import React from "react"
 import renderer from "react-test-renderer"
+import { render, fireEvent } from "@testing-library/react"
 import routeFactory from "../factories/route"
 import RouteLadders from "../../src/components/routeLadders"
 import { Route, TimepointsByRouteId } from "../../src/schedule.d"
 
-test("renders a route ladder", () => {
-  const routes: Route[] = [
-    routeFactory.build({ id: "1", name: "1" }),
-    routeFactory.build({ id: "28", name: "28" }),
-  ]
-  const timepointsByRouteId: TimepointsByRouteId = {
-    "1": [
-      { id: "WASMA", name: "WASMA Name" },
-      { id: "MELWA", name: "MELWA Name" },
-      { id: "HHGAT", name: "HHGAT Name" },
-    ],
-    "28": [
-      { id: "MATPN", name: "MATPN Name" },
-      { id: "WELLH", name: "WELLH Name" },
-      { id: "MORTN", name: "MORTN Name" },
-    ],
-    "71": undefined,
-    "73": null,
-  }
+const routes: Route[] = [
+  routeFactory.build({ id: "1", name: "1" }),
+  routeFactory.build({ id: "28", name: "28" }),
+]
+const timepointsByRouteId: TimepointsByRouteId = {
+  "1": [
+    { id: "WASMA", name: "WASMA Name" },
+    { id: "MELWA", name: "MELWA Name" },
+    { id: "HHGAT", name: "HHGAT Name" },
+  ],
+  "28": [
+    { id: "MATPN", name: "MATPN Name" },
+    { id: "WELLH", name: "WELLH Name" },
+    { id: "MORTN", name: "MORTN Name" },
+  ],
+  "71": undefined,
+  "73": null,
+}
 
-  const tree = renderer
-    .create(
+describe("RouteLadders", () => {
+  test("renders a route ladder", () => {
+    const tree = renderer
+      .create(
+        <RouteLadders
+          routes={routes}
+          timepointsByRouteId={timepointsByRouteId}
+          selectedVehicleId={undefined}
+          deselectRoute={jest.fn()}
+          reverseLadder={jest.fn()}
+          toggleCrowding={jest.fn()}
+          ladderDirections={{}}
+          ladderCrowdingToggles={{}}
+        />
+      )
+      .toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  test("can scroll horizontally with mouse wheel", () => {
+    const result = render(
       <RouteLadders
         routes={routes}
         timepointsByRouteId={timepointsByRouteId}
@@ -37,7 +57,12 @@ test("renders a route ladder", () => {
         ladderCrowdingToggles={{}}
       />
     )
-    .toJSON()
 
-  expect(tree).toMatchSnapshot()
+    const routeLaddersDiv = result.getByTestId("route-ladders-div")
+    routeLaddersDiv.scrollTo = jest.fn()
+
+    fireEvent.wheel(routeLaddersDiv, { deltaY: 10 })
+
+    expect(routeLaddersDiv.scrollTo).toHaveBeenCalled()
+  })
 })
