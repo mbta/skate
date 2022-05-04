8ea81f20dbaa65ce8825fb8f8d139297df624e77
Maldonado <emaldonado@mbta.com>
2022-05-04 17:23:49 -0400

    feat: notification icon

--- assets/src/components/nav/topNav.tsx
+++ assets/src/components/nav/topNav.tsx
@@ -1,8 +1,31 @@
-import React from "react"
+import React, { useContext } from "react"
+import { StateDispatchContext } from "../../contexts/stateDispatchContext"
 import { logoIcon } from "../../helpers/icon"
+import { toggleNotificationDrawer } from "../../state"
+import NotificationBellIcon from "../notificationBellIcon"
 
 const TopNav = (): JSX.Element => {
-  return <div className="m-top-nav">{logoIcon("m-top-nav__logo")}</div>
+  const [{ notificationDrawerIsOpen }, dispatch] =
+    useContext(StateDispatchContext)
+
+  const bellIconClasses = notificationDrawerIsOpen
+    ? ["m-top-nav__notifications-icon", "m-top-nav__notifications-icon--active"]
+    : ["m-top-nav__notifications-icon"]
+
+  return (
+    <div className="m-top-nav">
+      {logoIcon("m-top-nav__logo")}
+      <div className="m-top-nav__right-items">
+        <button
+          className="m-top-nav__right-item"
+          onClick={() => dispatch(toggleNotificationDrawer())}
+          title="Notifications"
+        >
+          <NotificationBellIcon extraClasses={bellIconClasses} />
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default TopNav
---
+++ assets/tests/components/nav/topNav.test.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+import { render } from "@testing-library/react"
+import TopNav from "../../../src/components/nav/topNav"
+import userEvent from "@testing-library/user-event"
+import { StateDispatchProvider } from "../../../src/contexts/stateDispatchContext"
+import { initialState, toggleNotificationDrawer } from "../../../src/state"
+
+describe("TopNav", () => {
+  test("clicking notifications icon toggles notifications drawer", async () => {
+    const dispatch = jest.fn()
+    const user = userEvent.setup()
+    const result = render(
+      <StateDispatchProvider state={initialState} dispatch={dispatch}>
+        <TopNav />
+      </StateDispatchProvider>
+    )
+
+    await user.click(result.getByTitle("Notifications"))
+
+    expect(dispatch).toHaveBeenCalledWith(toggleNotificationDrawer())
+  })
+})
