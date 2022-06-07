4b73584c8e925e89f446b64b1ef9c31c22148d22
Logan Hughes <loganalexhughes@gmail.com>
2022-06-07 13:41:56 -0400

    initialize placeholder for bottom nav

---
+++ assets/src/components/nav/bottomNavMobile.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const BottomNavMobile = (): JSX.Element => {
+
+  return (
+    <div className="m-bottom-nav-mobile" data-testid="bottom-nav-mobile">
+      <div className="m-bottom-nav-mobile__content">
+        Bottom Nav Placeholder
+      </div>
+    </div>
+  )
+}
+
+export default BottomNavMobile
