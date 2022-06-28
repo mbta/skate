32c4785dad5f4ab8c0f2dd1a753c326018ce6746
Logan Hughes <loganalexhughes@gmail.com>
2022-06-28 11:18:10 -0400

    Remove no longer necessary code to close notification if you move away from the Ladder Page

--- assets/src/components/rightPanel.tsx
+++ assets/src/components/rightPanel.tsx
@@ -1,8 +1,7 @@
-import React, { ReactElement, useContext, useEffect } from "react"
-import { useLocation } from "react-router-dom"
+import React, { ReactElement, useContext } from "react"
 import { StateDispatchContext } from "../contexts/stateDispatchContext"
 import { VehicleOrGhost } from "../realtime.d"
-import { setNotification, OpenView } from "../state"
+import { OpenView } from "../state"
 import NotificationDrawer from "./notificationDrawer"
 import PropertiesPanel from "./propertiesPanel"
 import SwingsView from "./swingsView"
@@ -12,17 +11,7 @@ const RightPanel = ({
 }: {
   selectedVehicleOrGhost?: VehicleOrGhost | null
 }): ReactElement<HTMLElement> | null => {
-  const [state, dispatch] = useContext(StateDispatchContext)
-
-  // close notification if you move away from ladder page
-  // TODO delete when notifications are viewable from anywhere
-  const location = useLocation()
-  if (location) {
-    /* istanbul ignore next */
-    useEffect(() => {
-      dispatch(setNotification(undefined))
-    }, [location])
-  }
+  const [state] = useContext(StateDispatchContext)
 
   if (selectedVehicleOrGhost) {
     return <PropertiesPanel selectedVehicleOrGhost={selectedVehicleOrGhost} />
