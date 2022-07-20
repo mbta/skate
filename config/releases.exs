b7282a701b09738bdb01c9aa0b7ccefb659291c6
Logan Hughes <loganalexhughes@gmail.com>
2022-07-20 14:26:57 -0400

    Replace secretsmanager fetch with system get env

--- config/releases.exs
+++ config/releases.exs
@@ -5,8 +5,8 @@ Application.ensure_all_started(:hackney)
 Application.ensure_all_started(:ex_aws)
 
 config :skate,
-  secret_key_base: {:system, "SECRET_KEY_BASE"}
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
-config :ueberauth, Ueberauth.Strategy.Cognito, client_secret: {:system, "COGNITO_CLIENT_SECRET"}
+config :ueberauth, Ueberauth.Strategy.Cognito, client_secret: System.get_env("COGNITO_CLIENT_SECRET")
 
-config :skate, SkateWeb.AuthManager, secret_key: {:system, "GUARDIAN_SECRET_KEY"}
+config :skate, SkateWeb.AuthManager, secret_key: System.get_env("GUARDIAN_SECRET_KEY")
