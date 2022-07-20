8854f9a97fe591a8d5289e5135ae84323e3ad00a
Logan Hughes <loganalexhughes@gmail.com>
2022-07-20 14:14:34 -0400

    replace values removed from releases

--- config/releases.exs
+++ config/releases.exs
@@ -3,3 +3,11 @@ import Config
 Application.ensure_all_started(:poison)
 Application.ensure_all_started(:hackney)
 Application.ensure_all_started(:ex_aws)
+
+config :skate,
+  secret_key_base: {:system, "SECRET_KEY_BASE"}
+
+config :ueberauth, Ueberauth.Strategy.Cognito,
+  client_secret: {:system, "COGNITO_CLIENT_SECRET"}
+
+config :skate, SkateWeb.AuthManager, secret_key: {:system, "GUARDIAN_SECRET_KEY"}
