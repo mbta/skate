a76af6edd42a1892bb6d7735464e8ae25a96c4e7
Logan Hughes <loganalexhughes@gmail.com>
2022-07-20 14:18:48 -0400

    formatting

--- config/releases.exs
+++ config/releases.exs
@@ -7,7 +7,6 @@ Application.ensure_all_started(:ex_aws)
 config :skate,
   secret_key_base: {:system, "SECRET_KEY_BASE"}
 
-config :ueberauth, Ueberauth.Strategy.Cognito,
-  client_secret: {:system, "COGNITO_CLIENT_SECRET"}
+config :ueberauth, Ueberauth.Strategy.Cognito, client_secret: {:system, "COGNITO_CLIENT_SECRET"}
 
 config :skate, SkateWeb.AuthManager, secret_key: {:system, "GUARDIAN_SECRET_KEY"}
