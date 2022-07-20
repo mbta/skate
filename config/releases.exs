f421a3cc0786534d18f0e0719ee9577628018f8f
Logan Hughes <loganalexhughes@gmail.com>
2022-07-20 14:35:16 -0400

    formatting

--- config/releases.exs
+++ config/releases.exs
@@ -7,6 +7,7 @@ Application.ensure_all_started(:ex_aws)
 config :skate,
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
-config :ueberauth, Ueberauth.Strategy.Cognito, client_secret: System.get_env("COGNITO_CLIENT_SECRET")
+config :ueberauth, Ueberauth.Strategy.Cognito,
+  client_secret: System.get_env("COGNITO_CLIENT_SECRET")
 
 config :skate, SkateWeb.AuthManager, secret_key: System.get_env("GUARDIAN_SECRET_KEY")
