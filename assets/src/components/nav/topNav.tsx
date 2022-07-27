e9243d3bc391d2d9c7cf14b1098bef75778907e1
Logan Hughes <loganalexhughes@gmail.com>
2022-07-27 15:04:26 -0400

    Updates to desktop left and top nav
    Separate links and views; move notifications to left nav

--- assets/src/components/nav/topNav.tsx
+++ assets/src/components/nav/topNav.tsx
@@ -1,18 +1,9 @@
 import React, { useContext } from "react"
 import { Link } from "react-router-dom"
-import { StateDispatchContext } from "../../contexts/stateDispatchContext"
 import { logoIcon, refreshIcon } from "../../helpers/icon"
 import { reload } from "../../models/browser"
-import { openNotificationDrawer } from "../../state"
-import NotificationBellIcon from "../notificationBellIcon"
 
 const TopNav = (): JSX.Element => {
-  const [{ notificationDrawerIsOpen }, dispatch] =
-    useContext(StateDispatchContext)
-
-  const bellIconClasses = notificationDrawerIsOpen
-    ? ["m-top-nav__notifications-icon", "m-top-nav__notifications-icon--active"]
-    : ["m-top-nav__notifications-icon"]
 
   return (
     <div className="m-top-nav">
@@ -20,15 +11,6 @@ const TopNav = (): JSX.Element => {
         {logoIcon("m-top-nav__logo-icon")}
       </Link>
       <ul className="m-top-nav__right-items">
-        <li>
-          <button
-            className="m-top-nav__right-item"
-            onClick={() => dispatch(openNotificationDrawer())}
-            title="Notifications"
-          >
-            <NotificationBellIcon extraClasses={bellIconClasses} />
-          </button>
-        </li>
         <li>
           <button
             className="m-top-nav__right-item"
