41e8c0aa988ea71ab1868ea7609895e53ec21146
Logan Hughes <loganalexhughes@gmail.com>
2022-08-04 11:06:49 -0400

    remove unneeded code - tests pass at this point

--- assets/src/components/nav/topNav.tsx
+++ assets/src/components/nav/topNav.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react"
+import React from "react"
 import { Link } from "react-router-dom"
 import { logoIcon, refreshIcon } from "../../helpers/icon"
 import { reload } from "../../models/browser"
