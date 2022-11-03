8fa9943c48adf2f7aa9b27211a047c0bbf67c69b
Kayla Firestack <kfirestack@mbta.com>
2022-11-03 17:52:18 -0400

    fix(skate/controllers/page): extract test group name from test_groups input
    the test revealed I did this wrong! which we couldn't tell before the test because there were no test groups in the database to test with

--- lib/skate_web/controllers/page_controller.ex
+++ lib/skate_web/controllers/page_controller.ex
@@ -29,7 +29,7 @@ defmodule SkateWeb.PageController do
     |> assign(:clarity_tag, Application.get_env(:skate, :clarity_tag))
     |> assign(:google_tag_manager_id, Application.get_env(:skate, :google_tag_manager_id))
     |> assign(:tileset_url, Application.get_env(:skate, :tileset_url))
-    |> assign(:user_test_groups, (user.test_groups))
+    |> assign(:user_test_groups, user.test_groups |> Enum.map(& &1.name))
     |> render("index.html")
   end
 
