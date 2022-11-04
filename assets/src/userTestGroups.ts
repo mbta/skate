3673d58d1ec610acb75963167678b32f4868aed0
Kayla Firestack <kfirestack@mbta.com>
2022-11-04 12:46:41 -0400

    fix: convert functions to const

--- assets/src/userTestGroups.ts
+++ assets/src/userTestGroups.ts
@@ -1,12 +1,14 @@
 import appData from "./appData"
 
-function getTestGroups(): string[] {
+const getTestGroups = (): string[] => {
   const groups = appData()?.userTestGroups
   const testGroups: string[] = groups === undefined ? [] : JSON.parse(groups)
 
   return testGroups
 }
 
-export default function inTestGroup(key: string) {
+const inTestGroup = (key: string) => {
   return getTestGroups().includes(key)
 }
+
+export default inTestGroup
