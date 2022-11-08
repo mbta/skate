9b0ef31cf2bafec45f7bc05295b13998e2fe8f7d
Kayla Firestack <kfirestack@mbta.com>
2022-11-08 09:22:42 -0500

    refactor(test/userTestGroups): dry out test by refactoring to use test.each

--- assets/tests/userTestGroups.test.ts
+++ assets/tests/userTestGroups.test.ts
@@ -5,52 +5,7 @@ import inTestGroup from "../src/userTestGroups"
 jest.mock("appData")
 
 describe("User Test Groups API", () => {
-  test("raise error when given wrong json data type", () => {
-    ;(appData as jest.Mock)
-      .mockImplementationOnce(() => ({
-        // Wrong data type
-        userTestGroups: null,
-      }))
-      .mockImplementationOnce(() => ({
-        // Wrong data type
-        userTestGroups: "1.0",
-      }))
-      .mockImplementation(() => ({
-        // Wrong data type
-        userTestGroups: "{}",
-      }))
-
-    // Start with no groups or keys
-    expect(() => inTestGroup("non-existent-group")).toThrow(StructError)
-    expect(() => inTestGroup("non-existent-group")).toThrow(StructError)
-    expect(() => inTestGroup("non-existent-group")).toThrow(StructError)
-  })
-
-  test("raise error when given invalid json", () => {
-    ;(appData as jest.Mock)
-      .mockImplementationOnce(() => ({
-        userTestGroups: "asdf1234!@#$",
-      }))
-      .mockImplementationOnce(() => ({
-        userTestGroups: [1, 2, 3],
-      }))
-      .mockImplementation(() => ({
-        userTestGroups: "",
-      }))
-
-    expect(() => inTestGroup("non-existent-group")).toThrow(SyntaxError)
-    expect(() => inTestGroup("non-existent-group")).toThrow(SyntaxError)
-    expect(() => inTestGroup("non-existent-group")).toThrow(SyntaxError)
-  })
-
-  test("confirm user is not in test group when missing user test group information", () => {
-    // Missing data key
-    ;(appData as jest.Mock).mockImplementation(() => {})
-
-    expect(inTestGroup("non-existent-group")).toBe(false)
-  })
-
-  test("confirm user in multiple test groups", () => {
+  test("returns true if user is in test group", () => {
     const group_1 = "user-test-group-1"
     const group_2 = "user-test-group-2"
     ;(appData as jest.Mock).mockImplementation(() => ({
@@ -61,4 +16,45 @@ describe("User Test Groups API", () => {
     expect(inTestGroup(group_2)).toBe(true)
     expect(inTestGroup(group_1 + group_2)).toBe(false)
   })
+
+  test.each([{}, { userTestGroup: undefined }])(
+    "returns false if the test group information is not found: case %#",
+    (mock) => {
+      // Missing data key
+      ;(appData as jest.Mock).mockImplementation(() => mock)
+
+      expect(inTestGroup("non-existent-group")).toBe(false)
+    }
+  )
+
+  test.each([
+    // JSON.parse should throw when given invalid json
+    { expectedError: SyntaxError, mockTestGroupData: "" },
+    { expectedError: SyntaxError, mockTestGroupData: "test 1234 !@#$ []{}" },
+    { expectedError: SyntaxError, mockTestGroupData: [1, 2, 3] },
+
+    // Superstruct catches valid json but invalid types
+    { expectedError: StructError, mockTestGroupData: null },
+    { expectedError: StructError, mockTestGroupData: JSON.stringify(null) },
+    { expectedError: StructError, mockTestGroupData: true },
+    { expectedError: StructError, mockTestGroupData: JSON.stringify(true) },
+    {
+      expectedError: StructError,
+      mockTestGroupData: JSON.stringify([1, 2, 3]),
+    },
+    {
+      expectedError: StructError,
+      mockTestGroupData: JSON.stringify({ irrelevant: "keys" }),
+    },
+  ])(
+    "raise error when test group data is not the correct type: case %#",
+    ({ mockTestGroupData, expectedError: expectedError }) => {
+      ;(appData as jest.Mock).mockImplementationOnce(() => ({
+        userTestGroups: mockTestGroupData,
+      }))
+      expect(() => inTestGroup("non-existent-test-group")).toThrowError(
+        expectedError
+      )
+    }
+  )
 })
