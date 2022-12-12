3b2164e02515af0ce1645d1a8065c0ed53a07951
Kayla Firestack <kfirestack@mbta.com>
2022-12-12 10:26:43 -0500

    doc(skate-web/authenticated-channel): write docs for new module, update spec to match upstream function

--- lib/skate_web/authenticated_channel.ex
+++ lib/skate_web/authenticated_channel.ex
@@ -1,19 +1,55 @@
 defmodule SkateWeb.AuthenticatedChannel do
-  @doc """
+  @moduledoc """
+  A `use` macro and `@behaviour` which implements `c:Phoenix.Channel.join/3` and
+  calls `c:SkateWeb.AuthenticatedChannel.join_authenticated/3` when socket is
+  authenticated.
 
+  This ensures that the token associated with the `socket` passed to
+  `c:Phoenix.Channel.join/3` is valid before forwarding the call to
+  `c:SkateWeb.AuthenticatedChannel.join_authenticated/3`.
+
+  ## Examples
+  Simple usage of `SkateWeb.AuthenticatedChannel`
+  ```
+  defmodule MyChannel do
+    use SkateWeb.AuthenticatedChannel
+
+    @impl SkateWeb.AuthenticatedChannel
+    def join_authenticated("room:lobby", payload, socket) do
+      # The token associated with this socket is valid within this function
+      {:ok, %{data: "payload"}, socket}
+    end
+  end
+  ```
   """
-  @callback join_authenticated(any, any, Phoenix.Socket.t()) ::
-              {:error, :not_authenticated | %{message: <<_::64, _::_*8>>}}
-              | {:ok, %{data: any}, any}
 
+  @doc """
+  Handle __authenticated__ socket channel joins by topic.
+
+  see: `c:Phoenix.Channel.join/3` for relevant docs.
+  """
+  @callback join_authenticated(
+              topic :: binary,
+              payload :: Phoenix.Channel.payload(),
+              socket :: Phoenix.Socket.t()
+            ) ::
+              {:ok, Phoenix.Socket.t()}
+              | {:ok, reply :: Phoenix.Channel.payload(), Phoenix.Socket.t()}
+              | {:error, reason :: map()}
+
+  @doc """
+  Macro which imports the `SkateWeb.AuthenticatedChannel` behaviour and
+  implements `c:Phoenix.Channel.join/3` for
+  `c:SkateWeb.AuthenticatedChannel.join_authenticated/3`
+  """
   defmacro __using__(_) do
     quote do
       @behaviour SkateWeb.AuthenticatedChannel
 
       @impl Phoenix.Channel
-      def join(topic, message, socket) do
+      def join(topic, payload, socket) do
         if SkateWeb.ChannelAuth.valid_token?(socket) do
-          join_authenticated(topic, message, socket)
+          join_authenticated(topic, payload, socket)
         else
           {:error, :not_authenticated}
         end
