263c3928dde5492d3cd90132f58a2256c91de779
Eddie Maldonado <emaldonado@mbta.com>
2022-12-16 13:21:38 -0500

    Retry failed Geonames requests (#1845)
    * chore(Geonames): log request duration
    
    * feat(Geonames): retry failed responses
    
    * test(Geonames): factor out dummy JSON response

--- lib/geonames.ex
+++ lib/geonames.ex
@@ -12,8 +12,8 @@ defmodule Geonames do
     end
   end
 
-  @spec get(String.t(), String.t()) :: map() | nil
-  defp get(latitude, longitude) do
+  @spec get(String.t(), String.t(), boolean()) :: map() | nil
+  defp get(latitude, longitude, retry? \\ true) do
     geonames_url_base = Application.get_env(:skate, :geonames_url_base)
     geonames_token = Application.get_env(:skate, :geonames_token)
     token_param = if geonames_token, do: "&token=#{geonames_token}", else: ""
@@ -28,19 +28,29 @@ defmodule Geonames do
         url
       end
 
-    case HTTPoison.get(url) do
+    {time, result} = :timer.tc(HTTPoison, :get, [url])
+
+    time_in_ms = time / :timer.seconds(1)
+
+    case result do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        Logger.info("#{__MODULE__} got_intersection_response url=#{sanitized_url}")
+        Logger.info(
+          "#{__MODULE__} got_intersection_response url=#{sanitized_url} time=#{time_in_ms}"
+        )
 
         body
         |> Jason.decode!(strings: :copy)
 
       response ->
         Logger.warn(
-          "#{__MODULE__} unexpected_response url=#{sanitized_url} response=#{inspect(response)}"
+          "#{__MODULE__} unexpected_response url=#{sanitized_url} response=#{inspect(response)} time=#{time_in_ms} retry=#{retry?}"
         )
 
-        nil
+        if retry? do
+          get(latitude, longitude, false)
+        else
+          nil
+        end
     end
   end
 
--- test/geonames_test.exs
+++ test/geonames_test.exs
@@ -2,8 +2,31 @@ defmodule GeonamesTest do
   use ExUnit.Case, async: true
   import Test.Support.Helpers
   import ExUnit.CaptureLog
+  import Plug.Conn
 
   alias Geonames
+
+  @json %{
+    "credits" => "1.0",
+    "intersection" => %{
+      "mtfcc1" => "S1400",
+      "mtfcc2" => "S1400",
+      "adminCode1" => "CA",
+      "lng" => "-122.180842",
+      "distance" => "0.08",
+      "bearing" => "242",
+      "placename" => "Menlo Park",
+      "street1Bearing" => "213",
+      "street2Bearing" => "303",
+      "adminName2" => "San Mateo",
+      "postalcode" => "94025",
+      "countryCode" => "US",
+      "street1" => "Roble Ave",
+      "street2" => "Curtis St",
+      "adminName1" => "California",
+      "lat" => "37.450649"
+    }
+  }
 
   describe "nearest_intersection" do
     test "returns nil if the request fails and logs" do
@@ -43,33 +66,11 @@ defmodule GeonamesTest do
       bypass = Bypass.open()
       reassign_env(:skate, :geonames_url_base, "http://localhost:#{bypass.port}")
 
-      json = %{
-        "credits" => "1.0",
-        "intersection" => %{
-          "mtfcc1" => "S1400",
-          "mtfcc2" => "S1400",
-          "adminCode1" => "CA",
-          "lng" => "-122.180842",
-          "distance" => "0.08",
-          "bearing" => "242",
-          "placename" => "Menlo Park",
-          "street1Bearing" => "213",
-          "street2Bearing" => "303",
-          "adminName2" => "San Mateo",
-          "postalcode" => "94025",
-          "countryCode" => "US",
-          "street1" => "Roble Ave",
-          "street2" => "Curtis St",
-          "adminName1" => "California",
-          "lat" => "37.450649"
-        }
-      }
-
       log =
         capture_log(
           [level: :info],
           fn ->
-            Bypass.expect(bypass, fn conn -> Plug.Conn.resp(conn, 200, Jason.encode!(json)) end)
+            Bypass.expect(bypass, fn conn -> Plug.Conn.resp(conn, 200, Jason.encode!(@json)) end)
             assert Geonames.nearest_intersection("40", "-70") == "Roble Ave & Curtis St"
           end
         )
@@ -77,16 +78,98 @@ defmodule GeonamesTest do
       assert log =~ "got_intersection_response"
     end
 
+    test "retries request on failure" do
+      set_log_level(:info)
+
+      bypass = Bypass.open()
+      reassign_env(:skate, :geonames_url_base, "http://localhost:#{bypass.port}")
+
+      {:ok, agent} = response_agent()
+
+      agent
+      |> add_response(fn conn ->
+        send_resp(conn, 500, "")
+      end)
+      |> add_response(fn conn ->
+        send_resp(conn, 200, Jason.encode!(@json))
+      end)
+
+      log =
+        capture_log(
+          [level: :info],
+          fn ->
+            Bypass.expect(bypass, fn conn -> agent_response(agent, conn) end)
+
+            assert Geonames.nearest_intersection("40", "-70") == "Roble Ave & Curtis St"
+          end
+        )
+
+      assert log =~ "retry=true"
+      assert log =~ "got_intersection_response"
+    end
+
+    test "only retries request once" do
+      bypass = Bypass.open()
+      reassign_env(:skate, :geonames_url_base, "http://localhost:#{bypass.port}")
+
+      {:ok, agent} = response_agent()
+
+      agent
+      |> add_response(fn conn ->
+        send_resp(conn, 500, "")
+      end)
+      |> add_response(fn conn ->
+        send_resp(conn, 500, "")
+      end)
+      |> add_response(fn conn ->
+        send_resp(conn, 200, Jason.encode!(@json))
+      end)
+
+      log =
+        capture_log(fn ->
+          Bypass.expect(bypass, fn conn -> agent_response(agent, conn) end)
+
+          assert Geonames.nearest_intersection("40", "-70") == nil
+        end)
+
+      assert log =~ "unexpected_response"
+      assert log =~ "retry=true"
+      assert log =~ "retry=false"
+    end
+
     test "returns nil if there is no nearby intersection" do
       bypass = Bypass.open()
       reassign_env(:skate, :geonames_url_base, "http://localhost:#{bypass.port}")
 
-      json = %{
+      empty_json = %{
         "credits" => "1.0"
       }
 
-      Bypass.expect(bypass, fn conn -> Plug.Conn.resp(conn, 200, Jason.encode!(json)) end)
+      Bypass.expect(bypass, fn conn -> Plug.Conn.resp(conn, 200, Jason.encode!(empty_json)) end)
       assert Geonames.nearest_intersection("40", "-70") == nil
     end
+  end
+
+  defp response_agent do
+    Agent.start_link(fn -> [] end)
+  end
+
+  defp add_response(agent, fun) do
+    :ok = Agent.update(agent, fn funs -> funs ++ [fun] end)
+    agent
+  end
+
+  defp agent_response(agent, conn) do
+    fun =
+      Agent.get_and_update(agent, fn
+        [] -> {&default_response/1, []}
+        [fun | funs] -> {fun, funs}
+      end)
+
+    fun.(conn)
+  end
+
+  defp default_response(conn) do
+    send_resp(conn, 200, "agent")
   end
 end
