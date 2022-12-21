28d59bcec33978bb01860106bf6e311e63ec69bb
Maldonado <emaldonado@mbta.com>
2022-12-21 15:32:15 -0500

    test: more thorough use of factories

--- assets/tests/factories/shape_data.ts
+++ assets/tests/factories/shape_data.ts
@@ -1,8 +1,9 @@
 import { Factory } from "fishery"
 import { ShapeData } from "../../src/models/shapeData"
+import stopFactory from "./stop"
 
 export default Factory.define<ShapeData>(({ sequence }) => ({
   id: `shape${sequence}`,
   points: [{ shape_id: `shape${sequence}`, sequence: 1, lat: 0, lon: 0 }],
-  stops: [{ id: "1", name: "Some Stop", lat: 0, lon: 0 }],
+  stops: [stopFactory.build({ id: `stop${sequence}` })],
 }))
