4f9a974fbfc73da8d84200d9a1736d510dbee6b4
Kayla Firestack <kfirestack@mbta.com>
2022-12-27 21:43:08 -0700

    update(ts/tests/components/routevariantname): use vehicleFactory per test

--- assets/tests/components/routeVariantName.test.tsx
+++ assets/tests/components/routeVariantName.test.tsx
@@ -4,67 +4,13 @@ import "@testing-library/jest-dom"
 import routeFactory from "../factories/route"
 import { RouteVariantName } from "../../src/components/routeVariantName"
 import { RoutesProvider } from "../../src/contexts/routesContext"
-import { Vehicle } from "../../src/realtime"
 import { Route } from "../../src/schedule"
 import vehicleFactory from "../factories/vehicle"
 
-const vehicle: Vehicle = vehicleFactory.build({
-  id: "v1",
-  label: "v1-label",
-  runId: "run-1",
-  timestamp: 123,
-  latitude: 0,
-  longitude: 0,
-  directionId: 0,
-  routeId: "39",
-  tripId: "t1",
-  headsign: "Forest Hills",
-  viaVariant: "X",
-  operatorId: "op1",
-  operatorFirstName: "WILL",
-  operatorLastName: "SMITH",
-  operatorLogonTime: new Date("2018-08-15T13:38:21.000Z"),
-  bearing: 33,
-  blockId: "block-1",
-  previousVehicleId: "v2",
-  scheduleAdherenceSecs: 0,
-  isShuttle: false,
-  isOverload: false,
-  isOffCourse: false,
-  isRevenue: true,
-  layoverDepartureTime: null,
-  dataDiscrepancies: [
-    {
-      attribute: "trip_id",
-      sources: [
-        {
-          id: "swiftly",
-          value: "swiftly-trip-id",
-        },
-        {
-          id: "busloc",
-          value: "busloc-trip-id",
-        },
-      ],
-    },
-  ],
-  stopStatus: {
-    stopId: "s1",
-    stopName: "Stop Name",
-  },
-  timepointStatus: {
-    fractionUntilTimepoint: 0.5,
-    timepointId: "tp1",
-  },
-  scheduledLocation: null,
-  routeStatus: "on_route",
-  endOfTripType: "another_trip",
-  blockWaivers: [],
-  crowding: null,
-})
 
 describe("RouteVariantName", () => {
   test("renders for a vehicle with variant and headsign", () => {
+    const vehicle = vehicleFactory.build()
 
     render(<RouteVariantName vehicle={vehicle} />)
 
@@ -74,11 +20,10 @@ describe("RouteVariantName", () => {
   })
 
   test("renders for a vehicle missing variant and headsign", () => {
-    const testVehicle: Vehicle = {
-      ...vehicle,
+    const testVehicle = vehicleFactory.build({
       headsign: null,
       viaVariant: null,
-    }
+    })
 
     render(<RouteVariantName vehicle={testVehicle} />)
 
@@ -86,11 +31,10 @@ describe("RouteVariantName", () => {
   })
 
   test("doesn't show underscore variant character", () => {
-    const testVehicle: Vehicle = {
-      ...vehicle,
+    const testVehicle = vehicleFactory.build({
       headsign: null,
       viaVariant: "_",
-    }
+    })
 
     render(<RouteVariantName vehicle={testVehicle} />)
 
@@ -98,10 +42,9 @@ describe("RouteVariantName", () => {
   })
 
   test("renders a static label for a shuttle", () => {
-    const testVehicle: Vehicle = {
-      ...vehicle,
+    const testVehicle = vehicleFactory.build({
       isShuttle: true,
-    }
+    })
 
     render(<RouteVariantName vehicle={testVehicle} />)
 
@@ -109,6 +52,7 @@ describe("RouteVariantName", () => {
   })
 
   test("uses route name if available", () => {
+    const vehicle = vehicleFactory.build()
     const route: Route = routeFactory.build({
       id: "39",
       name: "ThirtyNine",
