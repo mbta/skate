e29f94d80851e423a93572fb375234a54f06526b
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2022-12-29 15:50:53 -0500

    feat(VPP): Link to open in search map

---
+++ assets/src/components/propertiesPanel/miniMap.tsx
@@ -0,0 +1,60 @@
+import React, { useContext } from "react"
+import { Link } from "react-router-dom"
+import { StateDispatchContext } from "../../contexts/stateDispatchContext"
+import { useStations } from "../../hooks/useStations"
+import { Vehicle, VehicleId } from "../../realtime"
+import { Shape, Stop } from "../../schedule"
+import { setSelectedVehicle } from "../../state/searchPageState"
+import inTestGroup, { MAP_BETA_GROUP_NAME } from "../../userInTestGroup"
+import { mapModeForUser } from "../../util/mapMode"
+import Map from "../map"
+
+const SearchMapLink = ({ vehicleId }: { vehicleId: VehicleId }) => {
+  const [_state, dispatch] = useContext(StateDispatchContext)
+
+  return (
+    <Link
+      className="m-vehicle-properties-panel__map-open-link leaflet-bar"
+      to={mapModeForUser().path}
+      onClick={() => {
+        dispatch(setSelectedVehicle(vehicleId))
+      }}
+    >
+      Open Map
+    </Link>
+  )
+}
+
+const MiniMap = ({
+  vehicle,
+  shapes,
+  routeVehicles,
+}: {
+  vehicle: Vehicle
+  shapes: Shape[]
+  routeVehicles: Vehicle[]
+}) => {
+  const stations: Stop[] | null = useStations()
+
+  return inTestGroup(MAP_BETA_GROUP_NAME) ? (
+    <Map
+      selectedVehicleId={vehicle.id}
+      vehicles={[vehicle]}
+      shapes={shapes}
+      secondaryVehicles={routeVehicles}
+      stations={stations}
+    >
+      <SearchMapLink vehicleId={vehicle.id} />
+    </Map>
+  ) : (
+    <Map
+      selectedVehicleId={vehicle.id}
+      vehicles={[vehicle]}
+      shapes={shapes}
+      secondaryVehicles={routeVehicles}
+      stations={stations}
+    />
+  )
+}
+
+export default MiniMap
---
+++ assets/tests/components/propertiesPanel/miniMap.test.tsx
@@ -0,0 +1,65 @@
+import React from "react"
+import { Vehicle } from "../../../src/realtime"
+import vehicleFactory from "../../factories/vehicle"
+import { render, screen } from "@testing-library/react"
+import "@testing-library/jest-dom"
+import getTestGroups from "../../../src/userTestGroups"
+import { MAP_BETA_GROUP_NAME } from "../../../src/userInTestGroup"
+import MiniMap from "../../../src/components/propertiesPanel/miniMap"
+import "@testing-library/jest-dom"
+import { MemoryRouter } from "react-router"
+import { StateDispatchProvider } from "../../../src/contexts/stateDispatchContext"
+import { initialState } from "../../../src/state"
+import { setSelectedVehicle } from "../../../src/state/searchPageState"
+import userEvent from "@testing-library/user-event"
+
+const vehicle: Vehicle = vehicleFactory.build()
+
+jest.mock("userTestGroups", () => ({
+  __esModule: true,
+  default: jest.fn(() => []),
+}))
+
+const originalScrollTo = global.scrollTo
+// Clicking/moving map calls scrollTo under the hood
+jest.spyOn(global, "scrollTo").mockImplementation(jest.fn())
+
+afterAll(() => {
+  global.scrollTo = originalScrollTo
+})
+
+describe("MiniMap", () => {
+  describe("For users in map test group", () => {
+    beforeAll(() => {
+      ;(getTestGroups as jest.Mock).mockReturnValueOnce([MAP_BETA_GROUP_NAME])
+    })
+    test("Map includes link to open vehicle in search map page", async () => {
+      const mockDispatch = jest.fn()
+
+      render(
+        <MemoryRouter initialEntries={["/"]}>
+          <StateDispatchProvider state={initialState} dispatch={mockDispatch}>
+            <MiniMap vehicle={vehicle} routeVehicles={[]} shapes={[]} />
+          </StateDispatchProvider>
+        </MemoryRouter>
+      )
+      expect(screen.getByText("Open Map")).toBeInTheDocument()
+      await userEvent.click(screen.getByText("Open Map"))
+      expect(mockDispatch).toHaveBeenCalledWith(setSelectedVehicle(vehicle.id))
+    })
+  })
+
+  describe("For users not in map test group", () => {
+    beforeAll(() => {
+      ;(getTestGroups as jest.Mock).mockReturnValue([])
+    })
+    test("Map does not include link to open vehicle in search map page", () => {
+      render(
+        <MemoryRouter initialEntries={["/"]}>
+          <MiniMap vehicle={vehicle} routeVehicles={[]} shapes={[]} />{" "}
+        </MemoryRouter>
+      )
+      expect(screen.queryByText("Open Map")).not.toBeInTheDocument()
+    })
+  })
+})
