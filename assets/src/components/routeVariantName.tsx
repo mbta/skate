2851f8d45c920a557b400f44788898df6c116882
Kayla Firestack <kfirestack@mbta.com>
2022-12-29 17:54:22 -0500

    feat(ts/components/routeVariantName): add RouteVariantName2 to prevent snapshot changes

--- assets/src/components/routeVariantName.tsx
+++ assets/src/components/routeVariantName.tsx
@@ -22,3 +22,27 @@ export const RouteVariantName = ({ vehicle }: { vehicle: VehicleOrGhost }) => {
     </div>
   )
 }
+
+export const RouteVariantName2 = ({ vehicle }: { vehicle: VehicleOrGhost }) => {
+  const route = useRoute(vehicle.routeId)
+  if (isVehicle(vehicle) && vehicle.isShuttle) {
+    return <div className="m-route-variant-name">Shuttle</div>
+  }
+
+  const { routeId, viaVariant, headsign } = vehicle
+  const viaVariantFormatted = viaVariant && viaVariant !== "_" ? viaVariant : ""
+  const headsignFormatted = headsign ? `${headsign}` : ""
+
+  return (
+    <output aria-label="Route Variant Name" className="m-route-variant-name">
+      <output
+        aria-label="Route Variant"
+        className="m-route-variant-name__route-id"
+      >
+        {`${route?.name || routeId}_${viaVariantFormatted}`}
+      </output>
+      &nbsp;
+      <output aria-label="Route Headsign">{headsignFormatted}</output>
+    </output>
+  )
+}
--- assets/tests/components/routeVariantName.test.tsx
+++ assets/tests/components/routeVariantName.test.tsx
@@ -2,11 +2,13 @@ import React from "react"
 import { render, screen } from "@testing-library/react"
 import "@testing-library/jest-dom"
 import routeFactory from "../factories/route"
-import { RouteVariantName } from "../../src/components/routeVariantName"
+import {
+  RouteVariantName,
+  RouteVariantName2,
+} from "../../src/components/routeVariantName"
 import { RoutesProvider } from "../../src/contexts/routesContext"
 import { Route } from "../../src/schedule"
 import vehicleFactory from "../factories/vehicle"
-
 
 describe("RouteVariantName", () => {
   test("renders for a vehicle with variant and headsign", () => {
@@ -66,5 +68,71 @@ describe("RouteVariantName", () => {
     expect(screen.getByTestId("variant-name")).toHaveTextContent(
       "ThirtyNine_X Forest Hills"
     )
+  })
+})
+
+describe("RouteVariantName2", () => {
+  test("renders for a vehicle with variant and headsign", () => {
+    const vehicle = vehicleFactory.build()
+
+    render(<RouteVariantName2 vehicle={vehicle} />)
+
+    expect(
+      screen.getByRole("status", { name: "Route Variant Name" })
+    ).toHaveTextContent("39_X Forest Hills")
+  })
+
+  test("renders for a vehicle missing variant and headsign", () => {
+    const testVehicle = vehicleFactory.build({
+      headsign: null,
+      viaVariant: null,
+    })
+
+    render(<RouteVariantName2 vehicle={testVehicle} />)
+
+    expect(
+      screen.getByRole("status", { name: "Route Variant Name" })
+    ).toHaveTextContent("39_")
+  })
+
+  test("doesn't show underscore variant character", () => {
+    const testVehicle = vehicleFactory.build({
+      headsign: null,
+      viaVariant: "_",
+    })
+
+    render(<RouteVariantName2 vehicle={testVehicle} />)
+
+    expect(
+      screen.getByRole("status", { name: "Route Variant Name" })
+    ).toHaveTextContent("39_")
+  })
+
+  test("renders a static label for a shuttle", () => {
+    const testVehicle = vehicleFactory.build({
+      isShuttle: true,
+    })
+
+    render(<RouteVariantName2 vehicle={testVehicle} />)
+
+    expect(screen.getByText("Shuttle")).toBeInTheDocument()
+  })
+
+  test("uses route name if available", () => {
+    const vehicle = vehicleFactory.build()
+    const route: Route = routeFactory.build({
+      id: "39",
+      name: "ThirtyNine",
+    })
+
+    render(
+      <RoutesProvider routes={[route]}>
+        <RouteVariantName2 vehicle={vehicle} />
+      </RoutesProvider>
+    )
+
+    expect(
+      screen.getByRole("status", { name: "Route Variant Name" })
+    ).toHaveTextContent("ThirtyNine_X Forest Hills")
   })
 })
