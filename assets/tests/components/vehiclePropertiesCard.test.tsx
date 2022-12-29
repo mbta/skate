f4419586283bbd3f5d88dc8543db99052168e531
Kayla Firestack <kfirestack@mbta.com>
2022-12-29 09:18:33 -0500

    feat(ts/components/vpc): add tests

--- assets/tests/components/vehiclePropertiesCard.test.tsx
+++ assets/tests/components/vehiclePropertiesCard.test.tsx
@@ -1,8 +1,6 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
 import "@testing-library/jest-dom"
-import { Vehicle } from "../../src/realtime"
-import { Route } from "../../src/schedule"
 import * as dateTime from "../../src/util/dateTime"
 import vehicleFactory from "../factories/vehicle"
 import routeFactory from "../factories/route"
@@ -11,95 +9,157 @@ import userEvent from "@testing-library/user-event"
 import VehiclePropertiesCard from "../../src/components/vehiclePropertiesCard"
 import { useNearestIntersection } from "../../src/hooks/useNearestIntersection"
 import { RoutesProvider } from "../../src/contexts/routesContext"
-jest
-  .spyOn(dateTime, "now")
-  .mockImplementation(() => new Date("2018-08-15T17:41:21.000Z"))
 
-jest.spyOn(Date, "now").mockImplementation(() => 234000)
 
-const route: Route = routeFactory.build({ id: "39", name: "39" })
-const vehicle: Vehicle = vehicleFactory.build({ routeId: "39" })
-
-const intersection = "Massachusetts Ave @ Marlborough St"
 jest.mock("../../src/hooks/useNearestIntersection", () => ({
   __esModule: true,
   useNearestIntersection: jest.fn(() => null),
 }))
 
-describe("VehiclePropertiesCard", () => {
-  describe("todo: card interactions", () => {
-    test.todo("The VPC can be closed by clicking the 'X' button. (kinda done)")
-    test.todo("Selecting another vehicle does not render old data")
-    test.todo("Selecting another vehicle renders with new information")
-    test.todo("when vehicle prop changes, card rerenders?")
+describe("<VehiclePropertiesCard/>", () => {
+  describe("logic constraints", () => {
+    test("when `close` button is clicked, should fire `onClose` callback", async () => {
+      const vehicle = vehicleFactory.build()
+      const onClose = jest.fn()
+      render(
+        <VehiclePropertiesCard vehicle={vehicle} onClose={onClose} />
+      )
+      await userEvent.click(screen.getByTitle("Close"))
+
+      expect(onClose).toHaveBeenCalled()
+    })
+
+    test("when rerendered, should display latest intersection", () => {
+      const intersection = "Massachusetts Ave @ 1"
+      const intersection2 = "Massachusetts Ave @ 2"
+        ; (useNearestIntersection as jest.Mock)
+          .mockReturnValueOnce(intersection)
+          // .mockReturnValueOnce(null)
+          .mockReturnValueOnce(intersection2)
+
+      const vehicle = vehicleFactory.build()
+
+      const { rerender } = render(<VehiclePropertiesCard vehicle={vehicle} onClose={jest.fn()} />)
+
+      expect(screen.getByRole("status", { name: /Current Location/i })).toHaveTextContent(intersection)
+
+      rerender(<VehiclePropertiesCard vehicle={vehicle} onClose={jest.fn()} />)
+      // expect(screen.getByRole("status", { name: /Current Location/i })).toHaveTextContent("")
+      expect(screen.getByRole("status", { name: /Current Location/i })).toHaveTextContent(intersection2)
+
+    })
+
+    test("when rerendered, should display latest data", () => {
+      const vehicle = vehicleFactory.build()
+      const vehicle2 = vehicleFactory.build()
+
+      const { rerender } = render(<VehiclePropertiesCard vehicle={vehicle} onClose={jest.fn()} />)
+
+      expect(screen.getByRole("cell", { name: /run/i })).toHaveTextContent(vehicle.runId!)
+      expect(screen.getByRole("cell", { name: /vehicle/i })).toHaveTextContent(vehicle.label)
+
+      rerender(<VehiclePropertiesCard vehicle={vehicle2} onClose={jest.fn()} />)
+
+      expect(screen.getByRole("cell", { name: /vehicle/i })).toHaveTextContent(vehicle2.label)
+      expect(screen.getByRole("cell", { name: /run/i })).toHaveTextContent(vehicle2.runId!)
+    })
   })
-  // The information displayed in the VPC, outlined in the Figma file,
-  // corresponds with the selected vehicle, following guidelines for component
-  // variants (default, no location data available, invalid bus, ghost bus).
-  describe("renders bus information according to the figma file", () => {
-    test.todo("Default > Location Available") // {
-    // Header
-    // Vehicle Summary
-    // "Vehicle Status"
-    // Location Information
-    // }
-    test.todo("Default > Location Not Available") // {
-    // shows "Exact location cannot be determined" when unavailable
-    // }
-    test.todo("Invalid Bus") // {
-    // Show invalid in Adherence Info
-    // Use correct icon
-    // }
-    test.todo("Ghost Bus") // {
+
+  describe("bus types and data scenarios rendering", () => {
+    describe("vehicle default, not invalid or ghost", () => {
+      test("has location available, should render all design fields", () => {
+        jest.spyOn(Date, "now").mockImplementation(() => 234000)
+        jest
+          .spyOn(dateTime, "now")
+          .mockImplementation(() => new Date("2018-08-15T17:41:21.000Z"))
+
+
+        const vehicle = vehicleFactory.build()
+        const route = routeFactory.build({ id: vehicle.routeId!, name: vehicle.routeId! })
+
+        const intersection = "Massachusetts Ave @ Marlborough St"
+          ; (useNearestIntersection as jest.Mock).mockImplementationOnce(
+            () => intersection
+          )
+
+        render(
+          <RoutesProvider routes={[route]}>
+            <VehiclePropertiesCard vehicle={vehicle} onClose={jest.fn()} />
+          </RoutesProvider>
+        )
+
+        // -- Assert
+        // - Header Bar
+        expect(screen.getByRole("status", { name: "Last Updated Time" }))
+          .toHaveTextContent("Updated 111 sec ago")
+        expect(
+          screen.getByRole("button", { name: /close/i })
+        ).toBeInTheDocument()
+
+        // - Vehicle Route Summary
+        // Vehicle Icon
+        // ToDo: make component accessible
+        expect(screen.getByTitle(/vehicle status icon/i)).toBeInTheDocument()
+        // expect(screen.getByRole("presentation", { description: /vehicle status icon/i })).toBeInTheDocument()
+
+        expect(screen.getByRole("status", { name: "Vehicle Adherence" }))
+          .toHaveTextContent(/on time \(0 min early\)/i)
+        expect(screen.getByRole("status", { name: "Route Direction" }))
+          .toHaveTextContent(/outbound/i)
+        expect(screen.getByRole("status", { name: "Route Variant Name" }))
+          .toHaveTextContent("39_X Forest Hills")
+
+        // - Vehicle Work Info
+        const { operatorFirstName, operatorLastName, operatorId } = vehicle;
+        // Run      | Run ID
+        expect(screen.getByRole("cell", { name: /run/ }))
+          .toHaveTextContent(vehicle.runId!)
+        // Vehicle  | Vehicle ID
+        expect(screen.getByRole("cell", { name: /vehicle/ }))
+          .toHaveTextContent(vehicle.label)
+        // Operator | Operator First, Last, #BadgeID
+        expect(screen.getByRole("cell", { name: /operator/ }))
+          .toHaveTextContent(`${operatorFirstName} ${operatorLastName} #${operatorId}`)
+
+        // - Vehicle Location Information
+        expect(screen.getByRole("status", { name: "Current Location" }))
+          .toHaveTextContent(intersection)
+        expect(
+          screen.getByRole("link", { name: /street view/i })
+        ).toBeInTheDocument()
+      })
+
+      test("when location not available, should show `exact location cannot be determined` backup text", () => {
+        const vehicle = vehicleFactory.build()
+          ; (useNearestIntersection as jest.Mock).mockImplementationOnce(() => null)
+
+        render(<VehiclePropertiesCard vehicle={vehicle} onClose={jest.fn()} />)
+
+        expect(screen.getByRole("status", { name: "Current Location" }))
+          .toHaveTextContent("Exact location cannot be determined")
+      })
+    })
+
+    test("vehicle is off course, should render invalid bus design", () => {
+      const vehicle = vehicleFactory.build({ isOffCourse: true })
+
+      render(<VehiclePropertiesCard vehicle={vehicle} onClose={jest.fn()} />)
+
+      // Show `invalid` in Adherence Info
+      expect(screen.getByRole("status", { name: /Vehicle Adherence/ }))
+        .toHaveTextContent(/invalid/i)
+
+      // Use correct icon // TODO: make accessible and ensure icon is correct
+      expect(screen.getByTitle(/vehicle status icon/i)).toBeInTheDocument()
+    })
+
+    // - Ghost Bus is not Displayable on map
+    // test.todo("vehicle is ghost bus, should render ghost bus design") //, {
     // Display "Ghost Bus or Dropped Trip" in `LastUpdated`
     // Do not Display the location information block
     // Display `N/A` in invalid labels
     // Display Ghost Icon and `N/A`
     // Do not display adherence information
-    // }
-  })
-  test("displays information about the vehicle", () => {
-    ;(useNearestIntersection as jest.Mock).mockImplementationOnce(
-      () => intersection
-    )
-    render(
-      <RoutesProvider routes={[route]}>
-        <VehiclePropertiesCard vehicle={vehicle} onClose={jest.fn()} />
-      </RoutesProvider>
-    )
-
-    expect(screen.getByText("Outbound")).toBeInTheDocument()
-    expect(screen.getByText("39_X")).toBeInTheDocument()
-    expect(screen.getByText("Forest Hills")).toBeInTheDocument()
-    expect(screen.getByText("Current Location")).toBeInTheDocument()
-    expect(screen.getByText(intersection)).toBeInTheDocument()
-    expect(screen.getByText("111s ago")).toBeInTheDocument()
-  })
-
-  test("location not displayed if not known", () => {
-    render(<VehiclePropertiesCard vehicle={vehicle} onClose={jest.fn()} />)
-
-    ;(useNearestIntersection as jest.Mock).mockImplementationOnce(() => null)
-
-    expect(screen.queryByText("Current Location")).not.toBeInTheDocument()
-  })
-
-  // Alt Name: Figma: The VPC can be closed by clicking the 'X' button.
-  test("clicking close button calls on close", async () => {
-    const mockOnClose = jest.fn()
-    render(
-      <VehiclePropertiesCard vehicle={vehicle} onClose={mockOnClose} />
-    )
-    await userEvent.click(screen.getByTitle("Close"))
-
-    expect(mockOnClose).toHaveBeenCalled()
-  })
-
-  test("has link to street view", async () => {
-    render(<VehiclePropertiesCard vehicle={vehicle} onClose={jest.fn()} />)
-
-    expect(
-      screen.getByRole("link", { name: "Go to Street View" })
-    ).toBeInTheDocument()
+    // })
   })
 })
