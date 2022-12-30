0a89f79f251bd90b067f5943054ade7ea13340cb
Kayla Firestack <kfirestack@mbta.com>
2022-12-30 09:18:41 -0500

    feat(ts/components/vpc): rewrite

--- assets/src/components/vehiclePropertiesCard.tsx
+++ assets/src/components/vehiclePropertiesCard.tsx
@@ -1,83 +1,53 @@
-import React, { ReactElement, useContext } from "react"
+import React, { ReactNode, useContext, useId } from "react"
 import { useRoute } from "../contexts/routesContext"
 import { StateDispatchContext } from "../contexts/stateDispatchContext"
+import { className } from "../helpers/dom"
 import vehicleLabel from "../helpers/vehicleLabel"
 import { useCurrentTimeSeconds } from "../hooks/useCurrentTime"
 import { useNearestIntersection } from "../hooks/useNearestIntersection"
 import { emptyLadderDirectionsByRouteId } from "../models/ladderDirection"
 import { currentRouteTab } from "../models/routeTab"
 import { directionName } from "../models/vehicle"
-import { drawnStatus } from "../models/vehicleStatus"
+import {
+  drawnStatus,
+  humanReadableScheduleAdherence,
+  statusClasses,
+} from "../models/vehicleStatus"
 import { Vehicle } from "../realtime"
-import { secondsAgoLabel } from "../util/dateTime"
-import CloseButton from "./closeButton"
-import { RouteVariantName } from "./routeVariantName"
-import StreetViewButton from "./streetViewButton"
-import VehicleIcon, { Size, vehicleOrientation } from "./vehicleIcon"
+import { secondsToMinutes } from "../util/dateTime"
+import { CloseButton2 } from "./closeButton"
+import { RouteVariantName2 } from "./routeVariantName"
+import StreetViewButton, {
+  WorldPositionBearing as WorldPositionBearing,
+} from "./streetViewButton"
+import { Size, VehicleIcon2, vehicleOrientation } from "./vehicleIcon"
 
+interface VehicleProp {
+  vehicle: Vehicle
+}
+
+//#region Vehicle Properties Card
 const VehiclePropertiesCard = ({
   vehicle,
   onClose,
-}: {
-  vehicle: Vehicle
+}: VehicleProp & {
   onClose: () => void
-}): ReactElement<HTMLElement> => {
-  const [{ routeTabs, userSettings }] = useContext(StateDispatchContext)
-  const currentTab = currentRouteTab(routeTabs)
-  const ladderDirections = currentTab
-    ? currentTab.ladderDirections
-    : emptyLadderDirectionsByRouteId
-  const epochNowInSeconds = useCurrentTimeSeconds()
-  const route = useRoute(vehicle.routeId)
+  }): React.ReactElement => (
+  <div className="m-vpc" title="Vehicle Properties Card">
+    <div className="m-vpc__title-bar">
+    </div>
 
-  const { latitude, longitude } = vehicle
-  const nearestIntersection: string | null = useNearestIntersection(
-    latitude,
-    longitude
-  )
+    <div className="m-vpc__summary">
+    </div>
 
-  return (
-    <div className="m-vehicle-card">
-      <div className="m-vehicle-card__icon">
-        <VehicleIcon
-          size={Size.Large}
-          orientation={vehicleOrientation(vehicle, ladderDirections)}
-          label={vehicleLabel(vehicle, userSettings)}
-          variant={vehicle.viaVariant}
-          status={drawnStatus(vehicle)}
-          userSettings={userSettings}
-        />
+    <div className="m-vpc__body">
+      <div className="m-vpc__properties m-info-section">
       </div>
-      <div>
-        <div className="m-vehicle-card__header">
-          <div className="m-vehicle-card__header-contents">
-            <div>
-              <div>{directionName(vehicle, route)}</div>
-              <RouteVariantName vehicle={vehicle} />
-            </div>
-            <div>{secondsAgoLabel(epochNowInSeconds, vehicle.timestamp)}</div>
-          </div>
-          <div>
-            <CloseButton onClick={onClose} closeButtonType={"s_light"} />
-          </div>
-        </div>
-        <div className="m-vehicle-card__content">
-          {nearestIntersection && (
-            <div className="m-vehicle-card__location">
-              <div className="m-vehicle-card__location-label">
-                Current Location
-              </div>
-              {nearestIntersection}
-            </div>
-          )}
-          <StreetViewButton
-            latitude={vehicle.latitude}
-            longitude={vehicle.longitude}
-            bearing={vehicle.bearing}
-          />
-        </div>
+
+      <div className="m-vpc__location-info m-info-section">
       </div>
     </div>
-  )
-}
+  </div>
+)
 export default VehiclePropertiesCard
+//#endregion
