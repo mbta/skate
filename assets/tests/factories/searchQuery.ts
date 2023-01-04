010c4b9b8ca8a1d52fa3a467efdebb7cff61ac5b
Kayla Firestack <kfirestack@mbta.com>
2023-01-04 12:58:04 -0500

    test(ts/factories/searchQuery): add searchFor helper and add default search types for queries

--- assets/tests/factories/searchQuery.ts
+++ assets/tests/factories/searchQuery.ts
@@ -1,21 +1,29 @@
 import { Factory } from "fishery"
 import { SearchQuery, SearchQueryType } from "../../src/models/searchQuery"
 
-export class SearchQueryFactory extends Factory<SearchQuery> {
+class SearchQueryFactory extends Factory<SearchQuery> {
   searchType(property: SearchQueryType) {
     return this.params({ property })
   }
 
-  // forProperty()
+  searchFor(text: string) {
+    return this.params({ text })
+  }
 }
 
-export const searchQueryAllFactory = SearchQueryFactory.define(
-  ({ sequence }) => ({
-    property: "all" as SearchQueryType,
-    text: sequence.toString(),
-  })
-)
+const searchQueryFactory = SearchQueryFactory.define(({ sequence }) => ({
+  property: "all" as SearchQueryType,
+  text: sequence.toString(),
+}))
 
-export const emptySearchQueryFactory = searchQueryAllFactory.params({
-  text: "",
-})
+export const searchQueryAllFactory = searchQueryFactory.searchType("all")
+
+export const searchQueryOperatorFactory =
+  searchQueryFactory.searchType("operator")
+
+export const searchQueryRunFactory = searchQueryFactory.searchType("run")
+
+export const searchQueryVehicleFactory =
+  searchQueryFactory.searchType("vehicle")
+
+export const emptySearchQueryFactory = searchQueryAllFactory.searchFor("")
