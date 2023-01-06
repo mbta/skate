d3c757cbe07fc3beefe347339e524fd452f0e959
Kayla Firestack <kfirestack@mbta.com>
2023-01-06 11:58:29 -0500

    feedback(tests/ts/factories): use `initialState` for `stateFactory`

--- assets/tests/factories/applicationState.ts
+++ assets/tests/factories/applicationState.ts
@@ -1,35 +1,8 @@
 import { Factory } from "fishery"
-import { OpenView, State } from "../../src/state"
-import { searchPageStateFactory } from "./searchPageState"
-import { userSettingsFactory } from "./userSettings"
+import { initialState, State } from "../../src/state"
 
 const stateFactory = Factory.define<State>(() => ({
-  openView: OpenView.None,
-  previousView: OpenView.None,
-  pickerContainerIsVisible: true,
-  openInputModal: null,
-
-  mobileMenuIsOpen: false,
-
-  notificationDrawerScrollPosition: 0,
-
-  routeTabs: [],
-  routeTabsPushInProgress: false,
-  routeTabsToPush: null,
-  routeTabsToPushNext: null,
-
-  selectedNotification: undefined,
-  selectedShuttleRouteIds: [],
-  selectedShuttleRunIds: "all",
-  selectedVehicleOrGhost: undefined,
-
-  showGaragesFilter: false,
-
-  showPastSwings: false,
-  swingsViewScrollPosition: 0,
-
-  searchPageState: searchPageStateFactory.build(),
-  userSettings: userSettingsFactory.build(),
+  ...initialState,
 }))
 
 export default stateFactory
