3d80631138f0ad33c58601ed4d47aebf171ec0dc
Kayla Firestack <kfirestack@mbta.com>
2023-01-10 09:41:01 -0500

    nit: use classNames

--- assets/src/components/vehiclePropertiesCard.tsx
+++ assets/src/components/vehiclePropertiesCard.tsx
@@ -4,6 +4,7 @@ import React, {
   ReactNode,
   useId,
 } from "react"
+import { className as classNames } from "../helpers/dom"
 import { useCurrentTimeSeconds } from "../hooks/useCurrentTime"
 import { useNearestIntersection } from "../hooks/useNearestIntersection"
 import { Vehicle } from "../realtime"
@@ -71,12 +72,10 @@ const TrNameValue = ({
         {name}
       </th>
       <td
-        className={[
+        className={classNames([
           "kv-value font-s-reg",
           shouldMaskInfo(sensitive !== HideSensitiveInfo.None),
-        ]
-          .filter(Boolean)
-          .join(" ")}
+        ])}
         aria-labelledby={id}
       >
         {value}
@@ -92,13 +91,11 @@ const VehicleWorkInfo = ({ vehicle }: VehicleProp): React.ReactElement => (
         <TrNameValue name="run">{vehicle.runId ?? "N/A"}</TrNameValue>
         <TrNameValue name="vehicle">{vehicle.label ?? "N/A"}</TrNameValue>
         <TrNameValue name="operator" sensitivity={HideSensitiveInfo.All}>
-          {[
+          {classNames([
             vehicle.operatorFirstName,
             vehicle.operatorLastName,
             vehicle.operatorId ? `#${vehicle.operatorId}` : null,
-          ]
-            .filter((e) => e !== null)
-            .join(" ") || "N/A"}
+          ]) || "N/A"}
         </TrNameValue>
       </tbody>
     </table>
