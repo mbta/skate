7ae684c39c32d2785845257dc2598de464d088f9
Kayla Firestack <kfirestack@mbta.com>
2023-01-16 10:39:28 -0500

    test(ts/components/map-page): add tests for what the page should do when rendering different states per designs

--- assets/tests/components/mapPage.test.tsx
+++ assets/tests/components/mapPage.test.tsx
@@ -16,11 +16,14 @@ import { SearchPageState } from "../../src/state/searchPageState"
 import stateFactory from "../factories/applicationState"
 import ghostFactory from "../factories/ghost"
 import { RunFactory } from "../factories/run"
-import { searchPageStateFactory } from "../factories/searchPageState"
+import { activeSearchPageStateFactory, searchPageStateFactory } from "../factories/searchPageState"
 import { searchQueryRunFactory } from "../factories/searchQuery"
 import shapeFactory from "../factories/shape"
 import stopFactory from "../factories/stop"
-import vehicleFactory from "../factories/vehicle"
+import vehicleFactory, { randomLocationVehicle } from "../factories/vehicle"
+import { runIdToLabel } from "../../src/helpers/vehicleLabel"
+import { VehicleLabelSetting } from "../../src/userSettings"
+import { setHtmlWidthHeightForLeafletMap } from "../testHelpers/leafletMapWidth"
 
 jest
   .spyOn(dateTime, "now")
@@ -506,4 +509,349 @@ describe("<MapPage />", () => {
       })
     })
   })
+
+  describe.skip("when rendering", () => {
+    beforeEach(() => {
+      setHtmlWidthHeightForLeafletMap()
+    })
+
+    // test("when rendered, map should be unpopulated", async () => {
+    // Render map
+    // Expect nothing on map
+    // })
+
+    // test("when search result is selected, should show vehicle icon and label, route, stops, ", async () => {})
+    describe("initial state", () => {
+      test("search should be empty, map should be empty", () => {
+        const changeApplicationState = jest.fn()
+
+        render(
+          <StateDispatchProvider
+            state={stateFactory.build({
+              userSettings: {
+                ladderVehicleLabel: VehicleLabelSetting.VehicleNumber,
+              },
+            })}
+            dispatch={changeApplicationState}
+          >
+            <MapPage />
+          </StateDispatchProvider>
+        )
+
+        const searchInput = screen.getByRole("textbox", { name: /search map/i })
+        expect(searchInput).toHaveTextContent("")
+        expect(searchInput).toHaveAttribute("placeholder", "Search")
+
+        expect(
+          screen.queryAllByRole("button", { name: /^vehicle #/ })
+        ).toHaveLength(0)
+        expect(changeApplicationState).not.toBeCalled()
+      })
+
+      // TODO: is this something I want?
+      // TODO: it should probably be in a different section
+      test("a new search, calls dispatch with search query", () => {
+        const changeApplicationState = jest.fn()
+
+        render(
+          <StateDispatchProvider
+            state={stateFactory.build({
+              userSettings: {
+                ladderVehicleLabel: VehicleLabelSetting.VehicleNumber,
+              },
+            })}
+            dispatch={changeApplicationState}
+          >
+            <MapPage />
+          </StateDispatchProvider>
+        )
+
+        const searchInput = screen.getByRole("textbox", { name: /search map/i })
+        expect(searchInput).toHaveTextContent("")
+        expect(searchInput).toHaveAttribute("placeholder", "Search")
+
+        expect(
+          screen.queryAllByRole("button", { name: /^vehicle #/ })
+        ).toHaveLength(0)
+        expect(changeApplicationState).not.toBeCalled()
+      })
+    })
+
+    describe("an active search, without selection", () => {
+      test("the map should be unpopulated", () => {
+        const changeApplicationState = jest.fn()
+
+        const { id: runId } = RunFactory.build()
+        const vehicle = vehicleFactory.build({ runId })
+        const shapes = shapeFactory.buildList(2)
+        const searchPageState = activeSearchPageStateFactory.build({
+          query: searchQueryRunFactory.searchFor(vehicle.runId!).build(),
+        })
+
+        ;(useSearchResults as jest.Mock).mockReturnValue([vehicle])
+        ;(useTripShape as jest.Mock).mockImplementation((tripId) =>
+          tripId === vehicle.tripId ? shapes : null
+        )
+
+        render(
+          <StateDispatchProvider
+            state={stateFactory.build({ searchPageState })}
+            dispatch={changeApplicationState}
+          >
+            <MapPage />
+          </StateDispatchProvider>
+        )
+
+        const searchInput = screen.getByRole("textbox", { name: /search map/i })
+        expect(searchInput).toHaveTextContent("")
+        expect(searchInput).toHaveAttribute("placeholder", "Search")
+
+        expect(screen.queryAllByRole("button", { name: /^run/ })).toHaveLength(
+          0
+        )
+        expect(changeApplicationState).not.toBeCalled()
+      })
+
+      test("when search is made, map should be unpopulated until search result is selected", async () => {
+        // define elements
+        // - vehicle icon
+        // - route shape FOR CURRENT DIRECTION
+        // - route stops FOR CURRENT DIRECTION
+        // -
+
+        // Render with active Search
+        // Ensure nothing on map
+        // Click Search Result
+        // Ensure only results on map
+        // jest.spyOn(global, "scrollTo").mockImplementationOnce(jest.fn())
+        const vehicle = vehicleFactory.build({ runId: "clickMe" })
+
+        ;(useSearchResults as jest.Mock).mockReturnValue([vehicle])
+
+        // const activeSearch: SearchPageState =
+
+        const shapes = shapeFactory.buildList(2)
+        ;(useTripShape as jest.Mock).mockImplementation((tripId) =>
+          tripId === vehicle.tripId ? shapes : null
+        )
+
+        const mockDispatch = jest.fn()
+
+        // const { container } = renderMapPageWithSearchState(
+        //   searchPageStateFactory.build({
+        //     query: { text: "clickMe", property: "run" },
+        //     isActive: true,
+        //   }),
+        //   mockDispatch
+        // )
+        const { container } = render(<MapPage />, {
+          wrapper: (props) => (
+            <StateDispatchProvider
+              state={stateFactory.build({
+                searchPageState: activeSearchPageStateFactory.build({
+                  query: searchQueryRunFactory.build({ text: vehicle.runId! }),
+                }),
+              })}
+              dispatch={mockDispatch}
+              {...props}
+            />
+          ),
+        })
+
+        const mapSearchPanel = screen.getByRole("generic", {
+          name: /map search panel/i,
+        })
+
+        // TODO:FIXME: this is not the correct format for this query
+        expect(
+          screen.queryByRole("button", { name: `run ${vehicle.runId}` })
+        ).not.toBeInTheDocument()
+        expect(
+          screen.queryByRole("button", { name: `route ${vehicle.routeId}` })
+        ).not.toBeInTheDocument()
+        expect(
+          container.querySelector(".m-vehicle-icon__label")
+        ).not.toBeInTheDocument()
+        expect(
+          container.querySelector(".m-vehicle-map__route-shape")
+        ).not.toBeInTheDocument()
+
+        await userEvent.click(
+          screen.getByRole("cell", { name: vehicle.runId! })
+        )
+
+        expect(
+          container.querySelector(".m-vehicle-icon__label")
+        ).toBeInTheDocument()
+        expect(
+          container.querySelector(".m-vehicle-map__route-shape")
+        ).toBeInTheDocument()
+        expect(
+          screen.getByRole("button", { name: vehicle.runId! })
+        ).toBeVisible()
+
+        expect(
+          screen.getByRole("generic", { name: /vehicle properties card/i })
+        ).toBeVisible()
+        expect(
+          container.querySelector(".m-vehicle-map__route-shape")
+        ).toBeVisible()
+        expect(mapSearchPanel).not.toBeVisible()
+      })
+    })
+
+    describe("an active selection", () => {
+      test("the map should be populated", () => {
+        const changeApplicationState = jest.fn()
+
+        const [selectedRun, unselectedRun] = RunFactory.buildList(2)
+        const selectedRouteVehicles = randomLocationVehicle.buildList(7, {
+          runId: selectedRun.id,
+        })
+        const unselectedRouteVehicles = randomLocationVehicle.buildList(5, {
+          runId: unselectedRun.id,
+        })
+
+        const [selectedVehicle] = selectedRouteVehicles
+        // const vehicle = vehicleFactory.build({ runId })
+        const shapes = shapeFactory.buildList(2)
+
+        const searchPageState = activeSearchPageStateFactory.build({
+          query: searchQueryRunFactory.searchFor(selectedRun.id).build(),
+          selectedVehicleId: selectedVehicle.id,
+        })
+
+        ;(useSearchResults as jest.Mock).mockReturnValue([
+          ...selectedRouteVehicles,
+          ...unselectedRouteVehicles,
+        ])
+        ;(useTripShape as jest.Mock).mockImplementation((tripId) =>
+          tripId === selectedVehicle.tripId ? shapes : null
+        )
+
+        // const searchPageState = activeSearchPageStateFactory.build({
+        //   query: searchQueryRunFactory.build({
+        //     text:
+        //   })
+        // })
+        render(
+          <StateDispatchProvider
+            state={stateFactory.build({
+              searchPageState,
+              userSettings: {
+                // ladderVehicleLabel: VehicleLabelSetting.VehicleNumber,
+              },
+            })}
+            dispatch={changeApplicationState}
+          >
+            <MapPage />
+          </StateDispatchProvider>
+        )
+
+        // vehicle, route, trip shape, trip stops, route vehicles
+        // selection state
+        const searchInput = screen.getByRole("textbox", { name: /search map/i })
+        expect(searchInput).toHaveTextContent("")
+        expect(searchInput).toHaveAttribute("placeholder", "Search")
+
+        expect(
+          screen.getAllByRole("button", { name: selectedVehicle.runId! })
+        ).toHaveLength(selectedRouteVehicles.length)
+        expect(changeApplicationState).not.toBeCalled()
+      })
+
+      describe("VehiclePropertiesCard", () => {
+        test("should be visible, search panel should be closed", async () => {
+          // const runId = "clickMe"
+          const vehicle = vehicleFactory.build({})
+          ;(useVehicleForId as jest.Mock).mockReturnValue(vehicle)
+          ;(useSearchResults as jest.Mock).mockReturnValue([vehicle])
+
+          render(
+            <StateDispatchProvider
+              state={stateFactory.build({
+                searchPageState: searchPageStateFactory.build({
+                  selectedVehicleId: vehicle.id,
+                }),
+              })}
+              dispatch={jest.fn()}
+            >
+              <BrowserRouter>
+                <MapPage />
+              </BrowserRouter>
+            </StateDispatchProvider>
+          )
+
+          expect(
+            screen.getByRole("generic", { name: /vehicle properties card/i })
+          ).toBeVisible()
+        })
+        // describe("renders", () => {
+        test("after search result is selected", async () => {
+          const runId = "clickMe"
+          const vehicle = vehicleFactory.build({ runId })
+          jest.spyOn(global, "scrollTo").mockImplementationOnce(jest.fn())
+          ;(useVehicleForId as jest.Mock).mockReturnValue(vehicle)
+          ;(useSearchResults as jest.Mock).mockReturnValue(
+            // vehicleFactory.buildList(1, { runId: runId })
+            [vehicle]
+          )
+
+          render(
+            <StateDispatchProvider
+              state={stateFactory.build({
+                searchPageState: activeSearchPageStateFactory.build({
+                  query: { text: runId },
+                }),
+              })}
+              dispatch={jest.fn()}
+            >
+              <BrowserRouter>
+                <MapPage />
+              </BrowserRouter>
+            </StateDispatchProvider>
+          )
+
+          await userEvent.click(
+            screen.getByRole("button", {
+              name: runIdToLabel(runId),
+            })
+          )
+          expect(
+            screen.getByRole("generic", { name: /vehicle properties card/i })
+          ).toBeVisible()
+        })
+
+        // })
+      })
+
+      describe("without a active search", () => {
+        // describe("an active selection without a active search", () => {
+        // describe("When a regular bus is selected by a search result or VPP(?)", () => {
+        describe("is a regular bus", () => {
+          test.todo("should display VPC")
+          test.todo(
+            "should style vehicle icon on map as selected and map should be centered on the vehicle"
+          )
+          test.todo("should display: vehicle icon, route shape and stops")
+          test.todo("should auto center on vehicle by default")
+        })
+
+        describe("is a shuttle", () => {
+          test.todo("should display VPC")
+          test.todo(
+            "should style vehicle icon on map as selected and map should be centered on the vehicle"
+          )
+          test.todo("should not display vehicle route shape and stops")
+          test.todo("should auto center on vehicle by default")
+        })
+        // })
+      })
+
+      describe("with an active search", () => {})
+    })
+  })
+
+  describe("when user interacts with", () => {})
+  describe("api", () => {})
 })
