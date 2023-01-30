dd1991cc0555c514059429ed63b2880108189580
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2023-01-30 13:30:53 -0500

    fix(UseRoutePatterns): Support null id, update when route id changes

--- assets/src/hooks/useRoutePatterns.ts
+++ assets/src/hooks/useRoutePatterns.ts
@@ -2,13 +2,17 @@ import { useEffect, useState } from "react"
 import { fetchRoutePatterns } from "../api"
 import { RouteId, RoutePattern } from "../schedule"
 
-const useRoutePatterns = (routeId: RouteId): RoutePattern[] | null => {
+const useRoutePatterns = (routeId: RouteId | null): RoutePattern[] | null => {
   const [routePatterns, setRoutePatterns] = useState<RoutePattern[] | null>(
     null
   )
   useEffect(() => {
-    fetchRoutePatterns(routeId).then(setRoutePatterns)
-  }, [])
+    if (routeId === null) {
+      setRoutePatterns([])
+    } else {
+      fetchRoutePatterns(routeId).then(setRoutePatterns)
+    }
+  }, [routeId])
   return routePatterns
 }
 
--- assets/tests/hooks/useRoutePatterns.test.ts
+++ assets/tests/hooks/useRoutePatterns.test.ts
@@ -33,6 +33,19 @@ describe("useRoutePatterns", () => {
     expect(result.current).toEqual(routePatterns)
   })
 
+  test("doesn't fetch routes when passed null id", () => {
+    const mockFetchRoutePatterns: jest.Mock =
+      Api.fetchRoutePatterns as jest.Mock
+    mockFetchRoutePatterns.mockReturnValueOnce(
+      instantPromise([routePatternFactory.buildList(2)])
+    )
+    const { rerender } = renderHook(() => {
+      return useRoutePatterns(null)
+    })
+    rerender()
+    expect(mockFetchRoutePatterns).toHaveBeenCalledTimes(0)
+  })
+
   test("doesn't refetch routes on every render", () => {
     const mockFetchRoutePatterns: jest.Mock =
       Api.fetchRoutePatterns as jest.Mock
@@ -44,5 +57,22 @@ describe("useRoutePatterns", () => {
     })
     rerender()
     expect(mockFetchRoutePatterns).toHaveBeenCalledTimes(1)
+  })
+
+  test("refetches when routeId changes", () => {
+    const mockFetchRoutePatterns: jest.Mock =
+      Api.fetchRoutePatterns as jest.Mock
+    mockFetchRoutePatterns.mockReturnValueOnce(
+      instantPromise([routePatternFactory.buildList(2)])
+    )
+    const { rerender } = renderHook(
+      (id) => {
+        return useRoutePatterns(id)
+      },
+      { initialProps: "66" }
+    )
+    expect(mockFetchRoutePatterns).toHaveBeenLastCalledWith("66")
+    rerender("39")
+    expect(mockFetchRoutePatterns).toHaveBeenLastCalledWith("39")
   })
 })
