aaed28fe45f4f0e55bbf09a5cfeb5a92f6c2b03c
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2023-02-23 11:31:57 -0500

    refactor(card.test): update test queries - toHaveClass, screen, getByRole, etc.

--- assets/tests/components/card.test.tsx
+++ assets/tests/components/card.test.tsx
@@ -1,28 +1,25 @@
 import React from "react"
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { Card, CardBody, CardProperties } from "../../src/components/card"
 import userEvent from "@testing-library/user-event"
 import "@testing-library/jest-dom"
 
 describe("Card", () => {
   test("renders content", () => {
-    const result = render(
+    render(
       <Card title="My Card" currentTime={new Date()} style="kiwi">
         Foo
       </Card>
     )
 
-    expect(result.queryByText(/My Card/)).not.toBeNull()
-    expect(result.queryByText(/Foo/)).not.toBeNull()
-    expect(result.queryByTitle("Close")).toBeNull()
-    expect(
-      result.queryByText(/Foo/)?.parentElement?.parentElement?.parentElement
-        ?.className
-    ).toMatch(/m-card--inactive/)
+    expect(screen.getByText(/My Card/)).toBeVisible()
+    expect(screen.getByText(/Foo/)).toBeVisible()
+    expect(screen.queryByTitle("Close")).toBeNull()
+    expect(screen.getByLabelText(/My Card/)).toHaveClass("m-card--inactive")
   })
 
   test("omits inactive class when active", () => {
-    const result = render(
+    render(
       <Card
         title="My Card"
         currentTime={new Date()}
@@ -33,13 +30,11 @@ describe("Card", () => {
       </Card>
     )
 
-    expect(
-      result.queryByText(/Foo/)?.parentElement?.parentElement?.className
-    ).not.toMatch(/m-card--inactive/)
+    expect(screen.getByLabelText(/My Card/)).not.toHaveClass("m-card--inactive")
   })
 
   test("can add custom additional class", () => {
-    const result = render(
+    render(
       <Card
         title="My Card"
         currentTime={new Date()}
@@ -50,14 +45,11 @@ describe("Card", () => {
       </Card>
     )
 
-    expect(
-      result.queryByText(/Foo/)?.parentElement?.parentElement?.parentElement
-        ?.className
-    ).toMatch(/my-custom-class/)
+    expect(screen.getByLabelText(/My Card/)).toHaveClass("my-custom-class")
   })
 
   test("can disable focus / hover states", () => {
-    const result = render(
+    render(
       <Card
         title="My Card"
         currentTime={new Date()}
@@ -68,14 +60,13 @@ describe("Card", () => {
       </Card>
     )
 
-    expect(
-      result.queryByText(/Foo/)?.parentElement?.parentElement?.parentElement
-        ?.className
-    ).toMatch(/m-card--no-focus-or-hover/)
+    expect(screen.getByLabelText(/My Card/)).toHaveClass(
+      "m-card--no-focus-or-hover"
+    )
   })
 
   test("can mark as selected", () => {
-    const result = render(
+    render(
       <Card
         title="My Card"
         currentTime={new Date()}
@@ -87,12 +78,12 @@ describe("Card", () => {
     )
 
     expect(
-      result.getByRole("generic", { name: "My Card", current: true })
+      screen.getByRole("generic", { name: "My Card", current: true })
     ).toBeInTheDocument()
   })
 
   test("includes age when time is given", () => {
-    const result = render(
+    render(
       <Card
         title="My Card"
         currentTime={new Date("2022-07-01T10:00:00Z")}
@@ -103,21 +94,21 @@ describe("Card", () => {
       </Card>
     )
 
-    expect(result.queryByText(/10 min/)).not.toBeNull()
+    expect(screen.getByText(/10 min/)).toBeVisible()
   })
 
   test("includes icon element when given", () => {
-    const result = render(
+    render(
       <Card title="My Card" icon={<>Bar</>} style="white">
         Foo
       </Card>
     )
 
-    expect(result.queryByText(/Bar/)).not.toBeNull()
+    expect(screen.getByText(/Bar/)).toBeVisible()
   })
 
   test("includes icon element when given along with open callback", () => {
-    const result = render(
+    render(
       <Card
         title="My Card"
         icon={<>Bar</>}
@@ -130,13 +121,13 @@ describe("Card", () => {
       </Card>
     )
 
-    expect(result.queryByText(/Bar/)).not.toBeNull()
+    expect(screen.getByText(/Bar/)).toBeVisible()
   })
 
   test("invokes callback when clicked", async () => {
     const openCallback = jest.fn()
     const user = userEvent.setup()
-    const result = render(
+    render(
       <Card
         title="My Card"
         currentTime={new Date()}
@@ -147,11 +138,7 @@ describe("Card", () => {
       </Card>
     )
 
-    expect(
-      result.getByText(/Contents/).parentElement?.parentElement?.tagName
-    ).toEqual("BUTTON")
-
-    await user.click(result.getByText(/Contents/))
+    await user.click(screen.getByRole("button", { name: /Contents/ }))
 
     expect(openCallback).toHaveBeenCalledTimes(1)
   })
@@ -159,7 +146,7 @@ describe("Card", () => {
   test("invokes callback on close", async () => {
     const closeCallback = jest.fn()
     const user = userEvent.setup()
-    const result = render(
+    render(
       <Card
         title="My Card"
         currentTime={new Date()}
@@ -170,7 +157,7 @@ describe("Card", () => {
       </Card>
     )
 
-    await user.click(result.getByRole("button", { name: /close/i }))
+    await user.click(screen.getByRole("button", { name: /close/i }))
 
     expect(closeCallback).toHaveBeenCalledTimes(1)
   })
@@ -178,15 +165,15 @@ describe("Card", () => {
 
 describe("CardBody", () => {
   test("renders content", () => {
-    const result = render(<CardBody>Foo</CardBody>)
+    render(<CardBody>Foo</CardBody>)
 
-    expect(result.queryByText("Foo")).not.toBeNull()
+    expect(screen.getByText("Foo")).toBeVisible()
   })
 })
 
 describe("CardProperties", () => {
   test("does render property that has a value", () => {
-    const result = render(
+    render(
       <CardProperties
         properties={[
           {
@@ -197,12 +184,12 @@ describe("CardProperties", () => {
       />
     )
 
-    expect(result.queryByText(/My property/)).not.toBeNull()
-    expect(result.queryByText(/Some value/)).not.toBeNull()
+    expect(screen.getByRole("cell", { name: /My property/ })).toBeVisible()
+    expect(screen.getByRole("cell", { name: /Some value/ })).toBeVisible()
   })
 
   test("doesn't render property that lacks a value", () => {
-    const result = render(
+    render(
       <CardProperties
         properties={[
           {
@@ -213,11 +200,11 @@ describe("CardProperties", () => {
       />
     )
 
-    expect(result.queryByText(/My property/)).toBeNull()
+    expect(screen.queryByRole("cell", { name: /My property/ })).toBeNull()
   })
 
   test("includes class for sensitive values", () => {
-    const result = render(
+    render(
       <CardProperties
         properties={[
           {
@@ -230,7 +217,7 @@ describe("CardProperties", () => {
     )
 
     expect(
-      result.queryByText(/Some sensitive value/)?.parentElement?.className
-    ).toMatch(/fs-mask/)
+      screen.getByRole("row", { name: /Some sensitive value/ })
+    ).toHaveClass("fs-mask")
   })
 })
