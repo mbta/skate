e33ed8e11e0ba7948b2b83b1a83ff696812bdad3
Kayla Brady <31781298+KaylaBrady@users.noreply.github.com>
2023-03-06 10:19:26 -0500

    feat(RoutePatternController): Include headsign of representative trip (#1955)

--- lib/skate_web/controllers/route_pattern_controller.ex
+++ lib/skate_web/controllers/route_pattern_controller.ex
@@ -13,12 +13,15 @@ defmodule SkateWeb.RoutePatternController do
         &Schedule.shape_with_stops_for_trip/1
       )
 
+    trip_fn = Application.get_env(:skate_web, :trip_fn, &Schedule.trip/1)
+
     route_patterns =
       route_id
       |> route_patterns_fn.()
       |> Enum.map(fn route_pattern ->
         Map.merge(Map.from_struct(route_pattern), %{
-          shape: shape_for_trip_fn.(route_pattern.representative_trip_id)
+          shape: shape_for_trip_fn.(route_pattern.representative_trip_id),
+          headsign: trip_fn.(route_pattern.representative_trip_id).headsign
         })
       end)
 
