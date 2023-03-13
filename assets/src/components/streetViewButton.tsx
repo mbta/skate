c61a0d28c5d7102a26d04a283e352c38fe2045ee
Kayla Firestack <kfirestack@mbta.com>
2023-03-13 14:28:13 -0400

    feat(ts/components/streetViewButton): add position and url info to FS event

--- assets/src/components/streetViewButton.tsx
+++ assets/src/components/streetViewButton.tsx
@@ -25,29 +25,38 @@ const StreetViewButton = ({
   children,
   title,
   ...worldPosition
-}: StreetViewButtonProps): ReactElement<HTMLElement> => (
-  <a
-    className={classNames([
-      "m-street-view-button",
-      "button-dark-small",
-      className,
-    ])}
-    href={streetViewUrl(worldPosition)}
-    target="_blank"
-    rel="noreferrer"
-    onClick={() => {
-      window.FS?.event("Street view link followed")
-    }}
-    {...(title && { title })}
-  >
-    <WalkingIcon
-      className="m-street-view-button__icon"
-      role="img"
-      aria-label=""
-      aria-hidden={true}
-    />
-    {children ?? text ?? "Street View"}
-  </a>
-)
+}: StreetViewButtonProps): ReactElement<HTMLElement> => {
+  const streetViewUrl_str = streetViewUrl(worldPosition)
+  return (
+    <a
+      className={classNames([
+        "m-street-view-button",
+        "button-dark-small",
+        className,
+      ])}
+      href={streetViewUrl_str}
+      target="_blank"
+      rel="noreferrer"
+      onClick={() => {
+        window.FS?.event("Street view link followed", {
+          streetViewUrl_str,
+          source: {
+            latitude_real: worldPosition.latitude,
+            longitude_real: worldPosition.longitude,
+          },
+        })
+      }}
+      {...(title && { title })}
+    >
+      <WalkingIcon
+        className="m-street-view-button__icon"
+        role="img"
+        aria-label=""
+        aria-hidden={true}
+      />
+      {children ?? text ?? "Street View"}
+    </a>
+  )
+}
 
 export default StreetViewButton
