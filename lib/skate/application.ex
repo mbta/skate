5db352bbf4016b91541718dc6a07afb087c084d6
Kayla Firestack <firestack@users.noreply.github.com>
2023-04-14 16:09:14 -0400

    cleanup(ex/skate/database): remove users without email (#2025)
    * feat(ex/skate/user): ensure email is not empty string
    
    * init(ex/skate/remove-users): add initial cleanup function
    
    * test(User.upsert/2): Add test for empty email string error
    
    * feat(RemoveUsersWithoutEmail): call to delete_all & add test
    
    * fix(user_factory): Unique email & usernames
    
    ---------
    
    Co-authored-by: Kayla Brady <KaylaBrady@users.noreply.github.com>

--- lib/skate/application.ex
+++ lib/skate/application.ex
@@ -32,6 +32,7 @@ defmodule Skate.Application do
     link = Supervisor.start_link(children, strategy: :one_for_all, name: Skate.Supervisor)
 
     Migrate.up()
+    Skate.RemoveUsersWithoutEmail.run()
 
     link
   end
