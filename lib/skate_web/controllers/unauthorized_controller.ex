9bae791df51186b9298a444c073f330718beb15c
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2023-04-19 13:13:53 -0400

    feat(EnsureEnvironmentAccess): New plug to ensure users can only access skate if they have access to that environment

--- lib/skate_web/controllers/unauthorized_controller.ex
+++ lib/skate_web/controllers/unauthorized_controller.ex
@@ -7,4 +7,10 @@ defmodule SkateWeb.UnauthorizedController do
     |> put_status(403)
     |> render("index.html", layout: {SkateWeb.LayoutView, "barebones.html"})
   end
+
+  def restricted_environment(conn, _params) do
+    conn
+    |> put_status(403)
+    |> render("restricted_environment.html", layout: {SkateWeb.LayoutView, "barebones.html"})
+  end
 end
--- test/skate_web/controllers/unauthorized_controller_test.exs
+++ test/skate_web/controllers/unauthorized_controller_test.exs
@@ -9,4 +9,14 @@ defmodule SkateWeb.UnauthorizedControllerTest do
       assert html_response(conn, 403) =~ "not authorized"
     end
   end
+
+  describe "restricted_environment/2" do
+    @tag :authenticated
+    test "renders test environment message", %{conn: conn} do
+      conn = get(conn, SkateWeb.Router.Helpers.unauthorized_path(conn, :restricted_environment))
+
+      assert html_response(conn, 403) =~
+               "The URL you followed is actually for Skate's test environment"
+    end
+  end
 end
