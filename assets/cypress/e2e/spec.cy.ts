f5052cc5a1d7b316e7604eb4f30f4f07240103fd
Maldonado <emaldonado@mbta.com>
2023-04-26 11:31:34 -0400

    test: example of how to call API to clear out route tabs before test
    Notes that this still relies on disabling CSRF protection (can be done
    in router.ex, remove the reference to :protect_from_forgery in the
    :browser pipeline). In the future we'll have to find a way to parse
    the CSRF token from the page and use that.

--- assets/cypress/e2e/spec.cy.ts
+++ assets/cypress/e2e/spec.cy.ts
@@ -2,8 +2,21 @@ describe("template spec", () => {
   it("passes", () => {
     cy.visit("/")
 
-    cy.findByRole("button", { name: /Swings View/i }).click()
+    // Call the API to clear out all pre-existing route tabs, giving
+    // a clean slate for reproducible tests.
+    // To actually work this will need the CSRF token. We can parse
+    // it out of the page from the initial request
+    cy.request({
+      method: "PUT",
+      url: "/api/route_tabs",
+      body: JSON.stringify({ route_tabs: [] }),
+      headers: {
+        "content-type": "application/json",
+      },
+    })
 
-    cy.compareSnapshot("swings")
+    cy.visit("/")
+
+    cy.findByRole("button", { name: /Swings View/i }).click()
   })
 })
