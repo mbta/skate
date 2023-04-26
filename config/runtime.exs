c255ffc98b0d96ea47af6efcc89e5f1516d7dd4f
Kayla Firestack <kfirestack@mbta.com>
2023-04-26 15:26:43 -0400

    feat(skate): add mock_data_sources variable

--- config/runtime.exs
+++ config/runtime.exs
@@ -6,7 +6,8 @@ Application.ensure_all_started(:ex_aws)
 
 config :skate,
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
-  restrict_environment_access?: System.get_env("RESTRICT_ENVIRONMENT_ACCESS") == "true"
+  restrict_environment_access?: System.get_env("RESTRICT_ENVIRONMENT_ACCESS") == "true",
+  mock_data_sources: System.get_env("MOCK_DATA") != nil and Mix.env() == :dev
 
 config :ueberauth, Ueberauth.Strategy.Cognito,
   client_secret: System.get_env("COGNITO_CLIENT_SECRET")
