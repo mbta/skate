1615d5ae8fdc87d03d59b7bb13ea65701a3665c8
Eddie Maldonado <emaldonado@mbta.com>
2023-06-05 13:52:57 -0400

    Basic setup of Cypress (#2047)
    * test: install Cypress and set up an example test
    
    * test: add cypress-visual-regression
    
    * test: example of how to call API to clear out route tabs before test
    
    Notes that this still relies on disabling CSRF protection (can be done
    in router.ex, remove the reference to :protect_from_forgery in the
    :browser pipeline). In the future we'll have to find a way to parse
    the CSRF token from the page and use that.
    
    * fix(docs): add comment explaining where assertions would go in test

---
+++ assets/cypress.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "cypress"
+import getCompareSnapshotsPlugin from "cypress-visual-regression/dist/plugin"
+
+export default defineConfig({
+  env: {
+    screenshotsFolder: "./cypress/snapshots/actual",
+    trashAssetsBeforeRuns: true,
+    video: false,
+  },
+  e2e: {
+    baseUrl: "http://localhost:4000",
+    setupNodeEvents(on, config) {
+      getCompareSnapshotsPlugin(on, config)
+    },
+  },
+})
