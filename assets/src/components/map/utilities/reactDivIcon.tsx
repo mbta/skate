4c93076c07ae2ef6daa6915f1eccbc64368e70db
Kayla Firestack <kfirestack@mbta.com>
2023-06-07 10:29:02 -0400

    feat(ts/components/map/reactDivIcon): create React abstraction around leaflet DivIcon

---
+++ assets/src/components/map/utilities/reactDivIcon.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react"
+
+import L, { DivIcon, DivIconOptions as LeafletDivIconOptions } from "leaflet"
+
+// Prevent user from setting parameters we intend to provide
+export type DivIconOptions = Omit<LeafletDivIconOptions, "html">
+
+export function useReactDivIcon(opts: DivIconOptions) {
+  // Constant element react portal to use between `divIcon` recreations
+  const [iconContainer, setIconContainer] = useState<HTMLDivElement | null>(
+    null
+  )
+  const [divIcon, setDivIcon] = useState<DivIcon>()
+
+  // Create portal element and divIcon on creation and when `opts` change
+  useEffect(() => {
+    let element = iconContainer
+    if (!element) {
+      element = createPortalElement()
+      setIconContainer(element)
+    }
+
+    // Leaflet doesn't support updating a `divIcon` in place.
+    // To ensure that the `divIcon` updates when `opts` change
+    // regenerate the `divIcon` with the portal element and provided `opts`
+    if (element) {
+      setDivIcon(L.divIcon({ ...opts, html: element }))
+    }
+  }, [opts, iconContainer])
+
+  return {
+    divIcon,
+    iconContainer,
+  }
+}
+
+// Extend this function or add more parameters to `useReactDivIcon` to override
+// portal element creation
+const createPortalElement = () => {
+  const element = document.createElement("div")
+  element.classList.add("w-100", "h-100")
+  return element
+}
