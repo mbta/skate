c5238c92591d6a94ee892433c4dad55fe74a1de6
Kayla Firestack <kfirestack@mbta.com>
2023-06-12 13:45:41 -0400

    test(ts/components/reactDivIcon): add tests

---
+++ assets/tests/components/map/utilities/reactDivIcon.test.tsx
@@ -0,0 +1,52 @@
+import { renderHook } from "@testing-library/react"
+import "@testing-library/jest-dom"
+import { DivIcon } from "leaflet"
+import React from "react"
+import { MapContainer } from "react-leaflet"
+import {
+  DivIconOptions,
+  useReactDivIcon,
+} from "../../../../src/components/map/utilities/reactDivIcon"
+
+const renderHookReactDivIcon = (initialProps?: DivIconOptions) =>
+  renderHook((props) => useReactDivIcon(props), {
+    wrapper: ({ children }) => <MapContainer children={children} />,
+    initialProps,
+  })
+
+describe("useReactDivIcon", () => {
+  test("Returns divIcon and icon element", () => {
+    const { result } = renderHookReactDivIcon()
+
+    expect(result.current.divIcon).toBeInstanceOf(DivIcon)
+    expect(result.current.iconContainer).toBeEmptyDOMElement()
+  })
+
+  test("Returns divIcon and icon element with custom options", () => {
+    const { result } = renderHookReactDivIcon({
+      className: "testClass",
+      iconSize: [20, 20],
+    })
+
+    expect(result.current.divIcon).toBeInstanceOf(DivIcon)
+    expect(result.current.iconContainer).toBeEmptyDOMElement()
+  })
+
+  test("Updates divIcon with new options on change", () => {
+    const options: DivIconOptions = {
+      className: "testClass",
+      iconSize: [20, 20],
+    }
+    const { result, rerender } = renderHookReactDivIcon(options)
+
+    const { divIcon: initialDivIcon, iconContainer: initialIconContainer } =
+      result.current
+
+    rerender({ ...options, className: "newClass" })
+
+    expect(result.current.divIcon).toBeInstanceOf(DivIcon)
+    expect(result.current.iconContainer).toBeEmptyDOMElement()
+    expect(result.current.divIcon).not.toBe(initialDivIcon)
+    expect(result.current.iconContainer).toBe(initialIconContainer)
+  })
+})
---
+++ assets/tests/components/map/utilities/reactMarker.test.tsx
@@ -0,0 +1,95 @@
+import { render, screen } from "@testing-library/react"
+import "@testing-library/jest-dom"
+import React, { ReactNode } from "react"
+import { MapContainer } from "react-leaflet"
+import { ReactMarker } from "../../../../src/components/map/utilities/reactMarker"
+import { localGeoCoordinateFactory } from "../../../factories/geoCoordinate"
+import { DivIconOptions } from "../../../../src/components/map/utilities/reactDivIcon"
+
+const mapWrapper = ({
+  children,
+}: {
+  children: ReactNode
+}) => <MapContainer center={[0, 0]} zoom={0} children={children} />
+
+describe("ReactMarker", () => {
+  test("should render icon onto the map", () => {
+    const { latitude, longitude } = localGeoCoordinateFactory.build()
+    const opts = {}
+    render(
+      <ReactMarker
+        position={[latitude, longitude]}
+        divIconSettings={opts}
+        icon={<div data-testid="hello!" />}
+      />,
+      { wrapper: mapWrapper }
+    )
+    expect(screen.getByTestId("hello!")).toBeVisible()
+  })
+
+  test("when icon changes, should render changes onto map", () => {
+    const opts = {}
+    const { latitude, longitude } = localGeoCoordinateFactory.build()
+    const testId = "test-id"
+    const newContent = "hello!"
+
+    const { rerender } = render(
+      <ReactMarker
+        position={[latitude, longitude]}
+        divIconSettings={opts}
+        icon={<div data-testid={testId} />}
+      />,
+      { wrapper: mapWrapper }
+    )
+    const icon = screen.getByTestId(testId)
+
+    expect(icon).toBeEmptyDOMElement()
+
+    rerender(
+      <ReactMarker
+        position={[latitude, longitude]}
+        divIconSettings={opts}
+        icon={<div data-testid={testId}>{newContent}</div>}
+      />
+    )
+
+    expect(icon).toHaveTextContent(newContent)
+  })
+
+  test("when divIconSettings change, should render changes onto map", () => {
+    const opts: DivIconOptions = {iconSize: [20, 20]}
+    const { latitude, longitude } = localGeoCoordinateFactory.build()
+    const testId = "test-id"
+
+    const { rerender } = render(
+      <ReactMarker
+        position={[latitude, longitude]}
+        divIconSettings={opts}
+        icon={<div data-testid={testId} />}
+      />,
+      { wrapper: mapWrapper }
+    )
+
+    const icon = screen.getByTestId(testId)
+
+    expect(icon).toBeEmptyDOMElement()
+
+    expect(icon.parentElement?.parentElement).toHaveStyle({
+      width: "20px",
+      height: "20px"
+    })
+
+    rerender(
+      <ReactMarker
+        position={[latitude, longitude]}
+        divIconSettings={{iconSize: [40, 50]}}
+        icon={<div data-testid={testId} />}
+      />
+    )
+
+    expect(icon.parentElement?.parentElement).toHaveStyle({
+      width: "40px",
+      height: "50px"
+    })
+  })
+})
