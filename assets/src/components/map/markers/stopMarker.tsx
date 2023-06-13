d22a2a1344b0209cddf4840a9fea659fb2721d1f
Kayla Firestack <kfirestack@mbta.com>
2023-06-13 11:20:51 -0400

    fix(ts/components/stopMarker/StopMarkerProps): convert `Partial` to `Omit<, "position">`

--- assets/src/components/map/markers/stopMarker.tsx
+++ assets/src/components/map/markers/stopMarker.tsx
@@ -197,7 +197,7 @@ export const StopIcon = ({
   )
 }
 
-interface StopMarkerProps extends Partial<MarkerProps> {
+interface StopMarkerProps extends Omit<MarkerProps, "position"> {
   stop: Stop
   selected?: boolean
   zoomLevel?: number
