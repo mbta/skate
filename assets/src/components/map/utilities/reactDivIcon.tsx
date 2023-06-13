34e2cad40455776f4904031af21ac37ddbd2cc0f
Kayla Firestack <kfirestack@mbta.com>
2023-06-13 14:35:21 -0400

    doc(ts/components/utilities/reactDivIcon): document `useReactDivIcon` niche.

--- assets/src/components/map/utilities/reactDivIcon.tsx
+++ assets/src/components/map/utilities/reactDivIcon.tsx
@@ -8,6 +8,18 @@ export type DivIconOptions = Omit<LeafletDivIconOptions, "html">
 // Prevent useEffect from triggering by providing stable default reference
 const defaultOptions = {}
 
+/**
+ * Hook to create a `divIcon` compatible with {@link ReactDOM.createPortal}, by
+ * creating a stable {@link HTMLDivElement container} for React to use for
+ * rendering.
+ *
+ * ---
+ *
+ * Because Leaflet by default doesn't support updating a `divIcon` in place,
+ * this replaces the `divIcon` every time that the parameter {@link options}
+ * is updated, to ensure that `divIcon` is up to date for react-leaflet
+ * `Marker`.
+ */
 export function useReactDivIcon(options?: DivIconOptions) {
   const opts = options || defaultOptions
   // Persistent element for react portal to use between `divIcon` recreations
