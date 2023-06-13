afca658fd671c886ac980ef4d1cd4dad84ebbd40
Kayla Firestack <kfirestack@mbta.com>
2023-06-13 09:23:24 -0400

    fix:test(ts/components/map/utilities): assert that leaflet does update styles on change

--- assets/tests/components/map/utilities/reactDivIcon.test.tsx
+++ assets/tests/components/map/utilities/reactDivIcon.test.tsx
@@ -1,6 +1,6 @@
 import { renderHook } from "@testing-library/react"
 import "@testing-library/jest-dom"
-import { DivIcon } from "leaflet"
+import { DivIcon, PointTuple } from "leaflet"
 import React from "react"
 import { MapContainer } from "react-leaflet"
 import {
@@ -23,30 +23,53 @@ describe("useReactDivIcon", () => {
   })
 
   test("when given custom options, should return divIcon and icon element", () => {
-    const { result } = renderHookReactDivIcon({
-      className: "testClass",
-      iconSize: [20, 20],
-    })
+    const className = "testClass"
 
-    expect(result.current.divIcon).toBeInstanceOf(DivIcon)
-    expect(result.current.iconContainer).toBeEmptyDOMElement()
+    const iconSize: PointTuple = [20, 20]
+    const [size_x, size_y] = iconSize
+
+    const { result } = renderHookReactDivIcon({
+      className,
+      iconSize,
+    })
+    const newIcon = result.current.divIcon?.createIcon()
+
+    expect(newIcon).toHaveClass(className)
+    expect(newIcon).toHaveStyle({ height: `${size_y}px`, width: `${size_x}px` })
   })
 
-  test("when options change, should replace divIcon", () => {
-    const options: DivIconOptions = {
-      className: "testClass",
-      iconSize: [20, 20],
+  test("when options change, should replace divIcon with changes", () => {
+    const initialClassName = "initialClass"
+    const newClassName = "newClass"
+
+    const initialOptions: DivIconOptions = {
+      className: initialClassName,
     }
-    const { result, rerender } = renderHookReactDivIcon(options)
 
-    const { divIcon: initialDivIcon, iconContainer: initialIconContainer } =
-      result.current
+    const { result, rerender } = renderHookReactDivIcon(initialOptions)
 
-    rerender({ ...options, className: "newClass" })
+    expect(result.current.divIcon?.createIcon()).toHaveClass(initialClassName)
 
-    expect(result.current.divIcon).toBeInstanceOf(DivIcon)
-    expect(result.current.iconContainer).toBeEmptyDOMElement()
+    const { divIcon: initialDivIcon } = result.current
+
+    rerender({ ...initialOptions, className: newClassName })
+
     expect(result.current.divIcon).not.toBe(initialDivIcon)
+    expect(result.current.divIcon?.createIcon()).toHaveClass(newClassName)
+  })
+
+  test("when options change, should not replace iconContainer", () => {
+    const newClassName = "newClass"
+
+    const { result, rerender } = renderHookReactDivIcon()
+    const { iconContainer: initialIconContainer } = result.current
+
+    expect(result.current.iconContainer).toBeEmptyDOMElement()
+
+    rerender({ className: newClassName })
+
+    expect(result.current.divIcon?.createIcon()).toHaveClass(newClassName)
+    expect(result.current.iconContainer).toBeEmptyDOMElement()
     expect(result.current.iconContainer).toBe(initialIconContainer)
   })
 })
--- assets/tests/components/map/utilities/reactMarker.test.tsx
+++ assets/tests/components/map/utilities/reactMarker.test.tsx
@@ -4,7 +4,7 @@ import React, { ReactNode } from "react"
 import { MapContainer } from "react-leaflet"
 import { ReactMarker } from "../../../../src/components/map/utilities/reactMarker"
 import { localGeoCoordinateFactory } from "../../../factories/geoCoordinate"
-import { DivIconOptions } from "../../../../src/components/map/utilities/reactDivIcon"
+import { PointTuple } from "leaflet"
 
 const mapWrapper = ({ children }: { children: ReactNode }) => (
   <MapContainer center={[0, 0]} zoom={0}>
@@ -27,7 +27,7 @@ describe("ReactMarker", () => {
     expect(screen.getByTestId("hello!")).toBeVisible()
   })
 
-  test("when icon changes, should render changes onto map", () => {
+  test("when icon changes, should render react changes onto map", () => {
     const { latitude, longitude } = localGeoCoordinateFactory.build()
     const testId = "test-id"
     const newContent = "hello!"
@@ -53,15 +53,21 @@ describe("ReactMarker", () => {
     expect(icon).toHaveTextContent(newContent)
   })
 
-  test("when divIconSettings change, should render changes onto map", () => {
-    const opts: DivIconOptions = { iconSize: [20, 20] }
-    const { latitude, longitude } = localGeoCoordinateFactory.build()
+  test("when divIconSettings change, should update leaflet marker on map", () => {
     const testId = "test-id"
+
+    const { latitude, longitude } = localGeoCoordinateFactory.build()
+
+    const initialIconSize: PointTuple = [20, 20]
+    const [initialSizeX, initialSizeY] = initialIconSize
+
+    const newIconSize: PointTuple = [40, 50]
+    const [newSizeX, newSizeY] = newIconSize
 
     const { rerender } = render(
       <ReactMarker
         position={[latitude, longitude]}
-        divIconSettings={opts}
+        divIconSettings={{ iconSize: initialIconSize }}
         icon={<div data-testid={testId} />}
       />,
       { wrapper: mapWrapper }
@@ -72,21 +78,21 @@ describe("ReactMarker", () => {
     expect(icon).toBeEmptyDOMElement()
 
     expect(icon.parentElement?.parentElement).toHaveStyle({
-      width: "20px",
-      height: "20px",
+      width: `${initialSizeX}px`,
+      height: `${initialSizeY}px`,
     })
 
     rerender(
       <ReactMarker
         position={[latitude, longitude]}
-        divIconSettings={{ iconSize: [40, 50] }}
+        divIconSettings={{ iconSize: newIconSize }}
         icon={<div data-testid={testId} />}
       />
     )
 
     expect(icon.parentElement?.parentElement).toHaveStyle({
-      width: "40px",
-      height: "50px",
+      width: `${newSizeX}px`,
+      height: `${newSizeY}px`,
     })
   })
 })
