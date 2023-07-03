1f99fcb7b8a900143f9622d9f7ac71cbf0bb5625
Kayla Firestack <kfirestack@mbta.com>
2023-07-03 16:24:45 -0400

    doc(ts/contexts/streetViewModeEnabledContext): document context intention

--- assets/src/contexts/streetViewModeEnabledContext.tsx
+++ assets/src/contexts/streetViewModeEnabledContext.tsx
@@ -1,7 +1,7 @@
 import { createContext } from "react"
 
 /**
- *
+ * Provides the enabled or disabled state of the map street view mode.
  */
 const StreetViewModeEnabledContext = createContext(false)
 
