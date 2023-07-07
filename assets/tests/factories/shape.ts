16e69d23c3994a37fd77fa7a04b1b9a8dc6653a5
Kayla Firestack <kfirestack@mbta.com>
2023-07-07 09:58:09 -0400

    fix:test: name default export `shapeFactory`

--- assets/tests/factories/shape.ts
+++ assets/tests/factories/shape.ts
@@ -2,8 +2,10 @@ import { Factory } from "fishery"
 import { Shape } from "../../src/schedule"
 import stopFactory from "./stop"
 
-export default Factory.define<Shape>(({ sequence }) => ({
+const shapeFactory = Factory.define<Shape>(({ sequence }) => ({
   id: `shape${sequence}`,
   points: [{ lat: 0, lon: 0 }],
   stops: [stopFactory.build({ id: `stop${sequence}` })],
 }))
+
+export default shapeFactory
