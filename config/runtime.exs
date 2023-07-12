2dec4b1598cbff3f285a958ec279bdb7f5dad4ca
Maldonado <emaldonado@mbta.com>
2023-07-12 15:48:55 -0400

    chore: pull in place index name from environment variable

--- config/runtime.exs
+++ config/runtime.exs
@@ -8,7 +8,8 @@ config :skate,
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
   restrict_environment_access?: System.get_env("RESTRICT_ENVIRONMENT_ACCESS") == "true",
   base_tileset_url: System.get_env("BASE_TILESET_URL"),
-  satellite_tileset_url: System.get_env("SATELLITE_TILESET_URL")
+  satellite_tileset_url: System.get_env("SATELLITE_TILESET_URL"),
+  aws_place_index: System.get_env("AWS_PLACE_INDEX")
 
 config :ueberauth, Ueberauth.Strategy.Cognito,
   client_secret: System.get_env("COGNITO_CLIENT_SECRET")
