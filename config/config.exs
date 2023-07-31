c3f0ec4465e03a38b24e7b1440ede4b6844338de
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2023-07-31 11:10:52 -0400

    chore(Skate.Warmup): update retry configuration

--- config/config.exs
+++ config/config.exs
@@ -114,8 +114,8 @@ config :skate, Skate.Repo,
 
 config :skate, Skate.WarmUp,
   minimum_percent_queries_to_succeed: 0.6,
-  max_attempts: 5,
-  seconds_between_attempts: 3
+  max_attempts: 20,
+  seconds_between_attempts: 1
 
 config :laboratory,
   features: [
