9554a8edf98637143b53b0839b8387ae26d790eb
Kayla Firestack <kfirestack@mbta.com>
2023-07-31 09:23:51 -0400

    test: initial tests for `useAutocompleteResults`

--- assets/tests/hooks/useAutocompleteResults.test.ts
+++ assets/tests/hooks/useAutocompleteResults.test.ts
@@ -1,15 +1,103 @@
+import { Factory } from "fishery"
+import { useAutocompleteResults } from "../../src/hooks/useAutocompleteResults"
+import { SearchProperties } from "../../src/models/searchQuery"
+import { renderHook } from "@testing-library/react"
+import { makeMockSocket, makeMockChannel } from "../testHelpers/socketHelpers"
+import vehicleDataFactory from "../factories/vehicle_data"
+
+export const searchFiltersFactory = Factory.define<SearchProperties<boolean>>(
+  () => ({
+    location: true,
+    operator: true,
+    run: true,
+    vehicle: true,
+  })
+)
+
+export const searchFiltersOffFactory = searchFiltersFactory.params({
+  location: false,
+  operator: false,
+  run: false,
+  vehicle: false,
+})
+
 describe("useAutocompleteResults", () => {
-  test.todo("returns null initially")
+  test("returns empty lists initially", () => {
+    const { result } = renderHook(() =>
+      useAutocompleteResults(
+        undefined,
+        "searchText",
+        searchFiltersFactory.build()
+      )
+    )
+    expect(result.current).toEqual({
+      operator: [],
+      run: [],
+      vehicle: [],
+    })
+  })
 
-  test.todo("initializing the hook subscribes to the search results")
+  test("should subscribe all channel types with search query", () => {
+    const vehicleData = vehicleDataFactory.build()
 
-  test.todo(
-    "initializing the hook without a search query does not subscribe to the search results"
-  )
+    const mockSocket = makeMockSocket()
+    const mockChannel = makeMockChannel("ok", {
+      data: { matching_vehicles: [vehicleData], has_more_matches: false },
+    })
+    mockSocket.channel.mockImplementation(() => mockChannel)
 
-  test.todo("returns results pushed to the channel")
+    const searchText = "searchText"
 
-  test.todo("leaves the channel and joins a new one when the search changes")
+    renderHook(() =>
+      useAutocompleteResults(
+        mockSocket,
+        searchText,
+        searchFiltersFactory.build()
+      )
+    )
 
-  test.todo("leaves the channel when typing even if there is no new channel")
+    expect(mockSocket.channel).toHaveBeenCalledWith(
+      `vehicles_search:limited:operator:${searchText}`
+    )
+    expect(mockSocket.channel).toHaveBeenCalledWith(
+      `vehicles_search:limited:vehicle:${searchText}`
+    )
+    expect(mockSocket.channel).toHaveBeenCalledWith(
+      `vehicles_search:limited:run:${searchText}`
+    )
+    expect(mockChannel.join).toHaveBeenCalledTimes(3)
+  })
+
+  test("when category is filtered, should not subscribe to filtered channel", () => {
+    const vehicleData = vehicleDataFactory.build()
+
+    const mockSocket = makeMockSocket()
+    const mockChannel = makeMockChannel("ok", {
+      data: { matching_vehicles: [vehicleData], has_more_matches: false },
+    })
+    mockSocket.channel.mockImplementation(() => mockChannel)
+
+    const searchText = "searchText"
+
+    renderHook(() =>
+      useAutocompleteResults(
+        mockSocket,
+        searchText,
+        searchFiltersFactory.build({
+          vehicle: false,
+        })
+      )
+    )
+
+    expect(mockSocket.channel).toHaveBeenCalledWith(
+      `vehicles_search:limited:operator:${searchText}`
+    )
+    expect(mockSocket.channel).not.toHaveBeenCalledWith(
+      `vehicles_search:limited:vehicle:${searchText}`
+    )
+    expect(mockSocket.channel).toHaveBeenCalledWith(
+      `vehicles_search:limited:run:${searchText}`
+    )
+    expect(mockChannel.join).toHaveBeenCalledTimes(2)
+  })
 })
