d55edba1af7cb40056c0111d33f5c61003d8c8ec
Kayla Firestack <kfirestack@mbta.com>
2023-07-31 15:42:46 -0400

    feat:test(ts/util/operatorFormatting): add tests for name formatting and fix `formatOperatorName`

--- assets/src/util/operatorFormatting.tsx
+++ assets/src/util/operatorFormatting.tsx
@@ -1,21 +1,37 @@
-import { Vehicle } from "../realtime"
 import { joinTruthy } from "../helpers/dom"
+import { Vehicle } from "../realtime"
+
+export const DefaultFallbackString = "Not Available"
+
+type OptionParameters = {
+  fallbackText?: string
+}
 
 export const formatOperatorName = (
   operatorFirstName: string | null,
   operatorLastName: string | null,
-  operatorId: string | null
+  operatorId: string | null,
+  _options?: OptionParameters
 ): string => {
-  return joinTruthy(
-    [operatorFirstName, operatorLastName, `#${operatorId}`],
-    " "
+  return (
+    joinTruthy([
+      operatorFirstName,
+      operatorLastName,
+      operatorId && `#${operatorId}`,
+    ]) ||
+    _options?.fallbackText ||
+    DefaultFallbackString
   )
 }
 
-export const formatOperatorNameFromVehicle = (value: Vehicle): string => {
+export const formatOperatorNameFromVehicle = (
+  value: Vehicle,
+  options?: OptionParameters
+): string => {
   return formatOperatorName(
     value.operatorFirstName,
     value.operatorLastName,
-    value.operatorId
+    value.operatorId,
+    options
   )
 }
