66d82eb8baf2437551deae4b167dd1ccfd7e6d20
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2023-08-02 13:45:54 -0400

    fix(ShapeData): Remove unneeded connections field

--- assets/src/models/shapeData.ts
+++ assets/src/models/shapeData.ts
@@ -18,15 +18,6 @@ export const ShapeData = type({
         name: string(),
         lat: number(),
         lon: number(),
-        connections: optional(
-          array(
-            type({
-              type: number(),
-              id: string(),
-              name: string(),
-            })
-          )
-        ),
         routes: optional(
           array(
             type({
