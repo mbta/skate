0d45f536a9351742e654d008d789d486e45a65cd
Eddie Maldonado <emaldonado@mbta.com>
2023-08-23 13:08:24 -0400

    refactor: make TileLayerControl its own component

--- assets/src/components/map/controls/layersControl.tsx
+++ assets/src/components/map/controls/layersControl.tsx
@@ -46,41 +46,51 @@ export const LayersControl = ({
         <MapLayersIcon />
       </button>
       {showLayersList && (
-        <div className="c-layers-control__layers_list">
-          <ul className="list-group">
-            <li className="list-group-item">
-              <input
-                className="form-check-input"
-                type="radio"
-                name="tileType"
-                value=""
-                id="base"
-                checked={tileType === "base"}
-                onChange={() => setTileType("base")}
-              />
-              <label className="form-check-label" htmlFor="base">
-                Map (default)
-              </label>
-            </li>
-            <li className="list-group-item">
-              <input
-                className="form-check-input"
-                type="radio"
-                name="tileType"
-                value=""
-                id="satellite"
-                checked={tileType === "satellite"}
-                onChange={() => setTileType("satellite")}
-              />
-              <label className="form-check-label" htmlFor="satellite">
-                Satellite
-              </label>
-            </li>
-          </ul>
-        </div>
+        <TileLayerControl tileType={tileType} setTileType={setTileType} />
       )}
     </CustomControl>
   )
 }
+
+const TileLayerControl = ({
+  tileType,
+  setTileType,
+}: {
+  tileType: TileType
+  setTileType: (tileType: TileType) => void
+}): JSX.Element => (
+  <div className="c-layers-control__layers_list">
+    <ul className="list-group">
+      <li className="list-group-item">
+        <input
+          className="form-check-input"
+          type="radio"
+          name="tileType"
+          value=""
+          id="base"
+          checked={tileType === "base"}
+          onChange={() => setTileType("base")}
+        />
+        <label className="form-check-label" htmlFor="base">
+          Map (default)
+        </label>
+      </li>
+      <li className="list-group-item">
+        <input
+          className="form-check-input"
+          type="radio"
+          name="tileType"
+          value=""
+          id="satellite"
+          checked={tileType === "satellite"}
+          onChange={() => setTileType("satellite")}
+        />
+        <label className="form-check-label" htmlFor="satellite">
+          Satellite
+        </label>
+      </li>
+    </ul>
+  </div>
+)
 
 LayersControl.WithTileContext = LayersControlWithTileContext
