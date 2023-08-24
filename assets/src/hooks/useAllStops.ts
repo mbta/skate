06b65eeb652f5f17296e5f53faed88d0e076dfe2
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2023-08-24 14:11:58 -0400

    doc(useAllStops): Add function doc w/ return type

--- assets/src/hooks/useAllStops.ts
+++ assets/src/hooks/useAllStops.ts
@@ -2,8 +2,11 @@ import { useEffect, useState } from "react"
 import { Stop } from "../schedule"
 import { fetchAllStops } from "../api"
 
+/**
+ * A hook to fetch all stops (stations + stops)
+ * @returns a list of {@link Stop}, or null if loading.
+ */
 export const useAllStops = (): Stop[] | null => {
-  // null means loading
   const [stops, setStops] = useState<Stop[] | null>(null)
 
   useEffect(() => {
