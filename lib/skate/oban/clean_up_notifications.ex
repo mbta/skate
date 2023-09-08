1f77d9e26c1fe11dc35ed684d4fd9329ca44da6d
Kayla Firestack <kfirestack@mbta.com>
2023-09-08 10:54:36 -0400

    feat(ex/skate/oban): implement deletion job

--- lib/skate/oban/clean_up_notifications.ex
+++ lib/skate/oban/clean_up_notifications.ex
@@ -1,10 +1,22 @@
 defmodule Skate.Oban.CleanUpNotifications do
   use Oban.Worker,
-    queue: :default
+    queue: :default,
     unique: [period: 300]
+
+  import Ecto.Query
+
+  @seconds_per_day 24 * 60 * 60
 
   @impl Oban.Worker
   def perform(%Oban.Job{}) do
+    oldest_date = DateTime.now!("Etc/UTC") |> DateTime.add(-100 * @seconds_per_day)
+
+    from(n in Notifications.Db.Notification,
+      where: n.created_at < ^oldest_date,
+      limit: 100
+    )
+    |> Skate.Repo.delete_all()
+
     :ok
   end
 end
