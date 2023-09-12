307ccb30cb2ea9676e4204699dbce07ff55c57e3
Eddie Maldonado <emaldonado@mbta.com>
2023-09-12 16:56:21 -0400

    chore: trip back notifications older than 1 day

--- config/config.exs
+++ config/config.exs
@@ -129,7 +129,7 @@ config :skate, Oban,
       Oban.Plugins.Cron,
       crontab: [
         {"*/15 * * * *", Skate.Oban.CleanUpNotifications,
-         args: %{"cutoff_days" => 10, "limit" => 1000}}
+         args: %{"cutoff_days" => 1, "limit" => 1000}}
       ]
     }
   ]
