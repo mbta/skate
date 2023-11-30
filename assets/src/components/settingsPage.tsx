d99b3b083b30bd4ea138f1e412bb37f12bf3a4c3
Kayla Firestack <firestack@users.noreply.github.com>
2023-11-30 17:08:46 -0500

    fix(ts/components/SettingsPage): fix settings label's and order (#2292)
    * fix(ts/components/settingsPage): fix toggle labels
    
    * fix(ts/components/settingsPage): move shuttle setting to after vehicle setting to match figma

--- assets/src/components/settingsPage.tsx
+++ assets/src/components/settingsPage.tsx
@@ -34,32 +34,6 @@ const SettingsPage = (): ReactElement<HTMLDivElement> => {
                 Vehicle labels on <strong>regular</strong> buses
               </>
             }
-            settingName="shuttle-vehicle-label"
-            value={userSettings.shuttleVehicleLabel}
-            onChange={(value) => {
-              const newSetting: VehicleLabelSetting = parseInt(value, 10)
-              dispatch(setShuttleVehicleLabelSetting(newSetting))
-              putShuttleVehicleLabel(newSetting)
-            }}
-            options={[
-              {
-                label: "Run number",
-                value: VehicleLabelSetting.RunNumber,
-                optionId: "shuttle-vehicle-label-run-number",
-              },
-              {
-                label: "Vehicle number",
-                value: VehicleLabelSetting.VehicleNumber,
-                optionId: "shuttle-vehicle-label-vehicle-number",
-              },
-            ]}
-          />
-          <ToggleSetting
-            label={
-              <>
-                Vehicle labels on <strong>shuttle</strong> buses
-              </>
-            }
             settingName="ladder-vehicle-label"
             value={userSettings.ladderVehicleLabel}
             onChange={(value) => {
@@ -77,6 +51,32 @@ const SettingsPage = (): ReactElement<HTMLDivElement> => {
                 label: "Vehicle number",
                 value: VehicleLabelSetting.VehicleNumber,
                 optionId: "ladder-vehicle-label-vehicle-number",
+              },
+            ]}
+          />
+          <ToggleSetting
+            label={
+              <>
+                Vehicle labels on <strong>shuttle</strong> buses
+              </>
+            }
+            settingName="shuttle-vehicle-label"
+            value={userSettings.shuttleVehicleLabel}
+            onChange={(value) => {
+              const newSetting: VehicleLabelSetting = parseInt(value, 10)
+              dispatch(setShuttleVehicleLabelSetting(newSetting))
+              putShuttleVehicleLabel(newSetting)
+            }}
+            options={[
+              {
+                label: "Run number",
+                value: VehicleLabelSetting.RunNumber,
+                optionId: "shuttle-vehicle-label-run-number",
+              },
+              {
+                label: "Vehicle number",
+                value: VehicleLabelSetting.VehicleNumber,
+                optionId: "shuttle-vehicle-label-vehicle-number",
               },
             ]}
           />
