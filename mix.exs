5903503e9a8b37153284b89dd297e2d711e5624a
Kayla Firestack <kfirestack@mbta.com>
2023-12-01 09:08:03 -0500

    update(mix): update elixir to 1.15

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Skate.MixProject do
     [
       app: :skate,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
