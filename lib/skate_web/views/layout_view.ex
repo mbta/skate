1e527411f00f85450a5329ffc08a5542f540761a
Josh Larson <jlarson@mbta.com>
2023-12-07 13:28:10 -0500

    fixup! feat(ex/phoenix): Introduce LiveView and try to fix layouts

--- lib/skate_web/views/layout_view.ex
+++ lib/skate_web/views/layout_view.ex
@@ -1,6 +1,6 @@
 defmodule SkateWeb.LayoutView do
-  use(SkateWeb, :html)
-  use(SkateWeb, :view)
+  use SkateWeb, :html
+  use SkateWeb, :view
 
   @spec record_fullstory?() :: boolean
   def record_fullstory?, do: Application.get_env(:skate, :record_fullstory, false)
