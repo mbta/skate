d9fe21a7da87b7958e603c2d2221e922aa00f312
Eddie Maldonado <emaldonado@mbta.com>
2023-12-19 19:02:03 -0500

    test: more unit tests for detour drawing

--- assets/tests/components/detours/detourMap.test.tsx
+++ assets/tests/components/detours/detourMap.test.tsx
@@ -4,6 +4,7 @@ import React from "react"
 import { DetourMap } from "../../../src/components/detours/detourMap"
 import { routePatternFactory } from "../../factories/routePattern"
 import userEvent from "@testing-library/user-event"
+import "@testing-library/jest-dom/jest-globals"
 
 beforeEach(() => {
   jest.spyOn(global, "scrollTo").mockImplementationOnce(jest.fn())
@@ -37,5 +38,81 @@ describe("DetourMap", () => {
     )
 
     expect(screen.getByTitle("Detour End")).not.toBeNull()
+  })
+
+  test("clicking on map while drawing a detour adds a shape", async () => {
+    const { container } = render(
+      <DetourMap routePattern={routePatternFactory.build()} />
+    )
+
+    await userEvent.click(
+      container.querySelector(".c-detour_map--original-route-shape")!
+    )
+
+    await userEvent.click(container.querySelector(".c-vehicle-map")!)
+
+    expect(
+      container.querySelectorAll(".c-detour_map-circle-marker--detour-point")
+    ).toHaveLength(1)
+  })
+
+  test("clicking on 'Clear Last Waypoint' removes last point from detour", async () => {
+    const { container } = render(
+      <DetourMap routePattern={routePatternFactory.build()} />
+    )
+
+    await userEvent.click(
+      container.querySelector(".c-detour_map--original-route-shape")!
+    )
+
+    await userEvent.click(container.querySelector(".c-vehicle-map")!)
+
+    await userEvent.click(
+      screen.getByRole("button", { name: "Clear Last Waypoint" })
+    )
+
+    expect(
+      container.querySelectorAll(".c-detour_map-circle-marker--detour-point")
+    ).toHaveLength(0)
+  })
+
+  test("'Clear Last Waypoint' is disabled before detour drawing is started", () => {
+    render(<DetourMap routePattern={routePatternFactory.build()} />)
+
+    expect(
+      screen.getByRole("button", { name: "Clear Last Waypoint" })
+    ).toBeDisabled()
+  })
+
+  test("'Clear Last Waypoint' is disabled when detour drawing has started but no waypoints are present", async () => {
+    const { container } = render(
+      <DetourMap routePattern={routePatternFactory.build()} />
+    )
+
+    await userEvent.click(
+      container.querySelector(".c-detour_map--original-route-shape")!
+    )
+
+    expect(
+      screen.getByRole("button", { name: "Clear Last Waypoint" })
+    ).toBeDisabled()
+  })
+
+  test("'Clear Last Waypoint' is disabled when detour drawing has finished", async () => {
+    const { container } = render(
+      <DetourMap routePattern={routePatternFactory.build()} />
+    )
+
+    await userEvent.click(
+      container.querySelector(".c-detour_map--original-route-shape")!
+    )
+
+    await userEvent.click(
+      container.querySelector(".c-detour_map--original-route-shape")!
+    )
+
+    expect(
+      screen.getByRole("button", { name: "Clear Last Waypoint" })
+    ).toBeDisabled()
   })
 })
