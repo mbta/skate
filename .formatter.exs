554c7019d2405f74ca728fd328d36f0ae31eedec
Josh Larson <jlarson@mbta.com>
2023-12-21 18:26:14 -0500

    chore: Format migration files

--- .formatter.exs
+++ .formatter.exs
@@ -2,5 +2,10 @@
   import_deps: [:ecto, :ecto_sql, :phoenix],
   plugins: [Phoenix.LiveView.HTMLFormatter],
   subdirectories: ["priv/*/migrations"],
-  inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}", "priv/*/seeds.exs"]
+  inputs: [
+    "*.{heex,ex,exs}",
+    "{config,lib,test}/**/*.{heex,ex,exs}",
+    "priv/*/seeds.exs",
+    "priv/repo/migrations/*.exs"
+  ]
 ]
--- priv/repo/migrations/20200928172840_create_user_settings.exs
+++ priv/repo/migrations/20200928172840_create_user_settings.exs
@@ -21,7 +21,10 @@ defmodule Skate.Repo.Migrations.CreateUserSettings do
     )
 
     create table(:user_settings, primary_key: false) do
-      add(:user_id, references(:users, on_delete: :delete_all, on_update: :update_all), primary_key: true)
+      add(:user_id, references(:users, on_delete: :delete_all, on_update: :update_all),
+        primary_key: true
+      )
+
       add(:ladder_page_vehicle_label, :vehicle_label)
       add(:shuttle_page_vehicle_label, :vehicle_label)
       timestamps()
