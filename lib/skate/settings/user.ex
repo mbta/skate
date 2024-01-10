19a2a72793bd3ca303dbee0c28c175720c84792b
Josh Larson <jlarson@mbta.com>
2024-01-10 11:00:18 -0500

    chore: Remove User.in_test_group?/2, which isn't called anywhere

--- lib/skate/settings/user.ex
+++ lib/skate/settings/user.ex
@@ -75,13 +75,6 @@ defmodule Skate.Settings.User do
     Skate.Repo.all(from(u in users_for_route_ids_query(route_ids), select: u.id))
   end
 
-  def in_test_group?(user_id, test_group_name) do
-    user_id
-    |> get_by_id!()
-    |> all_test_group_names()
-    |> Enum.member?(test_group_name)
-  end
-
   def all_test_group_names(user) do
     user = Skate.Repo.preload(user, :test_groups)
 
