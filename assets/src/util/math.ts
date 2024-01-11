4bf28cd6ab8d8c267beec136bebb90d92f852651
Kayla Firestack <kfirestack@mbta.com>
2024-01-11 09:13:10 -0500

    fix(ts/math): fix and test `closestPosition`
    The old implementation was not tested and returned the input `position` instead of the closest point within `positions`.

--- assets/src/util/math.ts
+++ assets/src/util/math.ts
@@ -1,3 +1,5 @@
+import { LatLng, LatLngExpression } from "leaflet"
+
 /**
  * A helper function to help clarify code when clamping a value between a range.
  * (Remove when tc39 implements `Math.clamp`)
@@ -7,3 +9,26 @@
  */
 export const clamp = (value: number, min: number, max: number): number =>
   Math.min(Math.max(min, value), max)
+
+/**
+ * Finds the element in {@link positions} that is closest to {@link point}
+ * @param positions List of coordinates to search
+ * @param point Point of reference to check distance to
+ * @returns The closest point in {@link positions} to {@link point}
+ */
+export const closestPosition = (
+  positions: LatLngExpression[],
+  point: LatLng
+) => {
+  const positionsByDistance = positions
+    .map((currentPosition, index) => ({
+      distance: point.distanceTo(currentPosition),
+      position: currentPosition,
+      index,
+    }))
+    .sort((lhs, rhs) => lhs.distance - rhs.distance)
+
+  return positionsByDistance[0] ?? undefined
+
+  // Interpolate here
+}
--- assets/tests/util/math.test.ts
+++ assets/tests/util/math.test.ts
@@ -1,5 +1,6 @@
-import { describe, test, expect } from "@jest/globals"
-import { clamp } from "../../src/util/math"
+import { describe, test, expect, it } from "@jest/globals"
+import { clamp, closestPosition } from "../../src/util/math"
+import { LatLngExpression, latLng } from "leaflet"
 
 describe("clamp", () => {
   test("when value is below minimum, returns minimum value", () => {
@@ -12,5 +13,34 @@ describe("clamp", () => {
 
   test("when value is between minimum and maximum, returns value", () => {
     expect(clamp(5, 0, 10)).toBe(5)
+  })
+})
+
+describe("closestPosition", () => {
+  it("returns `undefined` if provided a empty list", () => {
+    expect(closestPosition([], latLng(0, 0))).toBeUndefined()
+  })
+
+  it("returns the closest point from the provided list", () => {
+    const point = latLng(0, 0)
+
+    const closestPoint: LatLngExpression = {
+      lat: point.lat + 1,
+      lng: 0,
+    }
+    const index = 3
+
+    // Generate range and offset so all are farther than `position`
+    const positions: LatLngExpression[] = Array(5).map((_, idx) => [
+      point.lat + closestPoint.lat + idx,
+      0,
+    ])
+
+    positions[index] = closestPoint
+
+    expect(closestPosition(positions, point)).toMatchObject({
+      index,
+      position: closestPoint,
+    })
   })
 })
