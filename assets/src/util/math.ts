16fdac1faadca6f1f1112b8ae0665416a32db45a
Kayla Firestack <kfirestack@mbta.com>
2024-01-11 09:44:33 -0500

    feedback:doc(ts/math/closestPosition): describe return value in detail

--- assets/src/util/math.ts
+++ assets/src/util/math.ts
@@ -10,16 +10,29 @@ import { LatLng, LatLngExpression } from "leaflet"
 export const clamp = (value: number, min: number, max: number): number =>
   Math.min(Math.max(min, value), max)
 
+
+interface ClosestPosition {
+  position: LatLngExpression,
+  index: number
+  distance: number
+}
+
 /**
  * Finds the element in {@link positions} that is closest to {@link point}
  * @param positions List of coordinates to search
  * @param point Point of reference to check distance to
- * @returns The closest point in {@link positions} to {@link point}
+ * @returns
+ * if {@link positions} is empty, returns `undefined`
+ *
+ * Otherwise, returns a {@link ClosestPosition} object containing
+ * - the closest element in {@link positions} to {@link point}
+ * - the index of the element in {@link positions}
+ * - the distance to {@link point}
  */
 export const closestPosition = (
   positions: LatLngExpression[],
   point: LatLng
-) => {
+): ClosestPosition | undefined => {
   const positionsByDistance = positions
     .map((currentPosition, index) => ({
       distance: point.distanceTo(currentPosition),
