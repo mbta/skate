95d12afa7bc509a0fb10c0143badf6fd3cb596a6
Josh Larson <jlarson@mbta.com>
2024-01-11 17:35:51 -0500

    refactor: Simplify conversion from DetourShapeData to DetourShape

--- assets/src/models/detourShapeData.ts
+++ assets/src/models/detourShapeData.ts
@@ -11,10 +11,5 @@ export const DetourShapeData = type({
 })
 export type DetourShapeData = Infer<typeof DetourShapeData>
 
-export const detourShapeFromData = (
-  shapeData: DetourShapeData
-): DetourShape => ({
-  coordinates: shapeData.coordinates.map((pointData) => {
-    return { lat: pointData.lat, lon: pointData.lon }
-  }),
-})
+export const detourShapeFromData = (shapeData: DetourShapeData): DetourShape =>
+  shapeData
