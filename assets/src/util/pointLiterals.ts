c79d9dcae0e399bb9f9f0474c09a792f206084c6
Josh Larson <jlarson@mbta.com>
2024-01-17 13:36:17 -0500

    fixup! refactor: clean up point literal helper functions

--- assets/src/util/pointLiterals.ts
+++ assets/src/util/pointLiterals.ts
@@ -1,10 +1,12 @@
 import { LatLngLiteral } from "leaflet"
 import { ShapePoint } from "../schedule"
 
-export const latLngLiteralToShapePoint = (
-  {lat, lng}: LatLngLiteral
-): ShapePoint => ({ lat, lon: lng })
+export const latLngLiteralToShapePoint = ({
+  lat,
+  lng,
+}: LatLngLiteral): ShapePoint => ({ lat, lon: lng })
 
-export const shapePointToLatLngLiteral = (
-  {lat, lon}: ShapePoint
-): LatLngLiteral => ({ lat, lng: lon })
+export const shapePointToLatLngLiteral = ({
+  lat,
+  lon,
+}: ShapePoint): LatLngLiteral => ({ lat, lng: lon })
