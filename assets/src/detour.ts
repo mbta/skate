5011881b8c80abf96f51a9c5114df10d95541f19
Kayla Firestack <kfirestack@mbta.com>
2024-01-24 09:04:27 -0500

    cleanup(ts/detour.ts): remove direction name and type

--- assets/src/detour.ts
+++ assets/src/detour.ts
@@ -1,26 +1,8 @@
 import { ShapePoint } from "./schedule"
 
-export type DirectionType =
-  | "left"
-  | "right"
-  | "sharp_left"
-  | "sharp_right"
-  | "slight_left"
-  | "slight_right"
-  | "straight"
-  | "enter_roundabout"
-  | "exit_roundabout"
-  | "u_turn"
-  | "goal"
-  | "depart"
-  | "keep_left"
-  | "keep_right"
-
 export interface DetourShape {
   coordinates: ShapePoint[]
   directions: {
     instruction: string
-    name: string
-    type: DirectionType
   }[]
 }
