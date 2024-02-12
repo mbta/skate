10ac59be1b5b77c54e3083ffbbb24a2eb8f026e3
Eddie Maldonado <emaldonado@mbta.com>
2024-02-12 11:24:39 -0500

    feat: more interactive states for circle button

--- assets/src/components/circleButton.tsx
+++ assets/src/components/circleButton.tsx
@@ -1,13 +1,22 @@
 import React, { ReactNode } from "react"
+import { joinClasses } from "../helpers/dom"
 
 export const CircleButton = ({
   children,
   onClick,
+  isActive,
 }: {
   children: ReactNode
   onClick: () => void
+  isActive: boolean
 }) => (
-  <button className="c-circle-button" onClick={onClick}>
+  <button
+    className={joinClasses([
+      "c-circle-button",
+      isActive && "c-circle-button--active",
+    ])}
+    onClick={onClick}
+  >
     <div className="c-circle-button__content">{children}</div>
   </button>
 )
