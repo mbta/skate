be8ccedfd66bd2f6f0810a3cf22fa66f949d45d1
Eddie Maldonado <emaldonado@mbta.com>
2024-02-12 11:36:22 -0500

    fixup! feat: more interactive states for circle button

--- assets/stories/skate-components/circleButton.stories.tsx
+++ assets/stories/skate-components/circleButton.stories.tsx
@@ -12,5 +12,5 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-  args: { children: <UserAvatar userName="foo" /> },
+  args: { children: <UserAvatar userName="foo" />, isActive: false },
 }
