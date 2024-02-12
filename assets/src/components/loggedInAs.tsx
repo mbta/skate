8b381a6ce4b5c011ff26fa5444918945b94e3572
Josh Larson <jlarson@mbta.com>
2024-02-12 15:19:42 -0500

    fixup! feat: Add <LoggedInAs /> component

--- assets/src/components/loggedInAs.tsx
+++ assets/src/components/loggedInAs.tsx
@@ -1,8 +1,11 @@
 import React from "react"
+import { UserAvatar } from "./userAvatar"
 
 export const LoggedInAs = ({ email }: { email: string }) => (
   <div className="c-logged-in-as__container">
-    <div className="c-logged-in-as__icon">{email[0]?.toUpperCase() ?? ""}</div>
+    <div className="c-logged-in-as__icon">
+      <UserAvatar userName={email} />
+    </div>
     <div className="c-logged-in-as__text">
       <p>Logged in as</p>
       <p className="c-logged-in-as__email">{email}</p>
