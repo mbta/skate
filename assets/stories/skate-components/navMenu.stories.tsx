5d1930487a1de13df3a2876b2ffc244768e77133
Kayla Firestack <kfirestack@mbta.com>
2024-02-13 10:25:21 -0500

    feat(sb/components/navMenu): add stories for mobile `navMenu`

---
+++ assets/stories/skate-components/navMenu.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import NavMenu from "../../src/components/nav/navMenu"
+import { MemoryRouter } from "react-router-dom"
+import React from "react"
+
+const meta = {
+  component: NavMenu,
+  args: {
+    mobileMenuIsOpen: true,
+  },
+  decorators: [
+    (StoryFn) => (
+      <MemoryRouter>
+        <StoryFn />
+      </MemoryRouter>
+    ),
+  ],
+} satisfies Meta<typeof NavMenu>
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Story: Story = {}
