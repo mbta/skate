7a535c44619639dfb6f5a67a1f567440cd3658c2
Eddie Maldonado <emaldonado@mbta.com>
2024-02-13 16:18:17 -0500

    CircleButton and UserAvatar components (#2388)
    * feat: user avatar icon
    
    * feat: basic circular top nav button
    
    * feat: more interactive states for circle button
    
    * fixup! feat: more interactive states for circle button
    
    * feat: more complete circle button
    
    * fix: naming of CircleButton tests
    
    * fix: some accessibility improvements
    
    * refactor: use Bootstrap primary color directly
    
    * fix: use :focus-visible for consistency
    
    * fix: fixup some colors to use 2024 tokens
    
    * fix: tweak selector to adjust specificity

---
+++ assets/src/components/circleButton.tsx
@@ -0,0 +1,26 @@
+import React, { ReactNode } from "react"
+import { joinClasses } from "../helpers/dom"
+
+export const CircleButton = ({
+  children,
+  onClick,
+  isActive,
+  title,
+}: {
+  children?: ReactNode
+  onClick?: () => void
+  isActive: boolean
+  title?: string
+}) => (
+  <button
+    className={joinClasses([
+      "c-circle-button",
+      isActive && "c-circle-button--active",
+    ])}
+    onClick={onClick}
+    title={title}
+    aria-pressed={isActive}
+  >
+    <div className="c-circle-button__content">{children}</div>
+  </button>
+)
