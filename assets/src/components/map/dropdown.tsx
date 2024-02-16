7519d4241c947c19ba6f0e69ac729a27dad8ebd4
Josh Larson <jlarson@mbta.com>
2024-02-16 17:09:40 -0500

    refactor: Make both map dropdown components inherit all configurable properties from their react-bootstrap counterparts

--- assets/src/components/map/dropdown.tsx
+++ assets/src/components/map/dropdown.tsx
@@ -1,27 +1,16 @@
-import React, { PropsWithChildren } from "react"
+import React, { ComponentProps } from "react"
 import { Dropdown } from "react-bootstrap"
 
-interface DropdownMenuProps extends PropsWithChildren {}
-
-export const DropdownMenu = ({ children }: DropdownMenuProps) => {
+export const DropdownMenu = (props: ComponentProps<typeof Dropdown.Menu>) => {
   return (
     <Dropdown.Menu
       className="c-dropdown-popup-menu border-box inherit-box"
       show
-    >
-      {children}
-    </Dropdown.Menu>
+      {...props}
+    />
   )
 }
 
-interface DropdownItemProps extends PropsWithChildren {
-  onClick?: () => void
-}
-
-export const DropdownItem = ({ children, onClick }: DropdownItemProps) => {
-  return (
-    <Dropdown.Item as="button" onClick={onClick}>
-      {children}
-    </Dropdown.Item>
-  )
+export const DropdownItem = (props: ComponentProps<typeof Dropdown.Item>) => {
+  return <Dropdown.Item as="button" {...props} />
 }
