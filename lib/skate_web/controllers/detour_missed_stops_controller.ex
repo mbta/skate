28f1c1607b7de512192839be56669de085963dfb
Eddie Maldonado <emaldonado@mbta.com>
2024-02-23 10:50:16 -0500

    refactor: put calls that get mocked into private functions

--- lib/skate_web/controllers/detour_missed_stops_controller.ex
+++ lib/skate_web/controllers/detour_missed_stops_controller.ex
@@ -10,29 +10,16 @@ defmodule SkateWeb.DetourMissedStopsController do
         "connection_start" => connection_start,
         "connection_end" => connection_end
       }) do
-    route_pattern_fn =
-      Application.get_env(:skate_web, :route_pattern_fn, &Schedule.route_pattern/1)
-
-    shape_with_stops_fn =
-      Application.get_env(
-        :skate_web,
-        :shape_with_stops_fn,
-        &Schedule.shape_with_stops_for_trip/1
-      )
-
-    missed_stops_fn =
-      Application.get_env(:skate_web, :missed_stops_fn, &MissedStops.missed_stops/1)
-
-    with route_pattern <- route_pattern_fn.(route_pattern_id),
+    with route_pattern <- route_pattern(route_pattern_id),
          false <- is_nil(route_pattern),
          shape_with_stops <-
-           shape_with_stops_fn.(route_pattern.representative_trip_id),
+           shape_with_stops(route_pattern.representative_trip_id),
          false <- is_nil(shape_with_stops) do
       connection_start_location = Location.new(connection_start["lat"], connection_start["lon"])
       connection_end_location = Location.new(connection_end["lat"], connection_end["lon"])
 
       missed_stops =
-        missed_stops_fn.(%MissedStops{
+        missed_stops(%MissedStops{
           connection_start: connection_start_location,
           connection_end: connection_end_location,
           stops: shape_with_stops.stops,
@@ -43,5 +30,23 @@ defmodule SkateWeb.DetourMissedStopsController do
     else
       _ -> send_resp(conn, :bad_request, "bad request")
     end
+  end
+
+  defp route_pattern(route_pattern_id) do
+    Application.get_env(:skate_web, :route_pattern_fn, &Schedule.route_pattern/1).(
+      route_pattern_id
+    )
+  end
+
+  defp shape_with_stops(trip_id) do
+    Application.get_env(
+      :skate_web,
+      :shape_with_stops_fn,
+      &Schedule.shape_with_stops_for_trip/1
+    ).(trip_id)
+  end
+
+  defp missed_stops(cfg) do
+    Application.get_env(:skate_web, :missed_stops_fn, &MissedStops.missed_stops/1).(cfg)
   end
 end
