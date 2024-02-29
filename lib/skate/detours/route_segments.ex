434618f81afa36e2eaff1bafade928165ffd6e88
Josh Larson <jlarson@mbta.com>
2024-02-29 13:29:49 -0500

    fixup! feat: Add algorithm for segmenting a route based on connection points

--- lib/skate/detours/route_segments.ex
+++ lib/skate/detours/route_segments.ex
@@ -5,11 +5,7 @@ defmodule Skate.Detours.RouteSegments do
 
   defmodule Params do
     @moduledoc false
-    @type t :: %__MODULE__{
-            connection_start: Util.Location.From.t(),
-            connection_end: Util.Location.From.t(),
-            shape: nonempty_list(Util.Location.From.t())
-          }
+    @type t :: %__MODULE__{}
     @enforce_keys [:connection_start, :connection_end, :shape]
     defstruct [:connection_start, :connection_end, :shape]
   end
@@ -31,24 +27,34 @@ defmodule Skate.Detours.RouteSegments do
   ## Examples
       iex> alias Util.Location
       iex> Skate.Detours.RouteSegments.route_segments(
-      ...>   %Skate.Detours.RouteSegments.Params{
-      ...>     shape: [Location.new(0, 0), Util.Location.new(0, 1), Util.Location.new(0, 2), Util.Location.new(0, 3)],
-      ...>     connection_start: Util.Location.new(0, 1),
-      ...>     connection_end: Util.Location.new(0, 2)
-      ...>   }
+      ...>   [Location.new(0, 0), Util.Location.new(0, 1), Util.Location.new(0, 2), Util.Location.new(0, 3)],
+      ...>   Util.Location.new(0, 1),
+      ...>   Util.Location.new(0, 2)
       ...> )
-      %Skate.Detours.RouteSegments.Result{
-        before_detour: [%Util.Location{latitude: 0, longitude: 0}, %Util.Location{latitude: 0, longitude: 1}],
-        detour: [%Util.Location{latitude: 0, longitude: 1}, %Util.Location{latitude: 0, longitude: 2}],
-        after_detour: [%Util.Location{latitude: 0, longitude: 2}, %Util.Location{latitude: 0, longitude: 3}]
-      }
+      {:ok,
+        %Skate.Detours.RouteSegments.Result{
+          before_detour: [%Util.Location{latitude: 0, longitude: 0}, %Util.Location{latitude: 0, longitude: 1}],
+          detour: [%Util.Location{latitude: 0, longitude: 1}, %Util.Location{latitude: 0, longitude: 2}],
+          after_detour: [%Util.Location{latitude: 0, longitude: 2}, %Util.Location{latitude: 0, longitude: 3}]
+        }}
   """
-  @spec route_segments(Params.t()) :: Result.t()
-  def route_segments(%__MODULE__.Params{
-        connection_start: connection_start,
-        connection_end: connection_end,
-        shape: shape
-      }) do
+  @spec route_segments(
+          nonempty_list(Util.Location.From.t()),
+          Util.Location.From.t(),
+          Util.Location.From.t()
+        ) :: {:ok, Result.t()} | :error
+  def route_segments(
+        [],
+        _connection_start,
+        _connection_end
+      ),
+      do: :error
+
+  def route_segments(
+        shape,
+        connection_start,
+        connection_end
+      ) do
     {_, start_index} =
       shape
       |> Enum.with_index()
@@ -59,10 +65,11 @@ defmodule Skate.Detours.RouteSegments do
       |> Enum.with_index()
       |> Enum.min_by(fn {point, _index} -> Util.Location.distance(point, connection_end) end)
 
-    %__MODULE__.Result{
-      before_detour: Enum.slice(shape, 0..start_index),
-      detour: Enum.slice(shape, start_index..end_index),
-      after_detour: Enum.slice(shape, end_index..-1)
-    }
+    {:ok,
+     %__MODULE__.Result{
+       before_detour: Enum.slice(shape, 0..start_index),
+       detour: Enum.slice(shape, start_index..end_index),
+       after_detour: Enum.slice(shape, end_index..-1)
+     }}
   end
 end
