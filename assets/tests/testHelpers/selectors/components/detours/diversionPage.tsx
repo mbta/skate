0f1e0029a734db6403e09150aa61b84652a90c13
Kayla Firestack <kfirestack@mbta.com>
2024-03-04 10:00:12 -0500

    fix:test: allow standalone expect in selector function

--- assets/tests/testHelpers/selectors/components/detours/diversionPage.tsx
+++ assets/tests/testHelpers/selectors/components/detours/diversionPage.tsx
@@ -8,6 +8,7 @@ export const originalRouteShape = {
     const maybeShape = container.querySelector(
       ".c-detour_map--original-route-shape"
     )
+    /* eslint-disable-next-line jest/no-standalone-expect */
     expect(maybeShape).not.toBeNull()
     return maybeShape as Element
   },
