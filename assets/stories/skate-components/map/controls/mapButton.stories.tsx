457cb2884400a0c2f214a7eb71b16c2dee65bd4d
Eddie Maldonado <emaldonado@mbta.com>
2024-03-04 14:14:59 -0500

    feat: get button working inside map component in stories

--- assets/stories/skate-components/map/controls/mapButton.stories.tsx
+++ assets/stories/skate-components/map/controls/mapButton.stories.tsx
@@ -1,6 +1,10 @@
 import type { Meta, StoryObj } from "@storybook/react"
 
+import React from "react"
+
 import { MapButton } from "../../../../src/components/map/controls/mapButton"
+import { inMapDecorator } from "../../../../.storybook/inMapDecorator"
+import { CustomControl } from "../../../../src/components/map/controls/customControl"
 
 const meta = {
   component: MapButton,
@@ -25,3 +29,22 @@ export const Small: Story = { args: { size: "s" } }
 export const Medium: Story = { args: { size: "m" } }
 
 export const Large: Story = { args: { size: "l" } }
+
+export const Active: Story = { args: { active: true } }
+
+export const Disabled: Story = { args: { disabled: true } }
+
+export const InMap: Story = {
+  parameters: {
+    layout: "fullscreen",
+    stretch: true,
+  },
+  render(args) {
+    return (
+      <CustomControl position="bottomleft">
+        <MapButton {...args} />
+      </CustomControl>
+    )
+  },
+  decorators: [inMapDecorator],
+}
