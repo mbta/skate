4e1a8765e192092d4ca59d3798765b1d54e4d743
Kayla Firestack <kfirestack@mbta.com>
2024-03-04 12:28:34 -0500

    fix:test: remove duplicate test? (review!)

--- assets/tests/components/propertiesList.test.tsx
+++ assets/tests/components/propertiesList.test.tsx
@@ -213,26 +213,9 @@ describe("vehicleProperties", () => {
   })
 
   test("operator information gives last name if that's all that's available", () => {
-    const vehicleSansOperator = {
-      ...vehicle,
-      operatorId: "1234",
-      operatorFirstName: null,
-      operatorLastName: "SMITH",
-    } as unknown
-
-    const properties = vehicleProperties(
-      vehicleSansOperator as VehicleInScheduledService
-    )
-
-    expect(properties.find((prop) => prop.label === "Operator")!.value).toEqual(
-      "SMITH #1234"
-    )
-  })
-
-  test("operator information gives last name if that's all that's available", () => {
     const properties = vehicleProperties(
       vehicleFactory.build({
-        operatorFirstName: "JOHN",
+        operatorFirstName: null,
         operatorLastName: "SMITH",
         operatorId: "1234",
       }),
