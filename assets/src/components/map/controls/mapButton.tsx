44c796c32e52ab2d7852be121b86189c69d91d8c
Eddie Maldonado <emaldonado@mbta.com>
2024-03-04 15:12:48 -0500

    feat: add at large size,refactor sizes to match react-bootstrap

--- assets/src/components/map/controls/mapButton.tsx
+++ assets/src/components/map/controls/mapButton.tsx
@@ -1,30 +1,28 @@
-import React, { ComponentPropsWithoutRef } from "react"
+import React, { ComponentPropsWithRef } from "react"
 import { Button } from "react-bootstrap"
 import { joinClasses } from "../../../helpers/dom"
 
-interface MapButtonProps extends ComponentPropsWithoutRef<typeof Button> {
-  size: "s" | "m" | "l"
-}
-
-export const MapButton = (props: MapButtonProps) => {
-  const buttonProps = { ...props, size: undefined }
-
+export const MapButton = (props: ComponentPropsWithRef<typeof Button>) => {
   return (
     <Button
-      {...buttonProps}
+      {...props}
       variant="outline-primary"
       className={joinClasses([
         "border-box",
         "inherit-box",
         "border-0",
         "d-flex",
+        "flex-column",
         "justify-content-center",
         "align-items-center",
         "c-map-button",
-        "c-map-button--" + props.size,
+        props.size && "c-map-button--" + props.size,
       ])}
     >
-      {props.size}
+      {props.children}
+      {props.size === "lg" && props.title && (
+        <span className="mt-1">{props.title}</span>
+      )}
     </Button>
   )
 }
--- assets/stories/skate-components/map/controls/mapButton.stories.tsx
+++ assets/stories/skate-components/map/controls/mapButton.stories.tsx
@@ -5,30 +5,43 @@ import React from "react"
 import { MapButton } from "../../../../src/components/map/controls/mapButton"
 import { inMapDecorator } from "../../../../.storybook/inMapDecorator"
 import { CustomControl } from "../../../../src/components/map/controls/customControl"
+import { PlusSquare } from "../../../../src/helpers/bsIcons"
 
 const meta = {
   component: MapButton,
   args: {
-    size: "m",
+    size: undefined,
+    title: "Button",
     active: false,
     disabled: false,
   },
   argTypes: {
-    size: { options: ["s", "m", "l"] },
+    size: {
+      control: {
+        type: "select",
+        labels: { sm: "Small", undefined: "Medium", lg: "Large" },
+      },
+      options: ["sm", undefined, "lg"],
+    },
     active: { control: "boolean" },
     disabled: { control: "boolean" },
   },
+  render: (args) => (
+    <MapButton {...args}>
+      <PlusSquare />
+    </MapButton>
+  ),
 } satisfies Meta<typeof MapButton>
 
 export default meta
 
 type Story = StoryObj<typeof meta>
 
-export const Small: Story = { args: { size: "s" } }
+export const Small: Story = { args: { size: "sm" } }
 
-export const Medium: Story = { args: { size: "m" } }
+export const Medium: Story = { args: { size: undefined } }
 
-export const Large: Story = { args: { size: "l" } }
+export const Large: Story = { args: { size: "lg" } }
 
 export const Active: Story = { args: { active: true } }
 
@@ -42,7 +55,9 @@ export const InMap: Story = {
   render(args) {
     return (
       <CustomControl position="bottomleft">
-        <MapButton {...args} />
+        <MapButton {...args}>
+          <PlusSquare />
+        </MapButton>
       </CustomControl>
     )
   },
