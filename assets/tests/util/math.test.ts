3839d8e7d4f0c321ce1481adcaaba77f77088491
Josh Larson <jlarson@mbta.com>
2024-03-12 14:50:30 -0400

    chore: Remove closestPosition

--- assets/tests/util/math.test.ts
+++ assets/tests/util/math.test.ts
@@ -1,6 +1,5 @@
 import { describe, test, expect } from "@jest/globals"
-import { clamp, closestPosition } from "../../src/util/math"
-import { LatLngLiteral, latLng } from "leaflet"
+import { clamp } from "../../src/util/math"
 
 describe("clamp", () => {
   test("when value is below minimum, returns minimum value", () => {
@@ -13,34 +12,5 @@ describe("clamp", () => {
 
   test("when value is between minimum and maximum, returns value", () => {
     expect(clamp(5, 0, 10)).toBe(5)
-  })
-})
-
-describe("closestPosition", () => {
-  test("returns `undefined` if provided a empty list", () => {
-    expect(closestPosition([], latLng(0, 0))).toBeUndefined()
-  })
-
-  test("returns the closest point from the provided list", () => {
-    const point = latLng(0, 0)
-
-    const closestPoint: LatLngLiteral = {
-      lat: point.lat + 1,
-      lng: 0,
-    }
-    const index = 3
-
-    // Generate range and offset so all are farther than `closestPoint`
-    const positions: LatLngLiteral[] = Array(5).map((_, idx) => ({
-      lat: point.lat + closestPoint.lat + idx,
-      lng: 0,
-    }))
-
-    positions[index] = closestPoint
-
-    expect(closestPosition(positions, point)).toMatchObject({
-      index,
-      position: closestPoint,
-    })
   })
 })
