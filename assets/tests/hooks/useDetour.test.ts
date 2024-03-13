4f39ea3f3158b4c5012f139848883c69bb8d703b
Eddie Maldonado <emaldonado@mbta.com>
2024-03-13 09:33:28 -0400

    feat: useDetour indicates routing errors

--- assets/tests/hooks/useDetour.test.ts
+++ assets/tests/hooks/useDetour.test.ts
@@ -11,7 +11,7 @@ import { finishedDetourFactory } from "../factories/finishedDetourFactory"
 import { routeSegmentsFactory } from "../factories/finishedDetourFactory"
 import { originalRouteFactory } from "../factories/originalRouteFactory"
 import shapeFactory from "../factories/shape"
-import { ok, loading } from "../../src/util/fetchResult"
+import { ok, loading, fetchError } from "../../src/util/fetchResult"
 
 jest.mock("../../src/api")
 
@@ -123,6 +123,24 @@ describe("useDetour", () => {
     await waitFor(() => {
       expect(result.current.detourShape).toStrictEqual(detourShape.coordinates)
       expect(result.current.directions).toStrictEqual(detourShape.directions)
+    })
+  })
+
+  test("indicates when there is an error with the routing service", async () => {
+    const start: ShapePoint = { lat: -2, lon: -2 }
+    const end: ShapePoint = { lat: -1, lon: -1 }
+
+    jest.mocked(fetchDetourDirections).mockResolvedValue(fetchError())
+
+    const { result } = renderHook(useDetourWithFakeRoutePattern)
+
+    act(() => result.current.addConnectionPoint?.(start))
+    act(() => result.current.addWaypoint?.(end))
+
+    await waitFor(() => {
+      expect(result.current.detourShape).toStrictEqual([])
+      expect(result.current.directions).toStrictEqual(undefined)
+      expect(result.current.routingError).toBeTruthy()
     })
   })
 
