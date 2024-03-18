cf7612ce3d5e1f98e1aebecd1f30836b862bc771
Kayla Firestack <firestack@users.noreply.github.com>
2024-03-18 11:01:30 -0400

    feat(ex/skate/open_route_service_api): send 40ft bus info to ORS for HGV profile routing (#2507)
    * feat(ex/skate/open_route_service_api): send 40ft bus info to ORS for HGV profile routing
    
    * cleanup(ex/skate/open_route_service_api): make API options typed
    
    * cleanup(ex/skate/open_route_service_api): move 40ft bus profile into function

--- lib/skate/open_route_service_api/directions_request.ex
+++ lib/skate/open_route_service_api/directions_request.ex
@@ -4,13 +4,46 @@ defmodule Skate.OpenRouteServiceAPI.DirectionsRequest do
   """
   @derive Jason.Encoder
 
+  defmodule Options do
+    @moduledoc false
+    defmodule ProfileParams do
+      @moduledoc false
+      defmodule HgvRestrictions do
+        @moduledoc false
+        @type t :: %{length: float(), width: float(), height: float()}
+        defstruct [:length, :width, :height]
+
+        def bus_40ft,
+          do: %{
+            length: 12.192,
+            width: 3.2004,
+            height: 3.5052
+          }
+      end
+
+      @type t :: %{restrictions: HgvRestrictions.t()}
+      defstruct [:restrictions]
+    end
+
+    @type t :: %{profile_params: ProfileParams.t()}
+    defstruct [:profile_params]
+  end
+
   @typedoc """
     Type that represents a request made to OpenRouteService's Directions API
   """
   @type t() :: %__MODULE__{
           coordinates: [[float()]],
-          continue_straight: boolean()
+          continue_straight: boolean(),
+          options: Options.t()
         }
 
-  defstruct coordinates: [], continue_straight: true
+  defstruct coordinates: [],
+            continue_straight: true,
+            options: %{
+              profile_params: %{
+                restrictions:
+                  Skate.OpenRouteServiceAPI.DirectionsRequest.Options.ProfileParams.HgvRestrictions.bus_40ft()
+              }
+            }
 end
