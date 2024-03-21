e1550c3e09f767fceec79817f378beb3ff229055
Hannah Purcell <69368883+hannahpurcell@users.noreply.github.com>
2024-03-21 14:49:09 -0400

    Use strong for accessibility, with added bootstrap class for font-weight
    Co-authored-by: Kayla Firestack <firestack@users.noreply.github.com>

--- assets/src/components/detours/diversionPanel.tsx
+++ assets/src/components/detours/diversionPanel.tsx
@@ -69,9 +69,10 @@ export const DiversionPanel = ({
               {detourFinished && (
                 <ListGroup.Item
                   as="li"
-                  className="c-diversion-panel__conclude-detour-text"
                 >
-                  Return to Regular Route
+                  <strong className="fw-medium">
+                    Return to Regular Route
+                  </strong>
                 </ListGroup.Item>
               )}
             </ListGroup>
