078c6a7ea2110dbbe49f10e2153e3b075ec734c7
Josh Larson <jlarson@mbta.com>
2024-04-08 13:35:57 -0400

    fixup! chore: Remove Skate.Endpoint.init/2, deprecated in Phoenix 1.17.11

--- config/test.exs
+++ config/test.exs
@@ -1,8 +1,7 @@
 import Config
 
 config :skate,
-  start_data_processes: false,
-  secret_key_base: "local_secret_key_base_at_least_64_bytes_________________________________"
+  start_data_processes: false
 
 config :skate, Schedule.CacheFile, cache_filename: "test_cache.terms"
 
