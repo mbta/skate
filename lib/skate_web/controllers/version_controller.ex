fc991340611293ea245590ded2160e5a8d52c99c
Josh Larson <jlarson@mbta.com>
2024-04-22 17:44:13 -0400

    fixup! feat: Add endpoint showing the currently-deployed version

--- lib/skate_web/controllers/version_controller.ex
+++ lib/skate_web/controllers/version_controller.ex
@@ -1,9 +1,11 @@
 defmodule SkateWeb.VersionController do
   use SkateWeb, :controller
 
+  @version Application.compile_env(:skate, :version)
+
   def version(conn, _params) do
     conn
     |> put_resp_content_type("text/plain")
-    |> send_resp(:ok, Application.get_env(:skate, :version))
+    |> send_resp(:ok, @version)
   end
 end
