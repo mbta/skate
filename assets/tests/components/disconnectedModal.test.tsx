b8181971b03c71ce20f67443b8a2119da8404afc
Hannah Purcell <hpurcell@mbta.com>
2024-05-02 11:30:18 -0400

    Migrate from using the react-test-renderer

--- assets/tests/components/disconnectedModal.test.tsx
+++ assets/tests/components/disconnectedModal.test.tsx
@@ -1,28 +1,19 @@
 import { jest, describe, test, expect } from "@jest/globals"
 import { render } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
-import React, { ReactNode, ReactPortal } from "react"
-import renderer from "react-test-renderer"
+import React from "react"
 import DisconnectedModal from "../../src/components/disconnectedModal"
 import { reload } from "../../src/models/browser"
-import ReactDOM from "react-dom"
 
 jest.mock("../../src/models/browser", () => ({
   __esModule: true,
   reload: jest.fn(),
 }))
 
-jest.mock("react-dom", () => {
-  return {
-    ...(jest.requireActual("react-dom") as typeof ReactDOM),
-    createPortal: (node: ReactNode): ReactPortal => node as ReactPortal,
-  }
-})
-
 describe("DisconnectedModal", () => {
   test("renders", () => {
-    const tree = renderer.create(<DisconnectedModal />).toJSON()
-    expect(tree).toMatchSnapshot()
+    const { baseElement } = render(<DisconnectedModal />)
+    expect(baseElement).toMatchSnapshot()
   })
 
   test("refreshes when you click the button", async () => {
