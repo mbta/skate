ed3ea5bf63c8f7559d0557e252851814b830744a
Hannah Purcell <69368883+hannahpurcell@users.noreply.github.com>
2024-05-07 10:56:31 -0400

    feat: Part 3 of converting to Restart modals: Preset modals (#2578)

--- assets/tests/components/inputModal.test.tsx
+++ assets/tests/components/inputModal.test.tsx
@@ -1,24 +1,25 @@
 import { jest, describe, test, expect } from "@jest/globals"
 import React from "react"
-import { render, fireEvent } from "@testing-library/react"
+import { render, waitFor } from "@testing-library/react"
 import InputModal from "../../src/components/inputModal"
 import { StateDispatchProvider } from "../../src/contexts/stateDispatchContext"
 import { initialState, closeInputModal } from "../../src/state"
+import userEvent from "@testing-library/user-event"
 
 describe("InputModal", () => {
-  test("can be dismissed with escape key", () => {
+  test("can be dismissed with escape key", async () => {
     const mockDispatch = jest.fn()
 
-    const result = render(
+    render(
       <StateDispatchProvider state={initialState} dispatch={mockDispatch}>
         <InputModal>
           <>Hello, world!</>
         </InputModal>
       </StateDispatchProvider>
     )
-
-    fireEvent.keyDown(result.getByText("Hello, world!"), { key: "Escape" })
-
-    expect(mockDispatch).toHaveBeenCalledWith(closeInputModal())
+    await userEvent.keyboard("{Escape}")
+    await waitFor(() =>
+      expect(mockDispatch).toHaveBeenCalledWith(closeInputModal())
+    )
   })
 })
