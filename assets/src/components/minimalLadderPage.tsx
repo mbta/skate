2489cadf89b73b977744453ea8d3e9f3fb51c430
Hannah Purcell <hpurcell@mbta.com>
2024-05-16 13:13:57 -0400

    Don't nest Button within NavLink

--- assets/src/components/minimalLadderPage.tsx
+++ assets/src/components/minimalLadderPage.tsx
@@ -1,7 +1,6 @@
 import React, { useContext } from "react"
 import { isPreset } from "../models/routeTab"
 import { StateDispatchContext } from "../contexts/stateDispatchContext"
-import { Button } from "react-bootstrap"
 import { NavLink } from "react-router-dom"
 
 export const MinimalLadderPage = () => {
@@ -14,10 +13,12 @@ export const MinimalLadderPage = () => {
     <div className="c-minimal-ladder-page">
       <h3>Select a preset to display</h3>
       {presets.map((preset) => (
-        <NavLink key={preset.uuid} to={"/minimal/" + preset.uuid}>
-          <Button className="c-minimal-ladder-page__button">
-            {preset.presetName}
-          </Button>
+        <NavLink
+          key={preset.uuid}
+          to={"/minimal/" + preset.uuid}
+          className="btn btn-primary c-minimal-ladder-page__button"
+        >
+          {preset.presetName}
         </NavLink>
       ))}
     </div>
