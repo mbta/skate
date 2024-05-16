e0043b3ef0c04725a02ad7b86423aef5e665e7b1
Hannah Purcell <hpurcell@mbta.com>
2024-05-16 11:22:01 -0400

    RoutePill story

---
+++ assets/stories/skate-components/routePill.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import { RoutePill } from "../../src/components/routePill"
+
+const meta = {
+  component: RoutePill,
+} satisfies Meta<typeof RoutePill>
+
+export default meta
+type Story = StoryObj<typeof RoutePill>
+
+export const Default: Story = {
+  args: { routeName: "66" },
+}
+
+export const LargeFormat: Story = {
+  args: { routeName: "66", className: "c-route-pill--large-format" },
+}
