d0353133965a48588fec2b6dad3f96000a72223b
Kayla Firestack <kfirestack@mbta.com>
2024-05-19 14:45:33 -0400

    feat(ts/detours): add context and input to detour machine

--- assets/src/models/createDetourMachine.ts
+++ assets/src/models/createDetourMachine.ts
@@ -1,11 +1,25 @@
 import { setup } from "xstate"
+import { Route, RoutePattern } from "../schedule"
 
 export const createDetourMachine = setup({
   types: {} as {
+    context: {
+      route: Route
+      routePattern: RoutePattern
+    }
+
+    input: {
+      // Caller has target route pattern
+      route: Route
+      routePattern: RoutePattern
+    }
     events: { type: "detour.edit.done" } | { type: "detour.edit.resume" }
   },
 }).createMachine({
   id: "Detours Machine",
+  context: ({ input }) => ({
+    ...input,
+  }),
 
   initial: "Detour Drawing",
   states: {
