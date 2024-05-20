eb13d97e991ca8c43de57ef3e654bcc8c26b0724
Josh Larson <jlarson@mbta.com>
2024-05-20 10:43:31 -0400

    fixup! refactor: Extract the render component part of RoutePropertiesCard as its own component

--- assets/stories/skate-components/molecules/routePropertiesCard.stories.tsx
+++ assets/stories/skate-components/molecules/routePropertiesCard.stories.tsx
@@ -5,18 +5,38 @@ import routeFactory from "../../../tests/factories/route"
 
 const routePattern0 = routePatternFactory.build({
   directionId: 0,
+  headsign: "Dedham Mall",
+  id: "52-5-0",
+  name: "Watertown - Dedham Mall via Meadowbrook Rd",
+  routeId: "52",
 })
 const routePattern1 = routePatternFactory.build({
   directionId: 0,
+  headsign: "Charles River Loop",
+  id: "52-4-0",
+  name: "Watertown - Charles River Loop via Meadowbrook Rd",
+  routeId: "52",
 })
 const routePattern2 = routePatternFactory.build({
   directionId: 1,
+  headsign: "Watertown Yard",
+  id: "52-5-1",
+  name: "Dedham Mall - Watertown via Meadowbrook Rd",
+  routeId: "52",
 })
 const routePattern3 = routePatternFactory.build({
   directionId: 1,
+  headsign: "Watertown Yard",
+  id: "52-4-1",
+  name: "Charles River Loop - Watertown via Meadowbrook Rd",
+  routeId: "52",
 })
 const routePattern4 = routePatternFactory.build({
   directionId: 1,
+  headsign: "Watertown Yard via Dedham St",
+  id: "52-6-1",
+  name: "Charles River Loop - Watertown via Dedham St",
+  routeId: "52",
 })
 const routePatterns = {
   [routePattern0.id]: routePattern0,
@@ -26,7 +46,7 @@ const routePatterns = {
   [routePattern4.id]: routePattern4,
 }
 
-const route = routeFactory.build()
+const route = routeFactory.build({ id: "52" })
 
 const meta = {
   component: RoutePropertiesCardRender,
@@ -41,3 +61,8 @@ export default meta
 type Story = StoryObj<typeof RoutePropertiesCardRender>
 
 export const Default: Story = {}
+export const WithInboundSelected: Story = {
+  args: {
+    selectedRoutePattern: routePattern3,
+  },
+}
