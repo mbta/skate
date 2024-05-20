9e2fb3e2018dab21c37271882c001f67b6b6a82d
Hannah Purcell <hpurcell@mbta.com>
2024-05-20 11:26:50 -0400

    New route ladder header

---
+++ assets/stories/skate-components/newRouteLadderHeader.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import { NewHeader as RouteLadderHeader } from "../../src/components/routeLadder"
+
+const meta = {
+  component: RouteLadderHeader,
+  args: {
+    routeName: "39",
+    onClose: () => {},
+    hasAlert: false,
+  },
+  argTypes: {
+    onClose: { table: { disable: true } },
+  },
+} satisfies Meta<typeof RouteLadderHeader>
+
+export default meta
+type Story = StoryObj<typeof RouteLadderHeader>
+
+export const Default: Story = {}
+export const WithAlert: Story = {
+  args: {
+    hasAlert: true,
+  },
+}
