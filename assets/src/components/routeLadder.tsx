a276674ce9b763e0881c58186bd06f351652f1ab
Hannah Purcell <hpurcell@mbta.com>
2024-05-21 14:51:55 -0400

    Removed the forwardRef addition to bsIcons

--- assets/src/components/routeLadder.tsx
+++ assets/src/components/routeLadder.tsx
@@ -29,7 +29,7 @@ import { CloseButton as OldCloseButton } from "./closeButton"
 import Tippy from "@tippyjs/react"
 import { tagManagerEvent } from "../helpers/googleTagManager"
 import inTestGroup, { TestGroups } from "../userInTestGroup"
-import { ExclamationTriangleFill, ExclamationTriangleFillJustSvg, ExclamationTriangleFillWithRef } from "../helpers/bsIcons"
+import { ExclamationTriangleFill } from "../helpers/bsIcons"
 import { RoutePill } from "./routePill"
 import { CloseButton } from "react-bootstrap"
 
@@ -78,15 +78,6 @@ export const Header = ({
   )
 }
 
-const AlertThing = forwardRef((props: any, ref: any) => (
-  <ExclamationTriangleFill
-      className="c-route-ladder__alert-icon"
-      aria-label="Route Alert"
-      ref={ref}
-      {...props}
-    />
-))
-
 // TODO: delete old header after roll-out
 export const NewHeader = ({
   routeName,
@@ -106,7 +97,9 @@ export const NewHeader = ({
           trigger="click"
           onShow={() => tagManagerEvent("alert_tooltip_clicked")}
         >
-          <AlertThing />
+          <div className="c-route-ladder__alert-icon">
+            <ExclamationTriangleFill aria-label="Route Alert" />
+          </div>
         </Tippy>
       )}
       <RoutePill
