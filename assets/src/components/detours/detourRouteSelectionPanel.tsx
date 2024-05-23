65f9de9213a33c8240a2925f575af39996d6b8cb
Josh Larson <jlarson@mbta.com>
2024-05-23 12:12:47 -0400

    feat: Have RoutePropertiesCard on DetourRouteSelectionPanel open to "variants"

--- assets/src/components/detours/detourRouteSelectionPanel.tsx
+++ assets/src/components/detours/detourRouteSelectionPanel.tsx
@@ -69,6 +69,7 @@ export const DetourRouteSelectionPanel = ({
                 selectedRouteInfo
               )}
               selectRoutePattern={() => {}}
+              defaultOpened="variants"
             />
           ) : (
             <p className="fst-italic">
