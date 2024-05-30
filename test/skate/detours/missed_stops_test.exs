ce938223405c5700f66dfcf3b791f4776184cf86
Josh Larson <jlarson@mbta.com>
2024-05-30 14:34:34 -0400

    fix: Missed stops bug when stop is close to start point

--- test/skate/detours/missed_stops_test.exs
+++ test/skate/detours/missed_stops_test.exs
@@ -5,7 +5,7 @@ defmodule Skate.Detours.MissedStopsTest do
   alias Util.Location
 
   describe "missed_stops" do
-    test "given a straight line, should return missed stops" do
+    test "given a straight line, returns missed stops" do
       ##                                                 (Missed Stop)
       ##        Stops: v(0.001, 0)                        v(0.001, 5)   v(0.001, 5)
       ##               o                                  o             o
@@ -14,7 +14,7 @@ defmodule Skate.Detours.MissedStopsTest do
       ## Shape Points: ^(0,0) ^(0,1) ^(0,2) ^(0,3) ^(0,4) ^(0,5) ^(0,6) ^(0,7)
       ##
       ## Connection Points :  o                                  o
-      ##                      ^(-0.001, 1)                       ^(-0.00, 6)
+      ##                      ^(-0.001, 1)                       ^(-0.001, 6)
       ##                   (connection_start)                  (connection_end)
       #
       # https://excalidraw.com/#json=OrMM928mw4CR3Qy8sc6oL,sXiFCU1s-K1ugEQvgSvh3g
@@ -212,6 +212,92 @@ defmodule Skate.Detours.MissedStopsTest do
 
       assert Enum.slice(stops, connection_start_index..(connection_end_index - 1)) ==
                missed_stops
+    end
+
+    test "counts stops as missed if they're just inside the detour" do
+      ##                              Both stops are missed
+      ##        Stops:         v(0.001, 1.1)             v(0.001, 4.9)
+      ##                       o                         o
+      ##
+      ##               o------o------o------o------o------o------o------o
+      ## Shape Points: ^(0,0) ^(0,1) ^(0,2) ^(0,3) ^(0,4) ^(0,5) ^(0,6) ^(0,7)
+      ##
+      ## Connection Points : o                             o
+      ##                     ^(-0.001, 0.9)                ^(-0.001, 5.1)
+      ##                  (connection_start)               (connection_end)
+      #
+      # https://excalidraw.com/#json=OrMM928mw4CR3Qy8sc6oL,sXiFCU1s-K1ugEQvgSvh3g
+      stops = [
+        Location.new(0.001, 1.1),
+        Location.new(0.001, 4.9)
+      ]
+
+      param = %Skate.Detours.MissedStops{
+        connection_start: Location.new(-0.001, 0.9),
+        connection_end: Location.new(-0.001, 5.1),
+        stops: stops,
+        shape: [
+          Location.new(0, 0),
+          Location.new(0, 1),
+          Location.new(0, 2),
+          Location.new(0, 3),
+          Location.new(0, 4),
+          Location.new(0, 5),
+          Location.new(0, 6),
+          Location.new(0, 7)
+        ]
+      }
+
+      assert %MissedStops.Result{
+               missed_stops: ^stops,
+               connection_stop_start: nil,
+               connection_stop_end: nil
+             } =
+               Skate.Detours.MissedStops.missed_stops(param)
+    end
+
+    test "does not count stops as missed if they're just outside the detour" do
+      ##                             Neither stop is missed
+      ##        Stops:       v(0.001, 0.9)                 v(0.001, 5.1)
+      ##                     o                             o
+      ##
+      ##               o------o------o------o------o------o------o------o
+      ## Shape Points: ^(0,0) ^(0,1) ^(0,2) ^(0,3) ^(0,4) ^(0,5) ^(0,6) ^(0,7)
+      ##
+      ## Connection Points :   o                         o
+      ##                       ^(-0.001, 1.1)            ^(-0.001, 4.9)
+      ##                    (connection_start)           (connection_end)
+      #
+      # https://excalidraw.com/#json=OrMM928mw4CR3Qy8sc6oL,sXiFCU1s-K1ugEQvgSvh3g
+
+      connection_stop_start = Location.new(0.001, 0.9)
+      connection_stop_end = Location.new(0.001, 5.1)
+
+      param = %Skate.Detours.MissedStops{
+        connection_start: Location.new(-0.001, 1.1),
+        connection_end: Location.new(-0.001, 4.9),
+        stops: [
+          connection_stop_start,
+          connection_stop_end
+        ],
+        shape: [
+          Location.new(0, 0),
+          Location.new(0, 1),
+          Location.new(0, 2),
+          Location.new(0, 3),
+          Location.new(0, 4),
+          Location.new(0, 5),
+          Location.new(0, 6),
+          Location.new(0, 7)
+        ]
+      }
+
+      assert %MissedStops.Result{
+               missed_stops: [],
+               connection_stop_start: ^connection_stop_start,
+               connection_stop_end: ^connection_stop_end
+             } =
+               Skate.Detours.MissedStops.missed_stops(param)
     end
   end
 end
