6034e0891b42218426b1844421c314e35af1c329
Hannah Purcell <hpurcell@mbta.com>
2024-05-29 20:27:42 -0400

    formatting

--- assets/src/components/detours/detourRouteSelectionPanel.tsx
+++ assets/src/components/detours/detourRouteSelectionPanel.tsx
@@ -41,63 +41,65 @@ export const DetourRouteSelectionPanel = ({
   allRoutes,
   selectedRouteInfo,
 }: DetourRouteSelectionPanelProps) => {
-  const [validated, setValidated] = useState(false);
+  const [validated, setValidated] = useState(false)
 
   const handleSubmit = () => {
-    setValidated(true);
-  };
+    setValidated(true)
+  }
 
   return (
-  <Panel as="article">
-    <Panel.Header className="">
-      <h1 className="c-diversion-panel__h1 my-3">Create Detour</h1>
-    </Panel.Header>
+    <Panel as="article">
+      <Panel.Header className="">
+        <h1 className="c-diversion-panel__h1 my-3">Create Detour</h1>
+      </Panel.Header>
 
-    <Panel.Body className="d-flex flex-column">
-      <Panel.Body.ScrollArea className="d-flex flex-column">
-        <section className="pb-3">
-          <h2 className="c-diversion-panel__h2">Choose route</h2>
-          <Form validated={validated}>
-            <FormSelect required defaultValue={selectedRouteInfo.selectedRoute?.id}>
-              <option value="">Select a route</option>
-              {allRoutes.map((route) => (
-                <option key={route.id} value={route.id}>
-                  {route.name}
-                </option>
-              ))}
-            </FormSelect>
-            <Form.Control.Feedback type="invalid">
-              Select a route to continue.
-            </Form.Control.Feedback>
-          </Form>
-          
-        </section>
+      <Panel.Body className="d-flex flex-column">
+        <Panel.Body.ScrollArea className="d-flex flex-column">
+          <section className="pb-3">
+            <h2 className="c-diversion-panel__h2">Choose route</h2>
+            <Form validated={validated}>
+              <FormSelect
+                required
+                defaultValue={selectedRouteInfo.selectedRoute?.id}
+              >
+                <option value="">Select a route</option>
+                {allRoutes.map((route) => (
+                  <option key={route.id} value={route.id}>
+                    {route.name}
+                  </option>
+                ))}
+              </FormSelect>
+              <Form.Control.Feedback type="invalid">
+                Select a route to continue.
+              </Form.Control.Feedback>
+            </Form>
+          </section>
 
-        <section className="pb-3">
-          <h2 className="c-diversion-panel__h2">Choose direction</h2>
-          {selectedRouteInfo.selectedRoute ? (
-            <RoutePropertiesCard
-              routePatterns={selectedRouteInfo.routePatterns}
-              selectedRoutePatternId={selectedRoutePatternFromInfo(
-                selectedRouteInfo
-              )}
-              selectRoutePattern={() => {}}
-              defaultOpened="variants"
-            />
-          ) : (
-            <p className="fst-italic">
-              Select a route in order to choose a direction.
-            </p>
-          )}
-        </section>
-      </Panel.Body.ScrollArea>
+          <section className="pb-3">
+            <h2 className="c-diversion-panel__h2">Choose direction</h2>
+            {selectedRouteInfo.selectedRoute ? (
+              <RoutePropertiesCard
+                routePatterns={selectedRouteInfo.routePatterns}
+                selectedRoutePatternId={selectedRoutePatternFromInfo(
+                  selectedRouteInfo
+                )}
+                selectRoutePattern={() => {}}
+                defaultOpened="variants"
+              />
+            ) : (
+              <p className="fst-italic">
+                Select a route in order to choose a direction.
+              </p>
+            )}
+          </section>
+        </Panel.Body.ScrollArea>
 
-      <Panel.Body.Footer className="d-flex">
-        <Button className="m-3 flex-grow-1" onClick={handleSubmit}>
-          Start drawing detour
-        </Button>
-      </Panel.Body.Footer>
-    </Panel.Body>
-  </Panel>
+        <Panel.Body.Footer className="d-flex">
+          <Button className="m-3 flex-grow-1" onClick={handleSubmit}>
+            Start drawing detour
+          </Button>
+        </Panel.Body.Footer>
+      </Panel.Body>
+    </Panel>
   )
 }
