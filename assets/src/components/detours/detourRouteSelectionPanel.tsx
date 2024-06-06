9ac2b585db5a68bdfbe2b412e7a5626ab47b920a
Kayla Firestack <kfirestack@mbta.com>
2024-06-06 09:31:02 -0400

    intermediate: convert arrow function to brackets

--- assets/src/components/detours/detourRouteSelectionPanel.tsx
+++ assets/src/components/detours/detourRouteSelectionPanel.tsx
@@ -47,60 +47,64 @@ export const DetourRouteSelectionPanel = ({
   onConfirm,
   onSelectRoute,
   onSelectRoutePattern,
-}: DetourRouteSelectionPanelProps) => (
-  <Panel as="article">
-    <Panel.Header className="">
-      <h1 className="c-diversion-panel__h1 my-3">Create Detour</h1>
-    </Panel.Header>
+}: DetourRouteSelectionPanelProps) => {
+  return (
+    <Panel as="article">
+      <Panel.Header className="">
+        <h1 className="c-diversion-panel__h1 my-3">Create Detour</h1>
+      </Panel.Header>
 
-    <Panel.Body className="d-flex flex-column">
-      <Panel.Body.ScrollArea className="d-flex flex-column">
-        <section className="pb-3">
-          <h2 className="c-diversion-panel__h2" id="choose-route-select">
-            Choose route
-          </h2>
-          <FormSelect
-            aria-labelledby="choose-route-select"
-            value={selectedRouteInfo.selectedRoute?.id}
-            onChange={(changeEvent) => {
-              onSelectRoute?.(
-                allRoutes.find((route) => route.id === changeEvent.target.value)
-              )
-            }}
-          >
-            <option>Select a route</option>
-            {allRoutes.map((route) => (
-              <option key={route.id} value={route.id}>
-                {route.name}
-              </option>
-            ))}
-          </FormSelect>
-        </section>
+      <Panel.Body className="d-flex flex-column">
+        <Panel.Body.ScrollArea className="d-flex flex-column">
+          <section className="pb-3">
+            <h2 className="c-diversion-panel__h2" id="choose-route-select">
+              Choose route
+            </h2>
+            <FormSelect
+              aria-labelledby="choose-route-select"
+              value={selectedRouteInfo.selectedRoute?.id}
+              onChange={(changeEvent) => {
+                onSelectRoute?.(
+                  allRoutes.find(
+                    (route) => route.id === changeEvent.target.value
+                  )
+                )
+              }}
+            >
+              <option>Select a route</option>
+              {allRoutes.map((route) => (
+                <option key={route.id} value={route.id}>
+                  {route.name}
+                </option>
+              ))}
+            </FormSelect>
+          </section>
 
-        <section className="pb-3">
-          <h2 className="c-diversion-panel__h2">Choose direction</h2>
-          {selectedRouteInfo.selectedRoute ? (
-            <RoutePropertiesCard
-              routePatterns={selectedRouteInfo.routePatterns}
-              selectedRoutePatternId={selectedRoutePatternFromInfo(
-                selectedRouteInfo
-              )}
-              selectRoutePattern={onSelectRoutePattern}
-              defaultOpened="variants"
-            />
-          ) : (
-            <p className="fst-italic">
-              Select a route in order to choose a direction.
-            </p>
-          )}
-        </section>
-      </Panel.Body.ScrollArea>
+          <section className="pb-3">
+            <h2 className="c-diversion-panel__h2">Choose direction</h2>
+            {selectedRouteInfo.selectedRoute ? (
+              <RoutePropertiesCard
+                routePatterns={selectedRouteInfo.routePatterns}
+                selectedRoutePatternId={selectedRoutePatternFromInfo(
+                  selectedRouteInfo
+                )}
+                selectRoutePattern={onSelectRoutePattern}
+                defaultOpened="variants"
+              />
+            ) : (
+              <p className="fst-italic">
+                Select a route in order to choose a direction.
+              </p>
+            )}
+          </section>
+        </Panel.Body.ScrollArea>
 
-      <Panel.Body.Footer className="d-flex">
-        <Button className="m-3 flex-grow-1" onClick={onConfirm}>
-          Start drawing detour
-        </Button>
-      </Panel.Body.Footer>
-    </Panel.Body>
-  </Panel>
-)
+        <Panel.Body.Footer className="d-flex">
+          <Button className="m-3 flex-grow-1" onClick={onConfirm}>
+            Start drawing detour
+          </Button>
+        </Panel.Body.Footer>
+      </Panel.Body>
+    </Panel>
+  )
+}
