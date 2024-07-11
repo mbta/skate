58d308a44bad9c3494f34605d4856abf9141d4c1
Josh Larson <jlarson@mbta.com>
2024-07-11 16:09:11 -0400

    fixup! feat: Add `TripModificationPublisher.publish_shape/2`

--- test/skate/detours/trip_modification_publisher_test.exs
+++ test/skate/detours/trip_modification_publisher_test.exs
@@ -25,7 +25,7 @@ defmodule Skate.Detours.TripModificationPublisherTest do
     {:ok, pid} =
       TripModificationPublisher.start_link(start: true, name: __MODULE__, on_connect: self())
 
-    assert_receive {:connected, ^pid}
+    assert_receive {:connected, ^pid}, 1000
   end
 
   @tag "Test.Integration": :mqtt
@@ -35,7 +35,7 @@ defmodule Skate.Detours.TripModificationPublisherTest do
     {:ok, pid} =
       TripModificationPublisher.start_link(start: true, name: __MODULE__, on_connect: self())
 
-    assert_receive {:connected, ^pid}
+    assert_receive {:connected, ^pid}, 1000
     assert_receive {:connected, ^reader_pid}
 
     trip_modification_message = %TripModification{
