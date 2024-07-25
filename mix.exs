ee4124bccdbf6bc9f629fb85fca63195ffe5aa91
Kayla Firestack <kfirestack@mbta.com>
2024-07-25 16:12:05 -0400

    fix: update credo for elixir `1.17.2`
    Context: https://github.com/rrrene/credo/issues/1139

--- mix.exs
+++ mix.exs
@@ -50,7 +50,7 @@ defmodule Skate.MixProject do
       {:bypass, "~> 2.1.0", only: :test},
       {:castore, "~> 0.1.5"},
       {:configparser_ex, "~> 4.0", only: :dev},
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:csv, "~> 2.4.1"},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:diskusage_logger, "~> 0.2.0"},
