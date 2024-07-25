01ff76ec219d93866ca7790d35f0b9356dcb58a9
Hannah Purcell <hpurcell@mbta.com>
2024-07-25 17:10:04 -0400

    Separated migration into table and index creation

--- priv/repo/migrations/20240723145806_create_detours_table.exs
+++ priv/repo/migrations/20240723145806_create_detours_table.exs
@@ -1,17 +1,11 @@
-defmodule Skate.Repo.Migrations.CreateDetours do
+defmodule Skate.Repo.Migrations.CreateDetoursTable do
   use Ecto.Migration
-
-  @disable_ddl_transaction true
-  @disable_migration_lock true
 
   def change do
     create table(:detours) do
       add :state, :map
       add :author_id, references(:users, on_delete: :nothing)
-
       timestamps()
     end
-
-    create index(:detours, [:author_id], concurrently: true)
   end
 end
