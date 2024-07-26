e87808e3961a2eed7badbca27c77075c226df5e3
Hannah Purcell <hpurcell@mbta.com>
2024-07-26 08:32:10 -0400

    Fixed test references to filename

--- test/skate/detours/db_test.exs
+++ test/skate/detours/db_test.exs
@@ -2,7 +2,7 @@ defmodule Skate.Detours.DbTest do
   use Skate.DataCase
   import Skate.Factory
 
-  alias Skate.Detours.Db
+  alias Skate.Detours.Detours
 
   describe "detours" do
     alias Skate.Detours.Db.Detour
@@ -15,55 +15,55 @@ defmodule Skate.Detours.DbTest do
 
     test "list_detours/0 returns all detours" do
       detour = detour_fixture()
-      assert Skate.Repo.preload(Db.list_detours(), :author) == [detour]
+      assert Skate.Repo.preload(Detours.list_detours(), :author) == [detour]
     end
 
     test "get_detour!/1 returns the detour with given id" do
       detour = detour_fixture()
 
       assert detour.id
-             |> Db.get_detour!()
+             |> Detours.get_detour!()
              |> Skate.Repo.preload(:author) == detour
     end
 
     test "create_detour/1 with valid data creates a detour" do
       valid_attrs = %{state: %{}}
 
-      assert {:ok, %Detour{} = detour} = Db.create_detour(valid_attrs)
+      assert {:ok, %Detour{} = detour} = Detours.create_detour(valid_attrs)
       assert detour.state == %{}
     end
 
     test "create_detour/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Db.create_detour(@invalid_attrs)
+      assert {:error, %Ecto.Changeset{}} = Detours.create_detour(@invalid_attrs)
     end
 
     test "update_detour/2 with valid data updates the detour" do
       detour = detour_fixture()
       update_attrs = %{state: %{}}
 
-      assert {:ok, %Detour{} = detour} = Db.update_detour(detour, update_attrs)
+      assert {:ok, %Detour{} = detour} = Detours.update_detour(detour, update_attrs)
       assert detour.state == %{}
     end
 
     test "update_detour/2 with invalid data returns error changeset" do
       detour = detour_fixture()
-      assert {:error, %Ecto.Changeset{}} = Db.update_detour(detour, @invalid_attrs)
+      assert {:error, %Ecto.Changeset{}} = Detours.update_detour(detour, @invalid_attrs)
 
       assert detour ==
                detour.id
-               |> Db.get_detour!()
+               |> Detours.get_detour!()
                |> Skate.Repo.preload(:author)
     end
 
     test "delete_detour/1 deletes the detour" do
       detour = detour_fixture()
-      assert {:ok, %Detour{}} = Db.delete_detour(detour)
-      assert_raise Ecto.NoResultsError, fn -> Db.get_detour!(detour.id) end
+      assert {:ok, %Detour{}} = Detours.delete_detour(detour)
+      assert_raise Ecto.NoResultsError, fn -> Detours.get_detour!(detour.id) end
     end
 
     test "change_detour/1 returns a detour changeset" do
       detour = detour_fixture()
-      assert %Ecto.Changeset{} = Db.change_detour(detour)
+      assert %Ecto.Changeset{} = Detours.change_detour(detour)
     end
   end
 end
