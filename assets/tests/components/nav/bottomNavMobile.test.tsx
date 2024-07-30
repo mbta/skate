85a0628011181cf1251ce21ea782984944b8aecb
Josh Larson <jlarson@mbta.com>
2024-07-30 13:27:02 -0400

    fixup! feat: Gate dummy detour page behind detours-list test group

--- assets/tests/components/nav/bottomNavMobile.test.tsx
+++ assets/tests/components/nav/bottomNavMobile.test.tsx
@@ -69,6 +69,21 @@ describe("BottomNavMobile", () => {
       </BrowserRouter>
     )
 
-    expect(screen.queryByTitle("Detours")).toBeInTheDocument()
+    expect(screen.queryByTitle("Detours")).toBeVisible()
+  })
+
+  test("does not render nav item with title 'Detours' if not in test group", () => {
+    jest.mocked(getTestGroups).mockReturnValue([])
+
+    render(
+      <BrowserRouter>
+        <BottomNavMobile
+          mobileMenuIsOpen={initialState.mobileMenuIsOpen}
+          openSwingsView={jest.fn()}
+        />
+      </BrowserRouter>
+    )
+
+    expect(screen.queryByTitle("Detours")).not.toBeInTheDocument()
   })
 })
