3d5726aba8e4abe0ff26f4e9f36857ca178b9590
Hannah Purcell <hpurcell@mbta.com>
2024-07-31 09:56:17 -0400

    Basic react-bootstrap table for detour list

--- assets/src/components/dummyDetourPage.tsx
+++ assets/src/components/dummyDetourPage.tsx
@@ -1,78 +1,65 @@
-import React, { useEffect, useMemo, useState } from "react"
-import { fetchRoutePatterns } from "../api"
-import { RoutePattern } from "../schedule"
-import { DiversionPage } from "./detours/diversionPage"
-import { useRoute } from "../contexts/routesContext"
-import { createDetourMachine } from "../models/createDetourMachine"
-import { reload } from "../models/browser"
-import { isOk, Err } from "../util/result"
-import { isValidSnapshot } from "../util/isValidSnapshot"
+import React from "react"
+import { Table } from "react-bootstrap"
+import { RoutePill } from "./routePill"
+import { useCurrentTimeSeconds } from "../hooks/useCurrentTime"
+import { timeAgoLabel } from "../util/dateTime"
 
 export const DummyDetourPage = () => {
-  const [routePattern, setRoutePattern] = useState<RoutePattern | null>(null)
 
-  const routeNumber = "66"
-
-  useEffect(() => {
-    fetchRoutePatterns(routeNumber).then((routePatterns) => {
-      setRoutePattern(routePatterns[0])
-    })
-  }, [])
-  const route = useRoute(routePattern?.routeId)
-
-  // The state machine won't "reinitialize", if it's inputs change, so only
-  // compute the snapshot from `localStorage` once.
-  const snapshot = useMemo(() => {
-    const snapshot_string = localStorage.getItem("snapshot")
-
-    if (!snapshot_string) {
-      return Err<"No Snapshot Present">("No Snapshot Present")
+  const fakeData = [
+    {
+      number: 45,
+      direction: "Outbound",
+      name: "Franklin Park via Ruggles Station",
+      intersection: "John F. Kennedy St & Memorial Drive",
+      activeSince: 1722372950
+    },
+    {
+      number: 83,
+      direction: "Inbound",
+      name: "Central Square",
+      intersection: "Pearl Street & Clearwell Ave",
+      activeSince: 1722361948
+    },
+    {
+      number: 83,
+      direction: "Outbound",
+      name: "Rindge Ave",
+      intersection: "Pearl Street & Clearwell Ave",
+      activeSince: 1721361948
     }
+  ]
 
-    let persisted_snapshot
-    try {
-      persisted_snapshot = JSON.parse(snapshot_string)
-    } catch (error) {
-      return Err<"Parsing JSON Failed">("Parsing JSON Failed")
-    }
-
-    return isValidSnapshot(createDetourMachine, persisted_snapshot)
-  }, [])
+  const epochNowInSeconds = useCurrentTimeSeconds()
 
   return (
     <>
-      {isOk(snapshot) ? (
-        <DiversionPage
-          snapshot={snapshot.ok}
-          showConfirmCloseModal={false}
-          onClose={() => {
-            // If the close button is clicked,
-            // clear snapshot from storage and reinitialize state machine
-            localStorage.setItem("snapshot", "")
-            reload()
-          }}
-        />
-      ) : (
-        <>
-          {
-            /* temp: since this is an internal page, show snapshot error as text
-             */
-            snapshot.err
-          }
-          {route && routePattern && routePattern.shape && (
-            <DiversionPage
-              originalRoute={{
-                center: { lat: 42.36, lng: -71.13 },
-                zoom: 16,
-                route,
-                routePattern,
-              }}
-              showConfirmCloseModal={false}
-              onClose={() => {}}
-            />
-          )}
-        </>
-      )}
+      <Table bordered hover className="c-detour-table">
+        <thead>
+          <tr>
+            <th>Route</th>
+            <th>Starting Intersection</th>
+            <th>On detour since</th>
+          </tr>
+        </thead>
+        <tbody>
+          {fakeData.map(detour => (
+            <tr>
+              <td className="align-middle">
+                <div className="c-detour-table__route-info-cell">
+                  <RoutePill routeName={detour.number.toString()} />
+                  <div className="c-detour-table__route-info-text">
+                    <span>{detour.direction}</span>
+                    <span className="c-detour-table__route-info-text--bold">{detour.name}</span>
+                  </div>
+                </div>
+              </td>
+              <td className="align-middle">{detour.intersection}</td>
+              <td className="align-middle">{timeAgoLabel(epochNowInSeconds, detour.activeSince)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </>
   )
 }
