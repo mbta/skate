00c56004c2aff9cd5f46c7e888e7b1523ba4fae0
Hannah Purcell <hpurcell@mbta.com>
2024-08-02 11:21:10 -0400

    format

--- assets/src/components/dummyDetourPage.tsx
+++ assets/src/components/dummyDetourPage.tsx
@@ -5,29 +5,28 @@ import { useCurrentTimeSeconds } from "../hooks/useCurrentTime"
 import { timeAgoLabel } from "../util/dateTime"
 
 export const DummyDetourPage = () => {
-
   const fakeData = [
     {
       number: 45,
       direction: "Outbound",
       name: "Franklin Park via Ruggles Station",
       intersection: "John F. Kennedy St & Memorial Drive",
-      activeSince: 1722372950
+      activeSince: 1722372950,
     },
     {
       number: 83,
       direction: "Inbound",
       name: "Central Square",
       intersection: "Pearl Street & Clearwell Ave",
-      activeSince: 1722361948
+      activeSince: 1722361948,
     },
     {
       number: 83,
       direction: "Outbound",
       name: "Rindge Ave",
       intersection: "Pearl Street & Clearwell Ave",
-      activeSince: 1721361948
-    }
+      activeSince: 1721361948,
+    },
   ]
 
   const epochNowInSeconds = useCurrentTimeSeconds()
@@ -43,19 +42,23 @@ export const DummyDetourPage = () => {
           </tr>
         </thead>
         <tbody>
-          {fakeData.map(detour => (
+          {fakeData.map((detour) => (
             <tr>
               <td className="align-middle">
                 <div className="c-detour-table__route-info-cell">
                   <RoutePill routeName={detour.number.toString()} />
                   <div className="c-detour-table__route-info-text">
                     <span>{detour.direction}</span>
-                    <span className="c-detour-table__route-info-text--bold">{detour.name}</span>
+                    <span className="c-detour-table__route-info-text--bold">
+                      {detour.name}
+                    </span>
                   </div>
                 </div>
               </td>
               <td className="align-middle">{detour.intersection}</td>
-              <td className="align-middle">{timeAgoLabel(epochNowInSeconds, detour.activeSince)}</td>
+              <td className="align-middle">
+                {timeAgoLabel(epochNowInSeconds, detour.activeSince)}
+              </td>
             </tr>
           ))}
         </tbody>
