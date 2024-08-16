199efe9c7dbcf8a55aa7ff112e0f95cb44cb301b
Hannah Purcell <hpurcell@mbta.com>
2024-08-16 16:22:14 -0400

    formatting

--- assets/tests/components/detourListPage.test.tsx
+++ assets/tests/components/detourListPage.test.tsx
@@ -3,7 +3,7 @@ import React from "react"
 import renderer from "react-test-renderer"
 import { DetourListPage } from "../../src/components/detourListPage"
 
-jest.useFakeTimers().setSystemTime(new Date('2024-08-16T13:00:00'));
+jest.useFakeTimers().setSystemTime(new Date("2024-08-16T13:00:00"))
 
 describe("DetourListPage", () => {
   test("renders detour list page with dummy data", () => {
