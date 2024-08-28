bd7b7cd3510ec61a5b6d83adab54a494d4e13032
Josh Larson <jlarson@mbta.com>
2024-08-28 08:08:35 -0400

    refactor: Explicitly export shapeDataFactory (#2756)

--- assets/tests/factories/shape_data.ts
+++ assets/tests/factories/shape_data.ts
@@ -2,7 +2,7 @@ import { Factory } from "fishery"
 import { ShapeData } from "../../src/models/shapeData"
 import { stopDataFactory } from "./stopData"
 
-export default Factory.define<ShapeData>(({ sequence }) => ({
+export const shapeDataFactory = Factory.define<ShapeData>(({ sequence }) => ({
   id: `shape${sequence}`,
   points: [{ shape_id: `shape${sequence}`, sequence: 1, lat: 0, lon: 0 }],
   stops: [stopDataFactory.build({ id: `stop${sequence}` })],
--- assets/tests/models/shapeData.test.ts
+++ assets/tests/models/shapeData.test.ts
@@ -4,7 +4,7 @@ import {
   shapeFromData,
   shapesFromData,
 } from "../../src/models/shapeData"
-import shapeDataFactory from "../factories/shape_data"
+import { shapeDataFactory } from "../factories/shape_data"
 import shapeFactory from "../factories/shape"
 import { stopDataFactory } from "../factories/stopData"
 import { stopsFromData } from "../../src/models/stopData"
