7062f2dff084300bdcea13b3353b0fb80710605f
Hannah Purcell <hpurcell@mbta.com>
2024-08-30 12:07:41 -0400

    fix: use beforeEach for setting up mock fetches

--- assets/tests/components/detourListPage.test.tsx
+++ assets/tests/components/detourListPage.test.tsx
@@ -1,54 +1,63 @@
-import { describe, test, expect, jest } from "@jest/globals"
+import { describe, test, expect, jest, beforeEach } from "@jest/globals"
 import React from "react"
-import renderer from "react-test-renderer"
 import { DetourListPage } from "../../src/components/detourListPage"
-import { useAllDetours } from "../../src/hooks/useAllDetours"
+import { fetchDetours } from "../../src/api"
+import { neverPromise } from "../testHelpers/mockHelpers"
+import { Ok } from "../../src/util/result"
+import { render, screen } from "@testing-library/react"
 
 jest.useFakeTimers().setSystemTime(new Date("2024-08-29T20:00:00"))
 
-jest.mock("../../src/hooks/useAllDetours", () => ({
-  useAllDetours: jest.fn(() => {}),
-}))
+jest.mock("../../src/api")
+
+beforeEach(() => {
+  jest.mocked(fetchDetours).mockReturnValue(neverPromise())
+})
 
 describe("DetourListPage", () => {
-  test("renders detour list page", () => {
-    jest.mocked(useAllDetours).mockReturnValue({
-      active: [
-        {
-          route: "1",
-          direction: "Inbound",
-          name: "Headsign A",
-          intersection: "Street A & Avenue B",
-          updated_at: 1724866392,
-        },
-        {
-          route: "2",
-          direction: "Outbound",
-          name: "Headsign B",
-          intersection: "Street C & Avenue D",
-          updated_at: 1724856392,
-        },
-      ],
-      draft: null,
-      past: [
-        {
-          route: "1",
-          direction: "Inbound",
-          name: "Headsign A",
-          intersection: "Street E & Avenue F",
-          updated_at: 1724866392,
-        },
-        {
-          route: "1",
-          direction: "Outbound",
-          name: "Headsign Z",
-          intersection: "Street C & Avenue D",
-          updated_at: 1724866392,
-        },
-      ],
-    })
+  test("renders detour list page", async () => {
+    jest.mocked(fetchDetours).mockResolvedValue(
+      Ok({
+        active: [
+          {
+            route: "1",
+            direction: "Inbound",
+            name: "Headsign A",
+            intersection: "Street A & Avenue B",
+            updated_at: 1724866392,
+          },
+          {
+            route: "2",
+            direction: "Outbound",
+            name: "Headsign B",
+            intersection: "Street C & Avenue D",
+            updated_at: 1724856392,
+          },
+        ],
+        draft: null,
+        past: [
+          {
+            route: "1",
+            direction: "Inbound",
+            name: "Headsign A",
+            intersection: "Street E & Avenue F",
+            updated_at: 1724866392,
+          },
+          {
+            route: "1",
+            direction: "Outbound",
+            name: "Headsign Z",
+            intersection: "Street C & Avenue D",
+            updated_at: 1724866392,
+          },
+        ],
+      })
+    )
 
-    const tree = renderer.create(<DetourListPage />).toJSON()
-    expect(tree).toMatchSnapshot()
+    const { baseElement } = render(<DetourListPage />)
+
+    await screen.findByText("Headsign Z")
+
+    expect(baseElement).toMatchSnapshot()
   })
 })
