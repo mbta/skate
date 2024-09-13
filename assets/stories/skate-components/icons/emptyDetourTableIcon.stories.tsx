1dd508eda58895e42d62d373ecd4ca54e9ffcbb2
Josh Larson <jlarson@mbta.com>
2024-09-13 14:15:21 -0400

    fixup! feat: Icon for empty detour list table

--- assets/stories/skate-components/icons/emptyDetourTableIcon.stories.tsx
+++ assets/stories/skate-components/icons/emptyDetourTableIcon.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react"
-import { EmptyDetourTableIcon } from "../../../src/components/detoursTable"
+import { EmptyDetourTableIcon } from "../../../src/helpers/skateIcons"
 
 const meta = {
   component: EmptyDetourTableIcon,
