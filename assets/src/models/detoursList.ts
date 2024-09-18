5eadaa3130e203effe0c1d3bcfa4666ebdd67dc6
Hannah Purcell <hpurcell@mbta.com>
2024-09-18 13:40:22 -0400

    fix: Fixed frontend models for detour api calls

--- assets/src/models/detoursList.ts
+++ assets/src/models/detoursList.ts
@@ -1,16 +1,14 @@
 import { array, Infer, nullable, number, string, type } from "superstruct"
 
-export const SimpleDetour = type({
-  route: string(),
-  direction: string(),
-  name: string(),
-  intersection: string(),
-  updatedAt: number(),
-})
+export interface SimpleDetour {
+  route: string
+  direction: string
+  name: string
+  intersection: string
+  updatedAt: number
+}
 
-export type SimpleDetour = Infer<typeof SimpleDetour>
-
-export const SimpleDetourFromApi = type({
+export const SimpleDetourData = type({
   route: string(),
   direction: string(),
   name: string(),
@@ -18,10 +16,10 @@ export const SimpleDetourFromApi = type({
   updated_at: number(),
 })
 
-export type SimpleDetourFromApi = Infer<typeof SimpleDetourFromApi>
+export type SimpleDetourData = Infer<typeof SimpleDetourData>
 
 export const simpleDetourFromData = (
-  detourData: SimpleDetourFromApi
+  detourData: SimpleDetourData
 ): SimpleDetour => ({
   route: detourData.route,
   direction: detourData.direction,
@@ -30,30 +28,24 @@ export const simpleDetourFromData = (
   updatedAt: detourData.updated_at,
 })
 
-export const GroupedSimpleDetours = type({
-  active: nullable(array(SimpleDetour)),
-  draft: nullable(array(SimpleDetour)),
-  past: nullable(array(SimpleDetour)),
+export interface GroupedSimpleDetours {
+  active?: SimpleDetour[]
+  draft?: SimpleDetour[]
+  past?: SimpleDetour[]
+}
+
+export const GroupedDetoursData = type({
+  active: nullable(array(SimpleDetourData)),
+  draft: nullable(array(SimpleDetourData)),
+  past: nullable(array(SimpleDetourData)),
 })
 
-export type GroupedSimpleDetours = Infer<typeof GroupedSimpleDetours>
-
-export const GroupedDetoursFromApi = type({
-  active: nullable(array(SimpleDetourFromApi)),
-  draft: nullable(array(SimpleDetourFromApi)),
-  past: nullable(array(SimpleDetourFromApi)),
-})
-
-export type GroupedDetoursFromApi = Infer<typeof GroupedDetoursFromApi>
+export type GroupedDetoursData = Infer<typeof GroupedDetoursData>
 
 export const groupedDetoursFromData = (
-  groupedDetours: GroupedDetoursFromApi
+  groupedDetours: GroupedDetoursData
 ): GroupedSimpleDetours => ({
-  active:
-    groupedDetours.active?.map((detour) => simpleDetourFromData(detour)) ||
-    null,
-  draft:
-    groupedDetours.draft?.map((detour) => simpleDetourFromData(detour)) || null,
-  past:
-    groupedDetours.past?.map((detour) => simpleDetourFromData(detour)) || null,
+  active: groupedDetours.active?.map((detour) => simpleDetourFromData(detour)),
+  draft: groupedDetours.draft?.map((detour) => simpleDetourFromData(detour)),
+  past: groupedDetours.past?.map((detour) => simpleDetourFromData(detour)),
 })
