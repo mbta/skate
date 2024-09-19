88ade75ec50ade276533be7dc90bf2bc320ddbd6
Kayla Firestack <kfirestack@mbta.com>
2024-09-19 18:01:37 -0400

    feat:doc: stub doc headers for database tables
    `mix docs` doesn't generate anything for these modules if `@moduledoc false` is set, but we're using `TypedEctoSchema` now which _does_ generate type information, which can be surfaced by `ExDoc`.
    
    It's also impossible to link to modules in documentation if `@moduledoc false` is set.

--- lib/skate/settings/db/user.ex
+++ lib/skate/settings/db/user.ex
@@ -1,5 +1,7 @@
 defmodule Skate.Settings.Db.User do
-  @moduledoc false
+  @moduledoc """
+  Ecto Model for `users` Database table
+  """
 
   use Skate.Schema
   import Ecto.Changeset
--- lib/skate/settings/db/user_settings.ex
+++ lib/skate/settings/db/user_settings.ex
@@ -1,5 +1,7 @@
 defmodule Skate.Settings.Db.UserSettings do
-  @moduledoc false
+  @moduledoc """
+  Ecto Model for `user_settings` Database table
+  """
 
   use Skate.Schema
   import Ecto.Changeset
