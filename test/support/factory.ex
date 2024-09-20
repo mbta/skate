9eeaca182018d091dbc17c5afb4712215a4d96d2
Kayla Firestack <kfirestack@mbta.com>
2024-09-20 08:45:42 -0400

    feat: send detour notification when detour is activated

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -416,30 +416,62 @@ defmodule Skate.Factory do
   def detour_factory do
     %Skate.Detours.Db.Detour{
       author: build(:user),
-      state: %{
-        "context" => %{
-          "route" => %{
-            "name" => sequence("detour_route_name:"),
-            "directionNames" => %{
-              "0" => "Outbound",
-              "1" => "Inbound"
-            }
-          },
-          "routePattern" => %{
-            "name" => sequence("detour_route_pattern_name:"),
-            "headsign" => sequence("detour_route_pattern_headsign:"),
-            "directionId" => sequence(:detour_route_pattern_direction, [0, 1])
-          }
-        }
-      }
+      state: build(:detour_snapshot)
     }
   end
 
-  def with_direction(%Skate.Detours.Db.Detour{} = detour, :inbound) do
-    put_in(detour.state["context"]["routePattern"]["directionId"], 1)
+  def detour_snapshot_factory do
+    %{
+      "context" => %{
+        "uuid" => nil,
+        "route" => %{
+          "name" => sequence("detour_route_name:"),
+          "directionNames" => %{
+            "0" => "Outbound",
+            "1" => "Inbound"
+          }
+        },
+        "routePattern" => %{
+          "name" => sequence("detour_route_pattern_name:"),
+          "headsign" => sequence("detour_route_pattern_headsign:"),
+          "directionId" => sequence(:detour_route_pattern_direction, [0, 1])
+        }
+      },
+      "value" => %{}
+    }
   end
 
-  def with_direction(%Skate.Detours.Db.Detour{} = detour, :outbound) do
-    put_in(detour.state["context"]["routePattern"]["directionId"], 0)
+  def with_id(%Skate.Detours.Db.Detour{} = detour, id) do
+    %{detour | state: with_id(detour.state, id)}
+  end
+
+  def with_id(%{"context" => %{"uuid" => _}} = snapshot, id) do
+    put_in(snapshot["context"]["uuid"], id)
+  end
+
+  def activated(%Skate.Detours.Db.Detour{} = detour) do
+    %{detour | state: activated(detour.state)}
+  end
+
+  def activated(%{"value" => %{}} = state) do
+    put_in(state["value"], %{"Detour Drawing" => %{"Active" => "Reviewing"}})
+  end
+
+  def with_direction(%Skate.Detours.Db.Detour{} = detour, direction) do
+    %{
+      detour
+      | state: with_direction(detour.state, direction)
+    }
+  end
+
+  def with_direction(%{"context" => %{"routePattern" => %{"directionId" => _}}} = state, :inbound) do
+    put_in(state["context"]["routePattern"]["directionId"], 1)
+  end
+
+  def with_direction(
+        %{"context" => %{"routePattern" => %{"directionId" => _}}} = state,
+        :outbound
+      ) do
+    put_in(state["context"]["routePattern"]["directionId"], 0)
   end
 end
