81a159704aada14a21f67a18bca952eba2894583
Hannah Purcell <69368883+hannahpurcell@users.noreply.github.com>
2024-10-10 15:29:48 -0400

    feat: Simple drawer component (#2843)
    * feat: Simple drawer component
    
    Co-authored-by: Kayla Firestack <firestack@users.noreply.github.com>

---
+++ assets/src/components/drawer.tsx
@@ -0,0 +1,53 @@
+import React, { PropsWithChildren, useState } from "react"
+import DrawerTab from "../components/drawerTab"
+import { joinClasses } from "../helpers/dom"
+
+interface Props {
+  open?: boolean
+  onToggleOpen?: () => void
+}
+
+const Drawer = ({
+  open = true,
+  onToggleOpen,
+  children,
+}: PropsWithChildren<Props>) => (
+  <>
+    <div
+      className={joinClasses([
+        "c-drawer",
+        ...(open
+          ? ["c-drawer--visible"]
+          : ["c-drawer--hidden", "u-hideable--hidden"]),
+      ])}
+    >
+      <DrawerTab
+        isVisible={open}
+        toggleVisibility={onToggleOpen || (() => {})}
+      />
+      {children}
+    </div>
+    <button onClick={onToggleOpen} className="c-drawer__backdrop-button" />
+  </>
+)
+
+interface DrawerWithStateProps {
+  startOpen?: boolean
+}
+
+export const DrawerWithState = (
+  props: PropsWithChildren<DrawerWithStateProps>
+) => {
+  const [drawerOpen, setDrawerOpen] = useState(props.startOpen)
+
+  return (
+    <Drawer
+      open={drawerOpen}
+      onToggleOpen={() => setDrawerOpen((drawerOpen) => !drawerOpen)}
+    >
+      {props.children}
+    </Drawer>
+  )
+}
+
+Drawer.WithState = DrawerWithState
