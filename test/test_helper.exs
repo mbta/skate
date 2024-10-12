e0ea78c6d284401eb106953844867011a15f0f2d
Kayla Firestack <kfirestack@mbta.com>
2024-10-12 15:57:49 -0400

    cleanup(ex/test_helper): combine `ensure_application_started` calls

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,8 @@
-Application.ensure_all_started(:stream_data)
-{:ok, _} = Application.ensure_all_started(:ex_machina)
+{:ok, _} =
+  Application.ensure_all_started([
+    :stream_data,
+    :ex_machina
+  ])
 
 ExUnit.start(
   capture_log: true,
