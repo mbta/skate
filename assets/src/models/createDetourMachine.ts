ce2ea77eabd6beccaa0bdfbeab0ea1ecefc5e67d
Hannah Purcell <69368883+hannahpurcell@users.noreply.github.com>
2024-10-31 11:52:18 -0400

    feat: Editable directions in state machine (#2880)
    * feat: Put directions text in state machine to allow edits
    
    * feat: Split apart copyable text and editable text in diversionPage
    
    * fix: Needed to include fallback for older detours

--- assets/src/models/createDetourMachine.ts
+++ assets/src/models/createDetourMachine.ts
@@ -1,7 +1,7 @@
 import { setup, assign, fromPromise, ActorLogicFrom, InputFrom } from "xstate"
 import { RoutePatternId, ShapePoint } from "../schedule"
 import { Route, RouteId, RoutePattern } from "../schedule"
-import { Ok, Result } from "../util/result"
+import { isOk, Ok, Result } from "../util/result"
 import {
   FetchDetourDirectionsError,
   fetchDetourDirections,
@@ -30,6 +30,8 @@ export const createDetourMachine = setup({
       detourShape: Result<DetourShape, FetchDetourDirectionsError> | undefined
 
       finishedDetour: FinishedDetour | undefined | null
+
+      editedDirections?: string
 
       selectedDuration?: string
       selectedReason?: string
@@ -67,6 +69,7 @@ export const createDetourMachine = setup({
       | { type: "detour.edit.place-waypoint-on-route"; location: ShapePoint }
       | { type: "detour.edit.place-waypoint"; location: ShapePoint }
       | { type: "detour.edit.undo" }
+      | { type: "detour.share.edit-directions"; detourText: string }
       | { type: "detour.share.copy-detour"; detourText: string }
       | { type: "detour.share.open-activate-modal" }
       | {
@@ -488,6 +491,19 @@ export const createDetourMachine = setup({
 
           onDone: {
             target: "Share Detour",
+            actions: assign({
+              editedDirections: ({ context }) => {
+                const detourShape =
+                  context.detourShape && isOk(context.detourShape)
+                    ? context.detourShape.ok
+                    : null
+
+                return [
+                  "From " + context.nearestIntersection,
+                  ...(detourShape?.directions?.map((v) => v.instruction) ?? []),
+                ].join("\n")
+              },
+            }),
           },
         },
         "Share Detour": {
@@ -505,6 +521,12 @@ export const createDetourMachine = setup({
               on: {
                 "detour.share.open-activate-modal": {
                   target: "Activating",
+                },
+                "detour.share.edit-directions": {
+                  target: "Reviewing",
+                  actions: assign({
+                    editedDirections: ({ event }) => event.detourText,
+                  }),
                 },
               },
             },
