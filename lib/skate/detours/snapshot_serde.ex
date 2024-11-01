2a0dcea0e30baa943a68924a3f2edca20e4a66ca
Kayla Firestack <kfirestack@mbta.com>
2024-11-01 10:35:01 -0400

    chore(ex/detours): create ser-de module

---
+++ lib/skate/detours/snapshot_serde.ex
@@ -0,0 +1,6 @@
+defmodule Skate.Detours.SnapshotSerde do
+  @moduledoc """
+  Deserializes XState JSON Snapshots to Ecto Database Changesets
+  Serializes Ecto Database Structs into XState JSON Snapshots
+  """
+end
