a815dba7497ddc83ae9efcfdf1ad856bb5c62424
Kayla Firestack <kfirestack@mbta.com>
2024-11-01 09:44:57 -0400

    init(ex/skate/snapshot_serde): create `deserialize` function

--- lib/skate/detours/snapshot_serde.ex
+++ lib/skate/detours/snapshot_serde.ex
@@ -3,4 +3,28 @@ defmodule Skate.Detours.SnapshotSerde do
   Deserializes XState JSON Snapshots to Ecto Database Changesets
   Serializes Ecto Database Structs into XState JSON Snapshots
   """
+
+  @doc """
+  Converts a XState JSON Snapshot to Detours Database Changeset
+  """
+  def deserialize(user_id, %{} = snapshot) do
+    Skate.Detours.Db.Detour.changeset(
+      %Skate.Detours.Db.Detour{
+        # `id` is `nil` by default, so a `nil` `id` should be fine
+        id: id_from_snapshot(snapshot),
+        author_id: user_id
+      },
+      %{
+        # Save Snapshot to DB until we've fully transitioned to serializing
+        # snapshots from DB data
+        state: snapshot
+      }
+    )
+  end
+
+  @doc """
+  Extracts the Detour ID from a XState Snapshot
+  """
+  def id_from_snapshot(%{"context" => %{"uuid" => id}}), do: id
+  def id_from_snapshot(%{"context" => %{}}), do: nil
 end
