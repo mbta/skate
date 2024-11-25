97518040477e03008f390e1ba0db160d0372bc0f
Hannah Purcell <hpurcell@mbta.com>
2024-11-25 12:28:42 -0500

    fix: migration safety

--- priv/repo/migrations/20241112190520_create_route_patterns_table.exs
+++ priv/repo/migrations/20241112190520_create_route_patterns_table.exs
@@ -2,21 +2,15 @@ defmodule Skate.Repo.Migrations.CreateRoutePatternsTable do
   use Ecto.Migration
 
   def change do
-    execute("CREATE TYPE direction_name AS ENUM ('Inbound', 'Outbound')")
-
     create table(:route_patterns) do
       add :gtfs_route_pattern_id, :string
       add :gtfs_route_pattern_name, :string
       add :gtfs_route_pattern_headsign, :string
-      add :gtfs_route_pattern_direction_name, :direction_name
+      add :gtfs_route_pattern_direction_name, :string
       add :gtfs_route_id, :string
       add :gtfs_route_name, :string
       add :gtfs_route_pattern_time_description, :string
       timestamps()
     end
-
-    execute(
-      "CREATE UNIQUE INDEX route_patterns_unique_index ON route_patterns (gtfs_route_pattern_id, gtfs_route_pattern_name, gtfs_route_pattern_headsign, gtfs_route_pattern_direction_name, gtfs_route_id, gtfs_route_name, coalesce(gtfs_route_pattern_time_description, ''))"
-    )
   end
 end
