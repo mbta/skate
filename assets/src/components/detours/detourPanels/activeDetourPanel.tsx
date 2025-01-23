cb2631ae2657b0b7909782bd16edc77a6e1726b7
Kayla Firestack <kfirestack@mbta.com>
2025-01-23 15:07:40 -0500

    fix(ts/activeDetourPanel): fix time ago label

--- assets/src/components/detours/detourPanels/activeDetourPanel.tsx
+++ assets/src/components/detours/detourPanels/activeDetourPanel.tsx
@@ -106,7 +106,7 @@ export const ActiveDetourPanel = ({
               On detour since
             </dt>
             <dd aria-labelledby={dlActiveSinceId}>
-              {timeAgoLabelFromDate(currentTime, activatedAt)}
+              {timeAgoLabelFromDate(activatedAt, currentTime)}
             </dd>
 
             <dt id={dlDurationId} className="fw-bold me-2">
