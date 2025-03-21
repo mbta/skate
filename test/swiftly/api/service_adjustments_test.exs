39e2ab990e368c211ce035be46de7febecb58c85
Kayla Firestack <kfirestack@mbta.com>
2025-03-21 15:32:42 -0400

    test: create `MockClient` using Mox

--- test/swiftly/api/service_adjustments_test.exs
+++ test/swiftly/api/service_adjustments_test.exs
@@ -1,3 +1,11 @@
 defmodule Swiftly.API.ServiceAdjustmentsTest do
   use ExUnit.Case
+
+  @mock_client_module Swiftly.API.MockClient
+
+  setup_all do
+    Mox.defmock(@mock_client_module, for: Swiftly.API.Client)
+
+    :ok
+  end
 end
