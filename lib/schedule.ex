88e8780577001d389514a74cbca83a7cc81fdd53
meagharty <149533950+meagharty@users.noreply.github.com>
2025-04-30 13:12:36 -0400

    fix(timepoints): filter schedule pieces to scheduled trips with stop times (#2996)
    * test: timepoints from trips that include Schedule.AsDirected
    
    * fix(timepoints): filter schedule pieces to scheduled trips
    
    * test: remove schedule test data not used by function
    
    * tweak: updates to typespecs and removing Schedule prefix

--- lib/schedule.ex
+++ lib/schedule.ex
@@ -9,7 +9,8 @@ defmodule Schedule do
     Data,
     Hastus,
     Trip,
-    Swing
+    Swing,
+    Piece
   }
 
   alias Schedule.Gtfs.{
@@ -221,19 +222,21 @@ defmodule Schedule do
     |> timepoint_names_for_pieces(persistent_term_key)
   end
 
+  @spec timepoint_names_for_pieces([Piece.t()], persistent_term_key()) :: [Timepoint.t()]
   defp timepoint_names_for_pieces(pieces, persistent_term_key) do
     timepoint_names_by_id = call_with_data(persistent_term_key, [], :timepoint_names_by_id, %{})
 
     pieces
-    |> Enum.map(&timepoint_names_for_piece(&1, timepoint_names_by_id))
+    |> Enum.map(&Piece.scheduled_trips/1)
+    |> Enum.map(&timepoint_names_for_trips(&1, timepoint_names_by_id))
     |> List.flatten()
     |> Enum.uniq()
   end
 
-  defp timepoint_names_for_piece(piece, timepoint_names_by_id) do
-    piece.trips
+  defp timepoint_names_for_trips(trips, timepoint_names_by_id) do
+    trips
     |> Enum.reject(&is_nil/1)
-    |> Enum.flat_map(&Schedule.Trip.timepoints/1)
+    |> Enum.flat_map(&Trip.timepoints/1)
     |> Enum.map(&Timepoint.timepoint_from_names_by_id(timepoint_names_by_id, &1.timepoint_id))
   end
 
