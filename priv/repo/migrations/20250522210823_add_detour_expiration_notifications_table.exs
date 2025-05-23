ba71315fb4999cb20fc9ab837a380121739e3b98
Brian Fauble <bfauble@users.noreply.github.com>
2025-05-23 08:27:07 -0500

    Update priv/repo/migrations/20250522210823_add_detour_expiration_notifications_table.exs
    Co-authored-by: Kayla Firestack <firestack@users.noreply.github.com>

--- priv/repo/migrations/20250522210823_add_detour_expiration_notifications_table.exs
+++ priv/repo/migrations/20250522210823_add_detour_expiration_notifications_table.exs
@@ -3,7 +3,7 @@ defmodule Skate.Repo.Migrations.AddDetourExpirationNotificationsTable do
 
   def up do
     create table("detour_expiration_notifications") do
-      add :expires_at, :utc_datetime, null: false
+      add :expires_at, :utc_datetime_usec, null: false
       add :detour_id, references(:detours), null: false
       timestamps()
     end
