9544d0662b80b3507e9e7fa9fb2d64d86c236c7c
Brian Fauble <bfauble@users.noreply.github.com>
2025-05-29 11:29:36 -0600

    format

--- priv/repo/migrations/20250522210823_add_detour_expiration_tasks_table.exs
+++ priv/repo/migrations/20250522210823_add_detour_expiration_tasks_table.exs
@@ -4,7 +4,10 @@ defmodule Skate.Repo.Migrations.AddDetourExpirationTasksTable do
   def change do
     create table("detour_expiration_tasks") do
       add :expires_at, :utc_datetime_usec, null: false
-      add :detour_id, references(:detours, on_delete: :delete_all, on_update: :update_all), null: false
+
+      add :detour_id, references(:detours, on_delete: :delete_all, on_update: :update_all),
+        null: false
+
       timestamps()
     end
   end
