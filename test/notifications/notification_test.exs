f3fbda67eadffc024853e45310373abd018fc863
Kayla Firestack <kfirestack@mbta.com>
2025-06-25 13:04:43 -0400

    fix:feedback(ex/notifications): sort users by id before assertion

--- test/notifications/notification_test.exs
+++ test/notifications/notification_test.exs
@@ -518,7 +518,8 @@ defmodule Notifications.NotificationTest do
                  estimated_duration: "1 hour"
                })
 
-      assert ^users = notification |> Ecto.assoc(:users) |> Skate.Repo.all()
+      assert ^users =
+               notification |> Ecto.assoc(:users) |> Skate.Repo.all() |> Enum.sort_by(& &1.id)
     end
   end
 end
