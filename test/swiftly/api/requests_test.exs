aed0cd28cbe3e91469b7720c21653e1fdfb3a565
Brian Fauble <bfauble@users.noreply.github.com>
2025-07-14 11:17:21 -0600

    ⚙️ Add “skippedStops” to Swiftly API request (#3038)
    * pass along missedStop ids with swiftly requests
    
    * format

--- test/swiftly/api/requests_test.exs
+++ test/swiftly/api/requests_test.exs
@@ -22,6 +22,7 @@ defmodule Swiftly.Api.RequestTest do
       detour =
         build(:detour)
         |> with_route(%{name: "test_route_name", id: "test_route_id"})
+        |> with_missed_stops(["skipped1", "skipped2"])
         |> with_direction(:inbound)
         |> with_coordinates()
         |> activated(~U[2025-03-27 12:45:00.00Z])
@@ -48,7 +49,48 @@ defmodule Swiftly.Api.RequestTest do
                         [42.339848, -71.067554],
                         [42.340134, -71.068427],
                         [42.340216, -71.068579]
-                      ]
+                      ],
+                      skippedStops: ["skipped1", "skipped2"]
+                    }
+                  ]
+                }
+              }} = to_swiftly(detour)
+
+      assert notes == Integer.to_string(detour.id)
+    end
+
+    test "it translates an active detour to the format expected by swiftly, no skipped stops" do
+      detour =
+        build(:detour)
+        |> with_route(%{name: "test_route_name", id: "test_route_id"})
+        |> with_direction(:inbound)
+        |> with_coordinates()
+        |> activated(~U[2025-03-27 12:45:00.00Z])
+        |> insert()
+
+      detour = get_detour_with_all_virtual_fields(detour.id)
+
+      assert {:ok,
+              %CreateAdjustmentRequestV1{
+                feedId: nil,
+                feedName: nil,
+                notes: notes,
+                details: %DetourV0CreationDetailsV1{
+                  adjustmentType: :DETOUR_V0,
+                  beginTime: "2025-03-27T08:45:00.000000-04:00",
+                  detourRouteDirectionDetails: [
+                    %{
+                      routeShortName: "test_route_name",
+                      direction: "1",
+                      shape: [
+                        [42.337949, -71.074936],
+                        [42.338488, -71.066487],
+                        [42.339672, -71.067018],
+                        [42.339848, -71.067554],
+                        [42.340134, -71.068427],
+                        [42.340216, -71.068579]
+                      ],
+                      skippedStops: []
                     }
                   ]
                 }
