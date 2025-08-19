f98d160698417122408b8f95f76246c00c5852cd
Meag Harty <149533950+meagharty@users.noreply.github.com>
2025-08-19 11:08:24 -0400

    fix: add check for non-null state_of_charge time

--- assets/src/models/vehicleData.ts
+++ assets/src/models/vehicleData.ts
@@ -227,7 +227,10 @@ export function vehicleFromData(
     stateOfCharge: vehicleData.state_of_charge
       ? {
           value: vehicleData.state_of_charge.value,
-          time: dateFromEpochSeconds(vehicleData.state_of_charge.time),
+          time:
+            vehicleData.state_of_charge.time != null
+              ? dateFromEpochSeconds(vehicleData.state_of_charge.time)
+              : vehicleData.state_of_charge.time,
           milesRemaining: calculateMilesRemaining(vehicleData.state_of_charge),
         }
       : null,
