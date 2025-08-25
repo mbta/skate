680cae67ceaed0d45e1a2a7ce6b7519f134c51ed
Brian Fauble <bfauble@users.noreply.github.com>
2025-08-25 13:14:13 -0600

    format

--- assets/src/components/vehicleIcon.tsx
+++ assets/src/components/vehicleIcon.tsx
@@ -196,10 +196,10 @@ const TooltipContent = React.memo(
       <b>Operator:</b> <span className="fs-mask">{operatorDetails}</span>
       {inTestGroup(TestGroups.StateOfCharge) && stateOfCharge && (
         <>
-        <br />
-        <b>Battery:</b> {stateOfCharge.value}%
-        <br />
-        <b>Miles Remaining Estimate:</b> {stateOfCharge.milesRemaining} miles
+          <br />
+          <b>Battery:</b> {stateOfCharge.value}%
+          <br />
+          <b>Miles Remaining Estimate:</b> {stateOfCharge.milesRemaining} miles
         </>
       )}
     </>
