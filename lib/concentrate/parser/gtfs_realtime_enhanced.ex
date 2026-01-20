55161d7b66760cdd689d057a0bba99f965f80293
Steven Rowe <srowe@TID27988SROWE.local>
2026-01-20 09:55:12 -0500

    Remove allow list from state of charge

--- lib/concentrate/parser/gtfs_realtime_enhanced.ex
+++ lib/concentrate/parser/gtfs_realtime_enhanced.ex
@@ -7,19 +7,6 @@ defmodule Concentrate.Parser.GTFSRealtimeEnhanced do
   alias Concentrate.{StopTimeUpdate, TripUpdate, VehiclePosition}
   alias Realtime.Crowding
 
-  @soc_vehicle_allow_list [
-    "4200",
-    "4201",
-    "4202",
-    "4203",
-    "4204",
-    "4300",
-    "4301",
-    "4302",
-    "4303",
-    "4304"
-  ]
-
   @impl Concentrate.Parser
   def parse(json), do: decode_entities(json)
 
@@ -100,7 +87,7 @@ defmodule Concentrate.Parser.GTFSRealtimeEnhanced do
             data_discrepancies: [],
             crowding: decode_crowding(vp),
             revenue: Map.get(vp, "revenue", true),
-            state_of_charge: decode_state_of_charge(vp, Map.get(vehicle, "label"))
+            state_of_charge: decode_state_of_charge(vp)
           )
         ]
 
@@ -161,7 +148,7 @@ defmodule Concentrate.Parser.GTFSRealtimeEnhanced do
     Map.get(vp, "occupancy_status")
   end
 
-  defp decode_state_of_charge(vp, vehicle_id) when vehicle_id in @soc_vehicle_allow_list do
+  defp decode_state_of_charge(vp) do
     case vp do
       %{
         "state_of_charge_percentage" => pct,
@@ -173,10 +160,6 @@ defmodule Concentrate.Parser.GTFSRealtimeEnhanced do
       _ ->
         %{value: nil, time: nil}
     end
-  end
-
-  defp decode_state_of_charge(_vp, _) do
-    nil
   end
 
   @spec date(String.t() | nil) :: :calendar.date() | nil
