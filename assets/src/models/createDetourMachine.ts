e5f2ef4c5a9a0c8dff7a4cf041d25f589cedc14c
Steven Rowe <srowe4@mbta.com>
2026-02-02 15:06:04 -0500

    Discard unsaved edits to active detours

--- assets/src/models/createDetourMachine.ts
+++ assets/src/models/createDetourMachine.ts
@@ -117,7 +117,7 @@ export const createDetourMachine = setup({
     // That leads to the following interface: if the user begins drafting a detour, adds waypoints, and then changes the route,
     // the database will reflect the old route and old waypoints up until the point where a new waypoint is added.
     // If that UX assumption isn't the right one, we can iterate in the future!
-    tags: "no-save",
+    tags: {} as "no-save" | "save-activated",
   },
   actors: {
     "fetch-route-patterns": fromPromise<
@@ -695,7 +695,9 @@ export const createDetourMachine = setup({
                     },
                   },
                 },
-                Done: { type: "final" },
+                Done: {
+                  type: "final",
+                },
               },
               onDone: {
                 target: "Done",
@@ -778,9 +780,12 @@ export const createDetourMachine = setup({
           onDone: {
             target: "Past",
           },
+          tags: "save-activated"
         },
 
-        Past: {},
+        Past: {
+          tags: "save-activated"
+        },
 
         Deleted: {
           id: "Deleted",
