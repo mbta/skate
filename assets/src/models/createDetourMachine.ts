dff51dbb451e8fb0ba6836fc61c4fe0a0392d38c
Steven Rowe <srowe4@mbta.com>
2026-02-02 15:15:10 -0500

    update save state comment

--- assets/src/models/createDetourMachine.ts
+++ assets/src/models/createDetourMachine.ts
@@ -115,7 +115,8 @@ export const createDetourMachine = setup({
     // -- when the route id / route pattern is getting selected
     // -- right after the route pattern is finalized, before any waypoints are added
     // That leads to the following interface: if the user begins drafting a detour, adds waypoints, and then changes the route,
-    // the database will reflect the old route and old waypoints up until the point where a new waypoint is added.
+    // the database will reflect the old route and old waypoints up until the point where a new waypoint is added,
+    // unless they are editing an already activated detour, when it will only be saved upon re-activation
     // If that UX assumption isn't the right one, we can iterate in the future!
     tags: {} as "no-save" | "save-activated",
   },
