8a3a1e322bd9c36d0e4f62a0a69b79efa84a709b
Steven Rowe <srowe4@mbta.com>
2026-03-09 15:47:24 -0400

    Remove from serde

--- lib/skate/detours/snapshot_serde.ex
+++ lib/skate/detours/snapshot_serde.ex
@@ -140,8 +140,7 @@ defmodule Skate.Detours.SnapshotSerde do
       "editedDirections" => editeddirections_from_detour(detour),
       "selectedDuration" => selectedduration_from_detour(detour),
       "selectedReason" => selectedreason_from_detour(detour),
-      "activatedAt" => activated_at_from_detour(detour),
-      "undoStack" => undostack_from_detour(detour)
+      "activatedAt" => activated_at_from_detour(detour)
     })
   end
 
@@ -304,20 +303,6 @@ defmodule Skate.Detours.SnapshotSerde do
   end
 
   defp editeddirections_from_detour(_), do: nil
-
-  # defp undostack_from_detour(%Detour{finished_detour: finished_detour}), do: finished_detour
-  defp undostack_from_detour(%Detour{
-         state: %{
-           "context" => %{
-             "undoStack" => undo_stack
-           }
-         }
-       }) do
-    log_fallback("undoStack")
-    undo_stack
-  end
-
-  defp undostack_from_detour(_), do: nil
 
   # defp selectedduration_from_detour(%Detour{snapshot_children: snapshot_children}), do: snapshot_children
   defp selectedduration_from_detour(
