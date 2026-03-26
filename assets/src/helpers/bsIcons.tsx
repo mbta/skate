1d392aaf008155cc4e5ebafc891cc07e3b3e047b
Steven Rowe <srowe4@mbta.com>
2026-03-26 14:57:16 -0400

    Fix failing test

--- assets/src/helpers/bsIcons.tsx
+++ assets/src/helpers/bsIcons.tsx
@@ -176,6 +176,7 @@ export const Calendar = (props: SvgProps) => (
     fill="currentColor"
     className="bi bi-calendar3"
     viewBox="0 0 16 16"
+    aria-hidden
     {...props}
   >
     <path d="M14 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2M1 3.857C1 3.384 1.448 3 2 3h12c.552 0 1 .384 1 .857v10.286c0 .473-.448.857-1 .857H2c-.552 0-1-.384-1-.857z" />
