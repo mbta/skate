8d4d33e84c0c8c2d5e1e0ca72aa22abecb0b8fbb
Brian Fauble <bfauble@users.noreply.github.com>
2026-04-14 11:06:11 -0600

    enable relativeSplatPath true

--- assets/tests/testHelpers/wrappers.tsx
+++ assets/tests/testHelpers/wrappers.tsx
@@ -6,7 +6,9 @@ import stateFactory from "../factories/applicationState"
 import { StateDispatchProvider } from "../../src/contexts/stateDispatchContext"
 
 export const Router = ({ children }: { children: React.ReactNode }) => (
-  <BrowserRouter future={{ v7_startTransition: true }}>
+  <BrowserRouter
+    future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+  >
     {children}
   </BrowserRouter>
 )
