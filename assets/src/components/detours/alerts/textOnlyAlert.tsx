1aa5916e7cbb41d3ee582d5d56151c5d66b369c3
Steven Rowe <srowe4@mbta.com>
2026-05-22 15:28:12 -0400

    Move alert to bottom on mobile

--- assets/src/components/detours/alerts/textOnlyAlert.tsx
+++ assets/src/components/detours/alerts/textOnlyAlert.tsx
@@ -3,7 +3,7 @@ import { ExclamationTriangleFill } from "../../../helpers/bsIcons"
 import BaseAlert from "../../alerts/baseAlert"
 
 const TextOnlyAlert = (): React.ReactElement => (
-  <div className="c-text-only-alert position-absolute top-0 left-0 m-2">
+  <div className="c-text-only-alert position-absolute m-2">
     <BaseAlert variant="warning" show={true}>
       <ExclamationTriangleFill aria-hidden={true} className="c-alert__icon" />
       Showing regular route - detour shape not available
