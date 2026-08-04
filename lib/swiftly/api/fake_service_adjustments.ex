ee24b75531b6726d975eb668d720c995112f2d30
Steven Rowe <srowe4@mbta.com>
2026-08-04 16:52:54 -0400

    Update fake specs

--- lib/swiftly/api/fake_service_adjustments.ex
+++ lib/swiftly/api/fake_service_adjustments.ex
@@ -8,7 +8,12 @@ defmodule Swiftly.API.FakeServiceAdjustments do
           keyword
         ) :: {:ok, Swiftly.API.ServiceAdjustments.AdjustmentIdResponse.t()}
   def create_adjustment_v1(_, _ \\ []) do
-    {:ok, %{adjustmentId: "0a9b90f5-f16a-4819-b051-565b39878530"}}
+    {
+      :ok,
+      %Swiftly.API.ServiceAdjustments.AdjustmentIdResponse{
+        adjustmentId: "0a9b90f5-f16a-4819-b051-565b39878530"
+      }
+    }
   end
 
   @spec delete_adjustment_v1(Swiftly.Api.ServiceAdjustments.AdjustmentId.t(), keyword) ::
@@ -17,9 +22,9 @@ defmodule Swiftly.API.FakeServiceAdjustments do
     {:ok, nil}
   end
 
-  # @spec get_adjustments_v1(keyword) ::
-  #         {:ok, Swiftly.API.ServiceAdjustments.AdjustmentsResponseV1.t()}
+  @spec get_adjustments_v1(keyword) ::
+          {:ok, Swiftly.API.ServiceAdjustments.AdjustmentsResponseV1.t()}
   def get_adjustments_v1(_ \\ []) do
-    {:ok, %{adjustments: []}}
+    {:ok, %Swiftly.API.ServiceAdjustments.AdjustmentsResponseV1{adjustments: []}}
   end
 end
